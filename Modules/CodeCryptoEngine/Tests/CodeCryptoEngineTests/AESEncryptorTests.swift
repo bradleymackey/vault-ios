2269580e50175320a3cafa37b1002809c9d48ea7
Bradley Mackey <bradleymackey@me.com>
2023-04-16 21:13:19 +0100

    Add AESEncryptor

---
+++ Modules/CodeCryptoEngine/Tests/CodeCryptoEngineTests/AESEncryptorTests.swift
@@ -0,0 +1,75 @@
+import CryptoSwift
+import Foundation
+import XCTest
+
+/// Encryption result.
+struct AESEncrypted {
+    let ciphertext: Data
+    let authenticationTag: Data
+}
+
+struct AESEncryptor {
+    private let key: Data
+    private let iv: Data
+
+    enum EncryptionError: Error {
+        case noGCMTagGenerated
+    }
+
+    init(key: Data, iv: Data) {
+        self.key = key
+        self.iv = iv
+    }
+
+    func encrypt(data: Data) throws -> AESEncrypted {
+        let gcm = GCM(iv: iv.bytes, mode: .detached)
+        let aes = try AES(key: key.bytes, blockMode: gcm, padding: .noPadding)
+        let ciphertextBytes = try aes.encrypt(data.bytes)
+        guard let authenticationTag = gcm.authenticationTag else {
+            throw EncryptionError.noGCMTagGenerated
+        }
+        return AESEncrypted(
+            ciphertext: Data(ciphertextBytes),
+            authenticationTag: Data(authenticationTag)
+        )
+    }
+}
+
+final class AESEncryptorTests: XCTestCase {
+    func test_encrypt_throwsForInvalidKeyLength() throws {
+        let key = Data("a".utf8)
+        let sut = makeSUT(key: key)
+
+        XCTAssertThrowsError(try sut.encrypt(data: anyData()))
+    }
+
+    func test_encrypt_performsKnownGoodOperationWithZero() throws {
+        let key = Data(hex: "0x00000000000000000000000000000000")
+        let iv = Data(hex: "0x000000000000000000000000")
+        let sut = makeSUT(key: key, iv: iv)
+
+        let message = Data()
+        let result = try sut.encrypt(data: message)
+
+        XCTAssertEqual(result.ciphertext, Data([]))
+        XCTAssertEqual(result.authenticationTag, Data(hex: "58e2fccefa7e3061367f1d57a4e7455a"))
+    }
+
+    func test_encrypt_performsKnownGoodOperationWithMessage() throws {
+        let key = Data(hex: "0xfeffe9928665731c6d6a8f9467308308")
+        let iv = Data(hex: "0xcafebabefacedbaddecaf888")
+        let sut = makeSUT(key: key, iv: iv)
+
+        let message = Data(hex: "0xd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255")
+        let result = try sut.encrypt(data: message)
+
+        XCTAssertEqual(result.ciphertext, Data(hex: "0x42831ec2217774244b7221b784d0d49ce3aa212f2c02a4e035c17e2329aca12e21d514b25466931c7d8f6a5aac84aa051ba30b396a0aac973d58e091473f5985"))
+        XCTAssertEqual(result.authenticationTag, Data(hex: "0x4d5c2af327cd64a62cf35abd2ba6fab4"))
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(key: Data = anyData(), iv: Data = anyData()) -> AESEncryptor {
+        AESEncryptor(key: key, iv: iv)
+    }
+}
