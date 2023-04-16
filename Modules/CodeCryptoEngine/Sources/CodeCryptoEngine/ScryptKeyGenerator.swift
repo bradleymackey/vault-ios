0eeba13083ebeaa96a80bf59e58e0a9cf271c57f
Bradley Mackey <bradleymackey@me.com>
2023-04-16 14:27:59 +0100

    Move ScryptKeyGenerator to production

---
+++ Modules/CodeCryptoEngine/Sources/CodeCryptoEngine/ScryptKeyGenerator.swift
@@ -0,0 +1,61 @@
+import CryptoSwift
+import Foundation
+
+/// A key generator, generating a 256-bit key using Scrypt.
+public struct ScryptKeyGenerator {
+    public struct Parameters {
+        /// **dkLen**
+        ///
+        /// Desired key length in bytes (Intended output length in octets of the derived key; a positive integer satisfying dkLen ≤ (232− 1) * hLen.)
+        public var outputLengthBytes: Int
+        /// **N**
+        ///
+        /// CPU/memory cost parameter – Must be a power of 2 (e.g. 1024)
+        public var costFactor: Int
+        /// **r**
+        ///
+        /// blocksize parameter, which fine-tunes sequential memory read size and performance. (8 is commonly used)
+        public var blockSizeFactor: Int
+        ///
+        ///
+        /// Parallelization parameter. (1 .. 232-1 * hLen/MFlen)
+        public var parallelizationFactor: Int
+
+        public init(outputLengthBytes: Int, costFactor: Int, blockSizeFactor: Int, parallelizationFactor: Int) {
+            self.outputLengthBytes = outputLengthBytes
+            self.costFactor = costFactor
+            self.blockSizeFactor = blockSizeFactor
+            self.parallelizationFactor = parallelizationFactor
+        }
+    }
+
+    private let engine: Scrypt
+
+    public init(password: Data, salt: Data, parameters: Parameters) throws {
+        engine = try Scrypt(
+            password: password.bytes,
+            salt: salt.bytes,
+            dkLen: parameters.outputLengthBytes,
+            N: parameters.costFactor,
+            r: parameters.blockSizeFactor,
+            p: parameters.parallelizationFactor
+        )
+    }
+
+    public func key() throws -> Data {
+        try Data(engine.calculate())
+    }
+}
+
+// MARK: - Parameters
+
+public extension ScryptKeyGenerator.Parameters {
+    static var aes256Strong: Self {
+        .init(
+            outputLengthBytes: 32,
+            costFactor: 16384,
+            blockSizeFactor: 8,
+            parallelizationFactor: 1
+        )
+    }
+}
--- Modules/CodeCryptoEngine/Tests/CodeCryptoEngineTests/ScryptKeyGeneratorTests.swift
+++ Modules/CodeCryptoEngine/Tests/CodeCryptoEngineTests/ScryptKeyGeneratorTests.swift
@@ -2,65 +2,6 @@ import CodeCryptoEngine
 import CryptoSwift
 import XCTest
 
-/// A key generator, generating a 256-bit key using Scrypt.
-struct ScryptKeyGenerator {
-    private let engine: Scrypt
-
-    struct Parameters {
-        /// **dkLen**
-        ///
-        /// Desired key length in bytes (Intended output length in octets of the derived key; a positive integer satisfying dkLen ≤ (232− 1) * hLen.)
-        var outputLengthBytes: Int
-        /// **N**
-        ///
-        /// CPU/memory cost parameter – Must be a power of 2 (e.g. 1024)
-        var costFactor: Int
-        /// **r**
-        ///
-        /// blocksize parameter, which fine-tunes sequential memory read size and performance. (8 is commonly used)
-        var blockSizeFactor: Int
-        ///
-        ///
-        /// Parallelization parameter. (1 .. 232-1 * hLen/MFlen)
-        var parallelizationFactor: Int
-    }
-
-    init(password: Data, salt: Data, parameters: Parameters) throws {
-        engine = try Scrypt(
-            password: password.bytes,
-            salt: salt.bytes,
-            dkLen: parameters.outputLengthBytes,
-            N: parameters.costFactor,
-            r: parameters.blockSizeFactor,
-            p: parameters.parallelizationFactor
-        )
-    }
-
-    func key() throws -> Data {
-        try Data(engine.calculate())
-    }
-}
-
-extension ScryptKeyGenerator.Parameters {
-    static var aes256Strong: Self {
-        .init(
-            outputLengthBytes: 32,
-            costFactor: 16384,
-            blockSizeFactor: 8,
-            parallelizationFactor: 1
-        )
-    }
-
-    static var fastForTesting: Self {
-        .init(
-            outputLengthBytes: 8,
-            costFactor: 16,
-            blockSizeFactor: 2,
-            parallelizationFactor: 1
-        )
-    }
-}
-
 final class KeyGeneratorV1Tests: XCTestCase {
     func test_init_doesNotThrowForValidParameters() {
         let params = ScryptKeyGenerator.Parameters(
@@ -115,4 +56,15 @@ final class KeyGeneratorV1Tests: XCTestCase {
 
 func anyData() -> Data {
     Data(hex: "FF")
+}
+
+extension ScryptKeyGenerator.Parameters {
+    static var fastForTesting: Self {
+        .init(
+            outputLengthBytes: 8,
+            costFactor: 16,
+            blockSizeFactor: 2,
+            parallelizationFactor: 1
+        )
+    }
 }
