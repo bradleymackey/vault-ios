eccc56bf6841573ffb4cb9fa799d0aaedf8f93d7
Bradley Mackey <bradleymackey@me.com>
2023-04-16 14:33:33 +0100

    Add data helpers doc comments

--- Modules/CodeCryptoEngine/Sources/CodeCryptoEngine/Data+Helpers.swift
+++ Modules/CodeCryptoEngine/Sources/CodeCryptoEngine/Data+Helpers.swift
@@ -8,12 +8,14 @@ extension UInt64 {
 }
 
 extension Data {
+    /// An unsafe operation to cast the bytes of this data to the provided type.
     func asType<T>(_: T.Type) -> T {
         withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
             ptr.load(as: T.self)
         }
     }
 
+    /// Interpret the UTF-8 bytes of the provided string as data.
     public init(byteString: String) {
         self.init(byteString.utf8)
     }
