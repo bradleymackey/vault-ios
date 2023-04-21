653ec0c7b32eb932fa75898bec0b860956ed5c04
Bradley Mackey <bradleymackey@me.com>
2023-04-21 21:45:30 +0100

    BlockExporter creates iterator

--- Modules/CryptoExporter/Package.swift
+++ Modules/CryptoExporter/Package.swift
@@ -5,19 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "CryptoExporter",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CryptoExporter",
-            targets: ["CryptoExporter"]),
+            targets: ["CryptoExporter"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "CryptoEngine", path: "../CryptoEngine"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CryptoExporter"),
+            name: "CryptoExporter",
+            dependencies: ["CryptoEngine"]
+        ),
         .testTarget(
             name: "CryptoExporterTests",
-            dependencies: ["CryptoExporter"]),
+            dependencies: ["CryptoExporter"]
+        ),
     ]
 )
