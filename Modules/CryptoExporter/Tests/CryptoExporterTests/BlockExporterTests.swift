a63de0284da597154ef4499c6fcc93e47c56a978
Bradley Mackey <bradleymackey@me.com>
2023-04-21 21:49:20 +0100

    Returns payload during iteration

--- Modules/CryptoExporter/Tests/CryptoExporterTests/BlockExporterTests.swift
+++ Modules/CryptoExporter/Tests/CryptoExporterTests/BlockExporterTests.swift
@@ -3,19 +3,33 @@ import CryptoExporter
 import XCTest
 
 struct BlockIterator: IteratorProtocol {
+    let payload: Data
     func next() -> Data? {
-        nil
+        payload
     }
 }
 
 struct BlockExporter: Sequence {
+    /// The data payload to export
+    let payload: Data
+
     func makeIterator() -> some IteratorProtocol<Data> {
-        BlockIterator()
+        BlockIterator(payload: payload)
     }
 }
 
 final class BlockExporterTests: XCTestCase {
-    func test_init_createsIterator() {
-        _ = BlockExporter().makeIterator()
+    func test_iterate_iteratesOverData() {
+        let payload = anyData()
+        let sut = BlockExporter(payload: payload)
+        var iter = sut.makeIterator()
+
+        XCTAssertEqual(iter.next(), payload)
+    }
+
+    // MARK: - Helpers
+
+    private func anyData() -> Data {
+        Data(hex: "FFFFFFEE")
     }
 }
