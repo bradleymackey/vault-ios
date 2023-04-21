ab519bebb374ce11d91616087c7bd564e05ea682
Bradley Mackey <bradleymackey@me.com>
2023-04-21 21:54:34 +0100

    Truncate block to max block size

--- Modules/CryptoExporter/Tests/CryptoExporterTests/BlockExporterTests.swift
+++ Modules/CryptoExporter/Tests/CryptoExporterTests/BlockExporterTests.swift
@@ -4,32 +4,55 @@ import XCTest
 
 struct BlockIterator: IteratorProtocol {
     let payload: Data
+    let maxBlockSize: Int
+
     func next() -> Data? {
-        payload
+        if payload.count > maxBlockSize {
+            return payload.subdata(in: 0 ..< maxBlockSize)
+        } else {
+            return payload
+        }
     }
 }
 
 struct BlockExporter: Sequence {
     /// The data payload to export
     let payload: Data
+    let maxBlockSize: Int
 
     func makeIterator() -> some IteratorProtocol<Data> {
-        BlockIterator(payload: payload)
+        BlockIterator(payload: payload, maxBlockSize: maxBlockSize)
     }
 }
 
 final class BlockExporterTests: XCTestCase {
-    func test_iterate_iteratesOverData() {
-        let payload = anyData()
-        let sut = BlockExporter(payload: payload)
+    func test_iterate_iteratesOverDataUnderBlockSizeLimit() {
+        let payload = anyData(bytes: 4)
+        let sut = BlockExporter(payload: payload, maxBlockSize: 8)
         var iter = sut.makeIterator()
 
         XCTAssertEqual(iter.next(), payload)
     }
 
+    func test_iterate_returnsDataAtPacketSizeLimit() {
+        let payload = anyData(bytes: 8)
+        let sut = BlockExporter(payload: payload, maxBlockSize: 8)
+        var iter = sut.makeIterator()
+
+        XCTAssertEqual(iter.next(), payload)
+    }
+
+    func test_iterate_truncatesDataOverLimit() {
+        let payload = anyData(bytes: 16)
+        let sut = BlockExporter(payload: payload, maxBlockSize: 8)
+        var iter = sut.makeIterator()
+
+        XCTAssertEqual(iter.next(), payload[0 ..< 8])
+    }
+
     // MARK: - Helpers
 
-    private func anyData() -> Data {
-        Data(hex: "FFFFFFEE")
+    private func anyData(bytes: Int = 8) -> Data {
+        Data(repeating: 0xFF, count: bytes)
     }
 }
