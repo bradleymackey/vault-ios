1e3a8b9da471588f9afe1eaf725d25416c52b359
Bradley Mackey <bradleymackey@me.com>
2023-04-22 14:19:01 +0100

    CryptoExporter is iOS only
    This allows us to use iOS primitives and libraries more easily.

--- Modules/CryptoExporter/Package.swift
+++ Modules/CryptoExporter/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CryptoExporter",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -39,8 +39,7 @@ let package = Package(
                 "CryptoExporter",
                 .product(
                     name: "SnapshotTesting",
-                    package: "swift-snapshot-testing",
-                    condition: .when(platforms: [.macOS])
+                    package: "swift-snapshot-testing"
                 ),
             ],
             exclude: ["__Snapshots__"]
