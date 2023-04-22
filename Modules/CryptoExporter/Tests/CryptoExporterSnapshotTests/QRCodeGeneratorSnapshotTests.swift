f75e3ee81f8758c281cf48737445eefa9ce11083
Bradley Mackey <bradleymackey@me.com>
2023-04-22 13:41:04 +0100

    Add QRCodeGenerator

---
+++ Modules/CryptoExporter/Tests/CryptoExporterSnapshotTests/QRCodeGeneratorSnapshotTests.swift
@@ -0,0 +1,36 @@
+import AppKit
+import CryptoExporter
+import Foundation
+import SnapshotTesting
+import XCTest
+
+final class QRCodeGeneratorSnapshotTests: XCTestCase {
+    func test_generatePNG_generatesPNGWithEmptyData() throws {
+        let sut = makeSUT()
+
+        let imageData = try XCTUnwrap(sut.generatePNG(data: Data()))
+
+        let image = try imageForSnapshotting(pngData: imageData)
+        assertSnapshot(matching: image, as: .image)
+    }
+
+    func test_generatePNG_generatesPNGWithNonEmptyData() throws {
+        let sut = makeSUT()
+
+        let data = Data(repeating: 0xFF, count: 200)
+        let imageData = try XCTUnwrap(sut.generatePNG(data: data))
+
+        let image = try imageForSnapshotting(pngData: imageData)
+        assertSnapshot(matching: image, as: .image)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> QRCodeGenerator {
+        QRCodeGenerator()
+    }
+
+    private func imageForSnapshotting(pngData: Data) throws -> NSImage {
+        try XCTUnwrap(NSImage(data: pngData))
+    }
+}
