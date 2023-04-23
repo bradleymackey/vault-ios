7ec5d7b789be53b59077ea87f9b712ac296cf6bb
Bradley Mackey <bradleymackey@me.com>
2023-04-23 14:06:29 +0100

    Add Snapshotting-specific extension for PDFs to consolidate PDF snapshotting logic
    Removes the need for PDF-specific test setup when snapshotting PDFs.

---
+++ Modules/CryptoExporter/Tests/CryptoExporterSnapshotTests/Helpers/PDFDocument+Snapshot.swift
@@ -0,0 +1,38 @@
+import Foundation
+import PDFKit
+import SnapshotTesting
+
+extension Snapshotting where Value == PDFDocument, Format == UIImage {
+    /// Snapshots a PDF as an image, so we don't worry about metadata/non-visible aspects of the PDF.
+    static func pdf(page: Int = 1) -> Snapshotting {
+        .init(
+            pathExtension: "png",
+            diffing: .image,
+            snapshot: { pdfDocument in
+                pdfDocument.asImage(page: page) ?? UIImage()
+            }
+        )
+    }
+}
+
+private extension PDFDocument {
+    func asImage(page: Int = 1) -> UIImage? {
+        guard let data = dataRepresentation() else { return nil }
+        let cfData = data as CFData
+        guard let provider = CGDataProvider(data: cfData) else { return nil }
+        guard let pdfDoc = CGPDFDocument(provider) else { return nil }
+        guard let page = pdfDoc.page(at: page) else { return nil }
+
+        let pageRect = page.getBoxRect(.mediaBox)
+        let renderer = UIGraphicsImageRenderer(size: pageRect.size)
+        return renderer.image { ctx in
+            UIColor.white.set()
+            ctx.fill(pageRect)
+
+            ctx.cgContext.translateBy(x: 0.0, y: pageRect.size.height)
+            ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+
+            ctx.cgContext.drawPDFPage(page)
+        }
+    }
+}
