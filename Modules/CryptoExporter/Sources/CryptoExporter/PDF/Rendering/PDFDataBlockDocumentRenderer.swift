2bcf55a12fb5901b1b54e45d70d7e77eed4831a9
Bradley Mackey <bradleymackey@me.com>
2023-04-24 10:49:33 +0100

    Denest drawing helper from PDFDataBlockDocumentRenderer

--- Modules/CryptoExporter/Sources/CryptoExporter/PDF/Rendering/PDFDataBlockDocumentRenderer.swift
+++ Modules/CryptoExporter/Sources/CryptoExporter/PDF/Rendering/PDFDataBlockDocumentRenderer.swift
@@ -26,7 +26,7 @@ public struct PDFDataBlockDocumentRenderer<
     public func render(document: DataBlockExportDocument) -> PDFDocument? {
         let renderer = rendererFactory.makeRenderer()
         let data = renderer.pdfData { context in
-            let drawer = DocumentPDFDrawer(context: context)
+            let drawer = PDFDocumentDrawerHelper(context: context)
             drawer.startNextPage()
             if let title = document.title {
                 drawer.draw(label: title)
@@ -40,78 +40,76 @@ public struct PDFDataBlockDocumentRenderer<
     }
 }
 
-extension PDFDataBlockDocumentRenderer {
-    private final class DocumentPDFDrawer {
-        let context: UIGraphicsPDFRendererContext
-        private var currentVerticalOffset = 0.0
-        private var currentImageNumberOnPage = 0
+private final class PDFDocumentDrawerHelper {
+    let context: UIGraphicsPDFRendererContext
+    private var currentVerticalOffset = 0.0
+    private var currentImageNumberOnPage = 0
 
-        init(context: UIGraphicsPDFRendererContext) {
-            self.context = context
-        }
+    init(context: UIGraphicsPDFRendererContext) {
+        self.context = context
+    }
 
-        func draw(label: DataBlockLabel) {
-            let (attributedString, rect) = renderedLabel(
-                for: label,
-                pageRect: context.pdfContextBounds,
-                textTop: currentVerticalOffset,
-                horizontalPadding: 10
-            )
-            attributedString.draw(in: rect)
-            currentVerticalOffset += label.padding.top
-            currentVerticalOffset += rect.height
-        }
+    func draw(label: DataBlockLabel) {
+        let (attributedString, rect) = renderedLabel(
+            for: label,
+            pageRect: context.pdfContextBounds,
+            textTop: currentVerticalOffset,
+            horizontalPadding: 10
+        )
+        attributedString.draw(in: rect)
+        currentVerticalOffset += label.padding.top
+        currentVerticalOffset += rect.height
+    }
 
-        func draw(images: [Data], imageRenderer: ImageRenderer, blockLayout: (CGRect) -> BlockLayout) {
-            var blockLayoutEngine = blockLayout(
-                context.pdfContextBounds.inset(by: UIEdgeInsets(top: currentVerticalOffset, left: 0, bottom: 0, right: 0))
-            )
-            for imageData in images {
-                defer { currentImageNumberOnPage += 1 }
-                var desiredRect = blockLayoutEngine.rect(atIndex: UInt(currentImageNumberOnPage))
-                if !blockLayoutEngine.isFullyWithinBounds(rect: desiredRect) {
-                    startNextPage()
-                    blockLayoutEngine = blockLayout(
-                        context.pdfContextBounds.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
-                    )
-                    currentImageNumberOnPage = 0
-                    desiredRect = blockLayoutEngine.rect(atIndex: UInt(currentImageNumberOnPage))
-                }
-                let image = imageRenderer.makeImage(fromData: imageData, size: desiredRect.size)
-                image?.draw(in: desiredRect)
+    func draw(images: [Data], imageRenderer: some PDFImageRenderer, blockLayout: (CGRect) -> some PDFDataBlockLayout) {
+        var blockLayoutEngine = blockLayout(
+            context.pdfContextBounds.inset(by: UIEdgeInsets(top: currentVerticalOffset, left: 0, bottom: 0, right: 0))
+        )
+        for imageData in images {
+            defer { currentImageNumberOnPage += 1 }
+            var desiredRect = blockLayoutEngine.rect(atIndex: UInt(currentImageNumberOnPage))
+            if !blockLayoutEngine.isFullyWithinBounds(rect: desiredRect) {
+                startNextPage()
+                blockLayoutEngine = blockLayout(
+                    context.pdfContextBounds.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+                )
+                currentImageNumberOnPage = 0
+                desiredRect = blockLayoutEngine.rect(atIndex: UInt(currentImageNumberOnPage))
             }
+            let image = imageRenderer.makeImage(fromData: imageData, size: desiredRect.size)
+            image?.draw(in: desiredRect)
         }
+    }
 
-        func startNextPage() {
-            context.beginPage()
-            currentVerticalOffset = 0.0
-        }
+    func startNextPage() {
+        context.beginPage()
+        currentVerticalOffset = 0.0
+    }
 
-        private func renderedLabel(for label: DataBlockLabel, pageRect: CGRect, textTop: CGFloat, horizontalPadding: CGFloat) -> (NSAttributedString, CGRect) {
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .center
-            paragraphStyle.lineBreakMode = .byWordWrapping
+    private func renderedLabel(for label: DataBlockLabel, pageRect: CGRect, textTop: CGFloat, horizontalPadding: CGFloat) -> (NSAttributedString, CGRect) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineBreakMode = .byWordWrapping
 
-            let attributedText = NSAttributedString(
-                string: label.text,
-                attributes: [
-                    NSAttributedString.Key.paragraphStyle: paragraphStyle,
-                    NSAttributedString.Key.font: label.font,
-                ]
-            )
-            let width = pageRect.width - horizontalPadding * 2
-            let boundingRect = attributedText.boundingRect(
-                with: CGSize(width: width, height: .greatestFiniteMagnitude),
-                options: .usesLineFragmentOrigin,
-                context: nil
-            )
-            let textRect = CGRect(
-                x: horizontalPadding,
-                y: textTop + label.padding.top,
-                width: width,
-                height: boundingRect.height + label.padding.bottom
-            )
-            return (attributedText, textRect)
-        }
+        let attributedText = NSAttributedString(
+            string: label.text,
+            attributes: [
+                NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                NSAttributedString.Key.font: label.font,
+            ]
+        )
+        let width = pageRect.width - horizontalPadding * 2
+        let boundingRect = attributedText.boundingRect(
+            with: CGSize(width: width, height: .greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            context: nil
+        )
+        let textRect = CGRect(
+            x: horizontalPadding,
+            y: textTop + label.padding.top,
+            width: width,
+            height: boundingRect.height + label.padding.bottom
+        )
+        return (attributedText, textRect)
     }
 }
