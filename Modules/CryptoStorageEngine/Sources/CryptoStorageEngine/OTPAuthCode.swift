d6dc8636f376b3b0be4738b91d8620c917809878
Bradley Mackey <bradleymackey@me.com>
2023-04-30 14:52:11 +0100

    Remove reundant OTPAuthURI object
    Encode the codes directly.

--- Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthCode.swift
+++ Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthCode.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct OTPAuthCode {
+    public var type: OTPAuthType
     public var secret: OTPAuthSecret
     public var algorithm: OTPAuthAlgorithm
     public var digits: OTPAuthDigits
@@ -9,6 +10,7 @@ public struct OTPAuthCode {
     public var period: UInt = 30
 
     public init(
+        type: OTPAuthType = .totp,
         secret: OTPAuthSecret,
         algorithm: OTPAuthAlgorithm = .sha1,
         digits: OTPAuthDigits = .six,
@@ -16,6 +18,7 @@ public struct OTPAuthCode {
         issuer: String? = nil,
         period: UInt = 30
     ) {
+        self.type = type
         self.secret = secret
         self.algorithm = algorithm
         self.digits = digits
@@ -50,4 +53,9 @@ public enum OTPAuthAlgorithm: String {
     case sha1 = "SHA1"
     case sha256 = "SHA256"
     case sha512 = "SHA512"
+}
+
+public enum OTPAuthType {
+    case totp
+    case hotp
 }
