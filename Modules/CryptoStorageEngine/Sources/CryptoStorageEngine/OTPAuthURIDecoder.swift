f2eb96ea978dc1c0206b5225701f34909c125f7e
Bradley Mackey <bradleymackey@me.com>
2023-05-02 10:43:13 +0100

    Move URL helpers to seperate file

--- Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthURIDecoder.swift
+++ Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthURIDecoder.swift
@@ -104,14 +104,3 @@ extension OTPAuthURIDecoder {
         }
     }
 }
-
-private extension URL {
-    var queryParameters: [String: String] {
-        guard
-            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
-            let queryItems = components.queryItems else { return [:] }
-        return queryItems.reduce(into: [String: String]()) { result, item in
-            result[item.name] = item.value
-        }
-    }
-}
