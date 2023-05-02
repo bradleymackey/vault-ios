1ff7e86e216f97d99168013ae1087ec5b474fe80
Bradley Mackey <bradleymackey@me.com>
2023-05-02 10:45:51 +0100

    Fix naming of OTPAuthURI type, add scheme as shared helper

--- Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthURI.swift
+++ Modules/CryptoStorageEngine/Sources/CryptoStorageEngine/OTPAuthURI.swift
@@ -1,3 +1,10 @@
 import Foundation
 
-public typealias OAuthURI = URL
+public typealias OTPAuthURI = URL
+
+public extension OTPAuthURI {
+    /// The canonical URI scheme used for OTP auth code URIs.
+    static var otpAuthScheme: String {
+        "otpauth"
+    }
+}
