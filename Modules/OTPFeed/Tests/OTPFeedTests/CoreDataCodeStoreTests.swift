cf3ec9288cdc1d744f0f64022744ca5057384cdf
Bradley Mackey <bradleymackey@me.com>
2023-05-04 09:28:23 +0100

    Move test helpers to test helper file

--- Modules/OTPFeed/Tests/OTPFeedTests/CoreDataCodeStoreTests.swift
+++ Modules/OTPFeed/Tests/OTPFeedTests/CoreDataCodeStoreTests.swift
@@ -91,15 +91,3 @@ extension CoreDataCodeStoreTests {
         return OTPAuthCode(secret: .init(data: randomData, format: .base32), accountName: "Some Account")
     }
 }
-
-extension Data {
-    static func random(count: Int) -> Data {
-        var bytes = [UInt8]()
-        bytes.reserveCapacity(count)
-        for _ in 0 ..< count {
-            let nextByte = UInt8.random(in: UInt8.min ... UInt8.max)
-            bytes.append(nextByte)
-        }
-        return Data(bytes)
-    }
-}
