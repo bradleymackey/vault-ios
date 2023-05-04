4c29b22939d08b0bbdbc476dda8bf30d8e680a1a
Bradley Mackey <bradleymackey@me.com>
2023-05-04 09:25:44 +0100

    Add strings to presentation folder

--- Modules/OTPFeed/Package.swift
+++ Modules/OTPFeed/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OTPFeed",
+    defaultLocalization: "en",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
