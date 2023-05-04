375797c2202ca51c95b23a0c5a733e820d201a17
Bradley Mackey <bradleymackey@me.com>
2023-05-04 07:05:13 +0100

    Move CoreData helpers to seperate file

--- Modules/OTPFeed/Sources/OTPFeed/Infrastructure/CoreData/CoreDataCodeStore.swift
+++ Modules/OTPFeed/Sources/OTPFeed/Infrastructure/CoreData/CoreDataCodeStore.swift
@@ -60,27 +60,3 @@ public class CoreDataCodeStore {
         }
     }
 }
-
-extension NSManagedObjectModel {
-    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        bundle
-            .url(forResource: name, withExtension: "momd")
-            .flatMap {
-                NSManagedObjectModel(contentsOf: $0)
-            }
-    }
-}
-
-extension NSPersistentContainer {
-    static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
-        let description = NSPersistentStoreDescription(url: url)
-        let container = NSPersistentContainer(name: name, managedObjectModel: model)
-        container.persistentStoreDescriptions = [description]
-
-        var loadError: Swift.Error?
-        container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw $0 }
-
-        return container
-    }
-}
