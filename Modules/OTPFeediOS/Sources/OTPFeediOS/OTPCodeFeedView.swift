9ce8d791b6e1ffe71a40ff776af0fbd2a5b66463
Bradley Mackey <bradleymackey@me.com>
2023-05-05 23:09:04 +0100

    Extract TOTP view generation to injected protocol

--- Modules/OTPFeediOS/Sources/OTPFeediOS/OTPCodeFeedView.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/OTPCodeFeedView.swift
@@ -3,15 +3,13 @@ import OTPCore
 import OTPFeed
 import SwiftUI
 
-public struct OTPCodeFeedView<Store: OTPCodeStoreReader>: View {
+public struct OTPCodeFeedView<Store: OTPCodeStoreReader, TOTPView: TOTPViewGenerator>: View {
     @ObservedObject public var viewModel: FeedViewModel<Store>
-    public let clock: EpochClock
-    public let timer: LiveIntervalTimer
+    public var totpGenerator: TOTPView
 
-    public init(viewModel: FeedViewModel<Store>, clock: EpochClock, timer: LiveIntervalTimer) {
+    public init(viewModel: FeedViewModel<Store>, totpGenerator: TOTPView) {
         _viewModel = ObservedObject(initialValue: viewModel)
-        self.clock = clock
-        self.timer = timer
+        self.totpGenerator = totpGenerator
     }
 
     public var body: some View {
@@ -19,7 +17,7 @@ public struct OTPCodeFeedView<Store: OTPCodeStoreReader>: View {
             ForEach(viewModel.codes) { storedCode in
                 switch storedCode.code.type {
                 case let .totp(period):
-                    makeTOTPView(period: period, code: storedCode.code)
+                    totpGenerator.makeTOTPView(period: period, code: storedCode.code)
                 case .hotp:
                     Text("unsupported")
                 }
@@ -29,28 +27,16 @@ public struct OTPCodeFeedView<Store: OTPCodeStoreReader>: View {
             await viewModel.reloadData()
         }
     }
-
-    private func makeTOTPView(period: UInt32, code: OTPAuthCode) -> some View {
-        let hotpGenerator = code.hotpGenerator()
-        let totpGenerator = TOTPGenerator(generator: hotpGenerator, timeInterval: UInt64(period))
-        let mapper = TOTPCodeMapper(period: Double(period), generator: totpGenerator, clock: clock, interval: timer)
-        let (timer, renderer) = mapper.create()
-        return OTPCodePreviewView(
-            accountName: code.accountName,
-            issuer: code.issuer,
-            textView: .init(viewModel: .init(renderer: renderer), codeSpacing: 10.0),
-            timerView: .init(viewModel: .init(updater: timer, clock: clock)),
-            previewViewModel: .init(renderer: renderer)
-        )
-    }
 }
 
 struct OTPCodeFeedView_Previews: PreviewProvider {
     static var previews: some View {
         OTPCodeFeedView(
             viewModel: .init(store: MockCodeStore()),
-            clock: EpochClock(makeCurrentTime: { Date.now.timeIntervalSince1970 }),
-            timer: LiveIntervalTimer()
+            totpGenerator: LiveTOTPViewGenerator(
+                clock: EpochClock(makeCurrentTime: { Date.now.timeIntervalSince1970 }),
+                timer: LiveIntervalTimer()
+            )
         )
     }
 }
