47298e1deec8a0f1b8f1c280b634992cded796fc
Bradley Mackey <bradleymackey@me.com>
2023-05-05 18:57:47 +0100

    Make CodeManager app project

---
+++ CodeManager/CodeManager/CodeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeManagerApp.swift
+//  CodeManager
+//
+//  Created by Bradley Mackey on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
