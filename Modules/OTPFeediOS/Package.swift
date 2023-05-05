b15a51d84a3c5dd65b545b22e97088939ff42ad0
Bradley Mackey <bradleymackey@me.com>
2023-05-05 11:23:42 +0100

    Deployment target is 16

--- Modules/OTPFeediOS/Package.swift
+++ Modules/OTPFeediOS/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OTPFeediOS",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "OTPFeediOS",
