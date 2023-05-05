3b92be37bfcde65c7f0b362070402714c7663445
Bradley Mackey <bradleymackey@me.com>
2023-05-05 17:55:55 +0100

    Add hotpGenerator to OAuthCode

---
+++ Modules/OTPCore/Sources/OTPCore/OTPAuthCode+Generator.swift
@@ -0,0 +1,31 @@
+import CryptoEngine
+import Foundation
+
+extension OTPAuthCode {
+    /// Create an `HOTPGenerator` from this code, initalized with the current parameters of the code.
+    public func hotpGenerator() -> HOTPGenerator {
+        HOTPGenerator(secret: secret.data, digits: hotpDigits, algorithm: hotpAlgorithm)
+    }
+
+    private var hotpDigits: HOTPGenerator.Digits {
+        switch digits {
+        case .six:
+            return .six
+        case .seven:
+            return .seven
+        case .eight:
+            return .eight
+        }
+    }
+
+    private var hotpAlgorithm: HOTPGenerator.Algorithm {
+        switch algorithm {
+        case .sha1:
+            return .sha1
+        case .sha256:
+            return .sha256
+        case .sha512:
+            return .sha512
+        }
+    }
+}
