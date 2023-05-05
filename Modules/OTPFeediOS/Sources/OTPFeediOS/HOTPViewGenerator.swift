f80cc127c851b895664820db5b421f7b0c9d3728
Bradley Mackey <bradleymackey@me.com>
2023-05-05 23:43:04 +0100

    makeHOTPView renders an HOTP-specific preview view

--- Modules/OTPFeediOS/Sources/OTPFeediOS/HOTPViewGenerator.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/HOTPViewGenerator.swift
@@ -13,7 +13,14 @@ public struct LiveHOTPViewGenerator: HOTPViewGenerator {
 
     public func makeHOTPView(counter: UInt32, code: OTPAuthCode) -> some View {
         let renderer = HOTPCodeRenderer(hotpGenerator: code.hotpGenerator(), initialCounter: UInt64(counter))
-//        let previewViewModel = CodePreviewViewModel(renderer: renderer)
-        return Text("Code needs rendering")
+        let previewViewModel = CodePreviewViewModel(renderer: renderer)
+        let incrementerViewModel = CodeIncrementerViewModel(hotpRenderer: renderer, counter: UInt64(counter))
+        return HOTPCodePreviewView(
+            accountName: code.accountName,
+            issuer: code.issuer,
+            textView: CodeTextView(viewModel: previewViewModel, codeSpacing: 10.0),
+            buttonView: CodeButtonView(viewModel: incrementerViewModel),
+            previewViewModel: previewViewModel
+        )
     }
 }
