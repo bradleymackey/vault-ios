32dd68621fa2e8560670e04ff5a2cc8bfdf668fb
Bradley Mackey <bradleymackey@me.com>
2023-05-07 14:43:24 +0100

    Move MockCodeStore to seperate folder, only allow in DEBUG builds

--- CodeManager/CodeManager/ContentView.swift
+++ CodeManager/CodeManager/ContentView.swift
@@ -149,15 +149,3 @@ struct OnTapOverrideButtonModifier: ViewModifier {
             }
     }
 }
-
-struct MockCodeStore: OTPCodeStoreReader {
-    let codes: [StoredOTPCode] = [
-        .init(id: UUID(), code: .init(secret: .empty(), accountName: "example@example.com", issuer: "Ebay")),
-        .init(id: UUID(), code: .init(secret: .empty(), accountName: "example@example.com", issuer: "Cloudflare")),
-        .init(id: UUID(), code: .init(type: .hotp(), secret: .empty(), accountName: "HOTP test", issuer: "Authority")),
-    ]
-
-    func retrieve() async throws -> [OTPFeed.StoredOTPCode] {
-        codes
-    }
-}
