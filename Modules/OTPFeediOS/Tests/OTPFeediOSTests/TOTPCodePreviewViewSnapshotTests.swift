4fa20a48aa0c478f210b027c130b91bb54640fdd
Bradley Mackey <bradleymackey@me.com>
2023-05-07 21:30:05 +0100

    Ensure long code fits on a single line and all characters are the same size

--- Modules/OTPFeediOS/Tests/OTPFeediOSTests/TOTPCodePreviewViewSnapshotTests.swift
+++ Modules/OTPFeediOS/Tests/OTPFeediOSTests/TOTPCodePreviewViewSnapshotTests.swift
@@ -50,6 +50,16 @@ final class TOTPCodePreviewViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: sut, as: .image)
     }
 
+    func test_textWrapping_longCodeMaintainsSameSizeForAllDigits() {
+        let digits = [6, 8, 20]
+        for count in digits {
+            let code = String(Array(repeating: Character("0"), count: count))
+            let sut = makeSUT(state: .visible(code))
+
+            assertSnapshot(matching: sut, as: .image, named: "\(count)-digits")
+        }
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(
