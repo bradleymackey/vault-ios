f1fe82e9fbefb2ebbf220f838494e9c29e92e2f7
Bradley Mackey <bradleymackey@me.com>
2023-05-08 16:29:39 +0100

    Horizontal progress bar view does not need a binding

--- Modules/OTPFeediOS/Sources/OTPFeediOS/CodeTimerHorizontalBarView.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/CodeTimerHorizontalBarView.swift
@@ -14,7 +14,7 @@ public struct CodeTimerHorizontalBarView<Updater: CodeTimerUpdater>: View {
     public var body: some View {
         GeometryReader { proxy in
             HorizontalTimerProgressBarView(
-                fractionCompleted: $currentFractionCompleted,
+                fractionCompleted: currentFractionCompleted,
                 color: color
             )
             .onChange(of: proxy.size) { _ in
--- Modules/OTPFeediOS/Sources/OTPFeediOS/HorizontalTimerProgressBarView.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/HorizontalTimerProgressBarView.swift
@@ -2,7 +2,7 @@ import Combine
 import SwiftUI
 
 public struct HorizontalTimerProgressBarView: View {
-    @Binding var fractionCompleted: Double
+    var fractionCompleted: Double
     var color: Color
     var backgroundColor: Color = .init(UIColor.systemGray6)
 
@@ -23,21 +23,21 @@ struct HorizontalTimerProgressBarView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             HorizontalTimerProgressBarView(
-                fractionCompleted: .constant(0.4),
+                fractionCompleted: 0.4,
                 color: .blue
             )
             .frame(width: 250, height: 20)
             .previewLayout(.fixed(width: 300, height: 300))
 
             HorizontalTimerProgressBarView(
-                fractionCompleted: .constant(0.6),
+                fractionCompleted: 0.6,
                 color: .red
             )
             .frame(width: 250, height: 20)
             .previewLayout(.fixed(width: 300, height: 300))
 
             HorizontalTimerProgressBarView(
-                fractionCompleted: .constant(0.75),
+                fractionCompleted: 0.75,
                 color: .red,
                 backgroundColor: .yellow
             )
