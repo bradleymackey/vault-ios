2a091a7adf71829ba1d920fd6a9308bfdf123eaf
Bradley Mackey <bradleymackey@me.com>
2023-05-11 22:00:54 +0100

    Use cached, shared timers for common periods

--- Modules/OTPFeediOS/Sources/OTPFeediOS/TOTPViewGenerator.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/TOTPViewGenerator.swift
@@ -11,10 +11,12 @@ public protocol TOTPViewGenerator {
 }
 
 @MainActor
-public struct TOTPPreviewViewGenerator: TOTPViewGenerator {
+public final class TOTPPreviewViewGenerator: TOTPViewGenerator {
     let clock: EpochClock
     let timer: LiveIntervalTimer
     let hideCodes: Bool
+
+    private var timerCache = [UInt64: CodeTimerController<LiveIntervalTimer>]()
 
     public init(clock: EpochClock, timer: LiveIntervalTimer, hideCodes: Bool) {
         self.clock = clock
@@ -22,8 +24,18 @@ public struct TOTPPreviewViewGenerator: TOTPViewGenerator {
         self.hideCodes = hideCodes
     }
 
+    private func makeTimer(period: UInt64) -> CodeTimerController<LiveIntervalTimer> {
+        if let timer = timerCache[period] {
+            return timer
+        } else {
+            let timerController = CodeTimerController(timer: timer, period: period, clock: clock)
+            timerCache[period] = timerController
+            return timerController
+        }
+    }
+
     public func makeTOTPView(period: UInt64, code: OTPAuthCode) -> some View {
-        let timerController = CodeTimerController(timer: timer, period: period, clock: clock)
+        let timerController = makeTimer(period: period)
         let totpGenerator = TOTPGenerator(generator: code.hotpGenerator(), timeInterval: period)
         let renderer = TOTPCodeRenderer(timer: timerController, totpGenerator: totpGenerator)
         let previewViewModel = CodePreviewViewModel(
