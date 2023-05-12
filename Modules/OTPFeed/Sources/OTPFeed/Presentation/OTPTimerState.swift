9ed32147c910db4eb2f6731bf3486dd95b727daf
Bradley Mackey <bradleymackey@me.com>
2023-05-12 12:57:14 +0100

    Format and document OTPTimerState

--- Modules/OTPFeed/Sources/OTPFeed/Presentation/OTPTimerState.swift
+++ Modules/OTPFeed/Sources/OTPFeed/Presentation/OTPTimerState.swift
@@ -6,31 +6,39 @@ public struct OTPTimerState: Equatable {
     /// The number of epoch seconds when the timer will end.
     public var endTime: Double
 
-    /// The duration for a full timer rotation.
-    public var totalTime: Double {
-        endTime - startTime
-    }
-
     public init(startTime: Double, endTime: Double) {
         self.startTime = startTime
         self.endTime = endTime
     }
+}
 
-    public func remainingTime(at epoch: Double) -> Double {
+public extension OTPTimerState {
+    /// The duration for a full timer rotation.
+    var totalTime: Double {
+        endTime - startTime
+    }
+
+    /// The amount of time remaining before the timer runs out.
+    ///
+    /// This won't go below 0.
+    func remainingTime(at epoch: Double) -> Double {
         max(0, endTime - epoch)
     }
 
-    public func fractionCompleted(at epoch: Double) -> Double {
+    /// The fraction of the timer that has already been completed at the given time.
+    ///
+    /// This will only give an output `0...1`.
+    func fractionCompleted(at epoch: Double) -> Double {
         if totalTime == 0 { return 1 }
         let remainingTime = remainingTime(at: epoch)
         let remainingFraction = remainingTime / totalTime
         let fractionCompleted = 1 - remainingFraction
-        if fractionCompleted > 1 {
-            return 1
-        } else if fractionCompleted < 0 {
-            return 0
-        } else {
-            return fractionCompleted
-        }
+        return fractionCompleted.clamped(to: 0 ... 1)
+    }
+}
+
+private extension Comparable {
+    func clamped(to limits: ClosedRange<Self>) -> Self {
+        min(max(self, limits.lowerBound), limits.upperBound)
     }
 }
