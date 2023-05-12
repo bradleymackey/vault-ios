c8297af6b466ce5137ee0a29e59f66833b52e4c5
Bradley Mackey <bradleymackey@me.com>
2023-05-12 10:11:43 +0100

    Add unit tests for CodeTimerAnimationState

---
+++ Modules/OTPFeediOS/Tests/OTPFeediOSTests/CodeTimerAnimationStateTests.swift
@@ -0,0 +1,17 @@
+import Foundation
+import XCTest
+@testable import OTPFeediOS
+
+final class CodeTimerAnimationStateTests: XCTestCase {
+    func test_initialFraction_freezeIsFraction() {
+        let sut = CodeTimerAnimationState.freeze(fraction: 0.69)
+
+        XCTAssertEqual(sut.initialFraction, 0.69, accuracy: .ulpOfOne)
+    }
+
+    func test_initialFraction_animateIsStartFraction() {
+        let sut = CodeTimerAnimationState.animate(startFraction: 0.68, duration: 100)
+
+        XCTAssertEqual(sut.initialFraction, 0.68, accuracy: .ulpOfOne)
+    }
+}
