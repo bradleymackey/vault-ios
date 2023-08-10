49d5d423f6658ce47311625376ec6854aea2afcc
Bradley Mackey <bradleymackey@me.com>
2023-08-10 10:44:17 +0100

    Render retrieved third-party libraries

--- Modules/Attribution/Sources/Attribution/ThirdPartyLibrary.swift
+++ Modules/Attribution/Sources/Attribution/ThirdPartyLibrary.swift
@@ -5,3 +5,9 @@ public struct ThirdPartyLibrary: Decodable {
     public var url: URL
     public var licence: String
 }
+
+extension ThirdPartyLibrary: Identifiable {
+    public var id: some Hashable {
+        url
+    }
+}
--- Modules/SettingsiOS/Sources/SettingsiOS/ThirdPartyView.swift
+++ Modules/SettingsiOS/Sources/SettingsiOS/ThirdPartyView.swift
@@ -2,9 +2,20 @@ import Attribution
 import SwiftUI
 
 public struct ThirdPartyView: View {
+    @State private var libraries = [ThirdPartyLibrary]()
     public init() {}
 
     public var body: some View {
-        Text("Third party!")
+        List(libraries) { library in
+            Text(library.name)
+        }
+        .task {
+            do {
+                let attribution = try Attribution.parse()
+                libraries = attribution.libraries
+            } catch {
+                // TODO: handle
+            }
+        }
     }
 }
