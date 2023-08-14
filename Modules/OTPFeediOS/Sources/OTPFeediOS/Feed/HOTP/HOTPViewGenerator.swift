18a8f8a043145434244642c0563abc7e5e48d47f
Bradley Mackey <bradleymackey@me.com>
2023-08-14 10:59:50 +0100

    Cache view models in HOTPViewGenerator

--- Modules/OTPFeediOS/Sources/OTPFeediOS/Feed/HOTP/HOTPViewGenerator.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/Feed/HOTP/HOTPViewGenerator.swift
@@ -10,31 +10,52 @@ public protocol HOTPViewGenerator {
 }
 
 @MainActor
-public struct HOTPPreviewViewGenerator: HOTPViewGenerator {
+public final class HOTPPreviewViewGenerator: HOTPViewGenerator {
     let timer: LiveIntervalTimer
     var isEditing: Bool
+
+    private struct CachedViewModels {
+        var preview: CodePreviewViewModel
+        var incrementer: CodeIncrementerViewModel<LiveIntervalTimer>
+    }
+
+    private var viewModelCache = [UUID: CachedViewModels]()
 
     public init(timer: LiveIntervalTimer, isEditing: Bool) {
         self.timer = timer
         self.isEditing = isEditing
     }
 
+    private func makeViewModelForCode(
+        counter: UInt64,
+        code: StoredOTPCode
+    ) -> CachedViewModels {
+        if let viewModel = viewModelCache[code.id] {
+            return viewModel
+        } else {
+            let renderer = HOTPCodeRenderer(hotpGenerator: code.code.hotpGenerator())
+            let previewViewModel = CodePreviewViewModel(
+                accountName: code.code.accountName,
+                issuer: code.code.issuer,
+                renderer: renderer
+            )
+            previewViewModel.hideCodeUntilNextUpdate()
+            let incrementerViewModel = CodeIncrementerViewModel(
+                hotpRenderer: renderer,
+                timer: timer,
+                initialCounter: counter
+            )
+            let cached = CachedViewModels(preview: previewViewModel, incrementer: incrementerViewModel)
+            viewModelCache[code.id] = cached
+            return cached
+        }
+    }
+
     public func makeHOTPView(counter: UInt64, code: StoredOTPCode) -> some View {
-        let renderer = HOTPCodeRenderer(hotpGenerator: code.code.hotpGenerator())
-        let previewViewModel = CodePreviewViewModel(
-            accountName: code.code.accountName,
-            issuer: code.code.issuer,
-            renderer: renderer
-        )
-        previewViewModel.hideCodeUntilNextUpdate()
-        let incrementerViewModel = CodeIncrementerViewModel(
-            hotpRenderer: renderer,
-            timer: timer,
-            initialCounter: counter
-        )
+        let viewModels = makeViewModelForCode(counter: counter, code: code)
         return HOTPCodePreviewView(
-            buttonView: CodeButtonView(viewModel: incrementerViewModel),
-            previewViewModel: previewViewModel,
+            buttonView: CodeButtonView(viewModel: viewModels.incrementer),
+            previewViewModel: viewModels.preview,
             isEditing: isEditing
         )
     }
