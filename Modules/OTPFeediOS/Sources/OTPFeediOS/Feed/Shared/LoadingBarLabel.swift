b6806f53c54180e360a0fda166240ae02869e8b0
Bradley Mackey <bradleymackey@me.com>
2023-08-20 16:52:06 +0100

    Limit text overlay to a single line

--- Modules/OTPFeediOS/Sources/OTPFeediOS/Feed/Shared/LoadingBarLabel.swift
+++ Modules/OTPFeediOS/Sources/OTPFeediOS/Feed/Shared/LoadingBarLabel.swift
@@ -5,6 +5,8 @@ struct LoadingBarLabel: View {
     var text: String
     var body: some View {
         Text(text)
+            .lineLimit(1)
+            .truncationMode(.tail)
             .textCase(.uppercase)
             .font(.system(size: 10, weight: .bold))
             .foregroundColor(.white)
