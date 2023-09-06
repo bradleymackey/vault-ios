a954d56d95b8ae9117d0645fe74b85147ffe8baa
Bradley Mackey <bradleymackey@me.com>
2023-09-07 00:13:23 +0100

    Draft out skeleton settings

--- CodeManager/CodeManager/CodeSettingsView.swift
+++ CodeManager/CodeManager/CodeSettingsView.swift
@@ -14,6 +14,8 @@ struct CodeSettingsView: View {
         Form {
             aboutSection
             exportSection
+            policySection
+            noticesSection
         }
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.inline)
@@ -22,11 +24,11 @@ struct CodeSettingsView: View {
     private var aboutSection: some View {
         Section {
             NavigationLink {
-                ThirdPartyView()
+                Text("About")
             } label: {
                 FormRow(
-                    title: "Libraries",
-                    image: Image(systemName: "text.book.closed.fill"),
+                    title: "About",
+                    image: Image(systemName: "key.fill"),
                     color: .blue
                 )
             }
@@ -36,14 +38,86 @@ struct CodeSettingsView: View {
     private var exportSection: some View {
         Section {
             NavigationLink {
+                Text("Backup history")
+            } label: {
+                VStack(alignment: .center) {
+                    Text("Last backed up")
+                        .foregroundColor(.secondary)
+                        .font(.footnote)
+                    Text("23 days ago")
+                        .foregroundColor(.primary)
+                        .font(.title)
+                    Text("2 codes not backed up")
+                        .foregroundColor(.secondary)
+                        .font(.footnote)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+            }
+
+            NavigationLink {
+                Text("Restore")
+            } label: {
+                FormRow(
+                    title: "Restore from Backup",
+                    image: Image(systemName: "square.and.arrow.down.fill"),
+                    color: .green
+                )
+            }
+
+            NavigationLink {
                 Text("Export")
             } label: {
                 FormRow(
                     title: "Save Backup",
                     image: Image(systemName: "square.and.arrow.up.on.square.fill"),
-                    color: .red
+                    color: .purple
                 )
             }
+        } header: {
+            Text("Export")
+        }
+    }
+
+    private var policySection: some View {
+        Section {
+            NavigationLink {
+                Text("Info about Open Source, on GitHub")
+            } label: {
+                FormRow(
+                    title: "Open Source",
+                    image: Image(systemName: "figure.2.arms.open"),
+                    color: .purple
+                )
+            }
+
+            NavigationLink {
+                Text("Open Standards")
+            } label: {
+                FormRow(
+                    title: "Open Standards",
+                    image: Image(systemName: "doc.text.fill"),
+                    color: .purple
+                )
+            }
+        } header: {
+            Text("Policy")
+        }
+    }
+
+    private var noticesSection: some View {
+        Section {
+            NavigationLink {
+                ThirdPartyView()
+            } label: {
+                FormRow(
+                    title: "Third-Party Libraries",
+                    image: Image(systemName: "text.book.closed.fill"),
+                    color: .blue
+                )
+            }
+        } header: {
+            Text("Notices and Thanks")
         }
     }
 }
