2cfce17972f3e85f12c32fb66410fced5ed3012a
Bradley Mackey <bradleymackey@me.com>
2023-09-07 23:30:36 +0100

    Improve documentation of Cache

--- Modules/CoreModels/Sources/CoreModels/Cache.swift
+++ Modules/CoreModels/Sources/CoreModels/Cache.swift
@@ -1,3 +1,4 @@
+/// An unbounded cache that provides helper methods for creating values if it does not already exist in the cache.
 public struct Cache<Key: Hashable, Value> {
     private var cacheStorage = [Key: Value]()
 
@@ -7,20 +8,22 @@ public struct Cache<Key: Hashable, Value> {
         cacheStorage[key]
     }
 
+    /// A collection of all values that are currently stored in the cache.
     public var values: some Collection<Value> {
         cacheStorage.values
     }
 
+    /// The number of items that are currently stored in the cache.
     public var count: Int {
         cacheStorage.count
     }
 
+    /// Removes all items from the cache, requiring them to be recreated.
     public mutating func removeAll() {
         cacheStorage.removeAll()
     }
 
-    /// Get a given item from the cache, otherwise create it using the
-    /// given closure.
+    /// Get a given item from the cache, otherwise create it using the given closure.
     public mutating func get(key: Key, otherwise generate: () throws -> Value) rethrows -> Value {
         if let existing = cacheStorage[key] {
             return existing
