d7adac72879372bd936f90b16a41bb845c868815
Bradley Mackey <bradleymackey@me.com>
2023-09-07 22:59:58 +0100

    Test current code generation and code value from HOTPPreviewViewGenerator

--- Modules/OTPFeediOS/Tests/OTPFeediOSTests/HOTPPreviewViewGeneratorTests.swift
+++ Modules/OTPFeediOS/Tests/OTPFeediOSTests/HOTPPreviewViewGeneratorTests.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OTPCore
 import OTPFeediOS
 import XCTest
 
@@ -9,6 +10,30 @@ final class HOTPPreviewViewGeneratorTests: XCTestCase {
 
         XCTAssertEqual(timer.recordedWaitedIntervals, [])
     }
+
+    func test_makeOTPView_generatesViews() {
+        let (sut, _) = makeSUT()
+
+        let view = sut.makeOTPView(id: UUID(), code: anyHOTPCode(), behaviour: nil)
+
+        XCTAssertNotNil(view)
+    }
+
+    func test_currentCode_isNilIfCacheEmpty() {
+        let (sut, _) = makeSUT()
+
+        let code = sut.currentCode(id: UUID())
+
+        XCTAssertNil(code)
+    }
+
+    func test_currentCode_isValueIfCodeHasBeenGenerated() {
+        let (sut, _) = makeSUT()
+
+        let code = sut.currentCode(id: UUID())
+
+        XCTAssertNil(code)
+    }
 }
 
 extension HOTPPreviewViewGeneratorTests {
@@ -16,5 +41,10 @@ extension HOTPPreviewViewGeneratorTests {
         let timer = MockIntervalTimer()
         let sut = HOTPPreviewViewGenerator(timer: timer)
         return (sut, timer)
+    }
+
+    private func anyHOTPCode() -> HOTPAuthCode {
+        let codeData = OTPAuthCodeData(secret: .empty(), accountName: "Test")
+        return .init(data: codeData)
     }
 }
