d328a642c38caeb6ce58f960c1b0d9d6fed31253
Bradley Mackey <bradleymackey@me.com>
2023-09-08 09:52:29 +0100

    TOTPPreviewViewGenerator has no side effects

--- Modules/OTPFeediOS/Tests/OTPFeediOSTests/TOTPPreviewViewGeneratorTests.swift
+++ Modules/OTPFeediOS/Tests/OTPFeediOSTests/TOTPPreviewViewGeneratorTests.swift
@@ -1,6 +1,46 @@
 import Foundation
+import OTPCore
+import OTPFeed
 import OTPFeediOS
+import SwiftUI
 import TestHelpers
 import XCTest
 
-final class TOTPPreviewViewGeneratorTests: XCTestCase {}
+@MainActor
+final class TOTPPreviewViewGeneratorTests: XCTestCase {
+    func test_init_hasNoSideEffects() {
+        let factory = MockTOTPViewFactory()
+        let timer = MockIntervalTimer()
+        _ = makeSUT(factory: factory, timer: timer)
+
+        XCTAssertEqual(factory.makeTOTPViewExecutedCount, 0)
+        XCTAssertEqual(timer.recordedWaitedIntervals, [])
+    }
+}
+
+extension TOTPPreviewViewGeneratorTests {
+    private typealias SUT = TOTPPreviewViewGenerator<MockTOTPViewFactory>
+    private func makeSUT(
+        factory: MockTOTPViewFactory = MockTOTPViewFactory(),
+        clock: EpochClock = EpochClock { 100 },
+        timer: MockIntervalTimer = MockIntervalTimer()
+    ) -> SUT {
+        SUT(viewFactory: factory, clock: clock, timer: timer)
+    }
+
+    private final class MockTOTPViewFactory: TOTPPreviewViewFactory {
+        var makeTOTPViewExecutedCount = 0
+        var makeTOTPViewExecuted: (CodePreviewViewModel, CodeTimerPeriodState, OTPViewBehaviour?) -> Void = { _, _, _ in
+        }
+
+        func makeTOTPView(
+            viewModel: CodePreviewViewModel,
+            periodState: CodeTimerPeriodState,
+            behaviour: OTPViewBehaviour?
+        ) -> some View {
+            makeTOTPViewExecutedCount += 1
+            makeTOTPViewExecuted(viewModel, periodState, behaviour)
+            return Text("Hello, TOTP!")
+        }
+    }
+}
