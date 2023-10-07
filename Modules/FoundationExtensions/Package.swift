397e295afacb6c21e4b2aa9b62e7f5f3e19a94fc
Bradley Mackey <bradleymackey@me.com>
2023-10-07 17:07:40 +0100

    Create FoundationExtensions module

---
+++ Modules/FoundationExtensions/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FoundationExtensions",
+    platforms: [.iOS(.v13), .macOS(.v11)],
+    products: [
+        .library(
+            name: "FoundationExtensions",
+            targets: ["FoundationExtensions"]),
+    ],
+    targets: [
+        .target(
+            name: "FoundationExtensions"),
+        .testTarget(
+            name: "FoundationExtensionsTests",
+            dependencies: ["FoundationExtensions"]),
+    ]
+)
