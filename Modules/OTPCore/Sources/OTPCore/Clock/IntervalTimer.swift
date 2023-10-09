314f878e60dbea6438604f6143086e11f7f318af
Bradley Mackey <bradleymackey@me.com>
2023-10-09 22:05:56 +0100

    Remove ObservableObject requirement from IntervalTimer

--- Modules/OTPCore/Sources/OTPCore/Clock/IntervalTimer.swift
+++ Modules/OTPCore/Sources/OTPCore/Clock/IntervalTimer.swift
@@ -1,7 +1,7 @@
 import Combine
 import Foundation
 
-public protocol IntervalTimer: ObservableObject {
+public protocol IntervalTimer {
     /// Set an expectation to publish once after the specified `time`.
     func wait(for time: Double) -> AnyPublisher<Void, Never>
 }
