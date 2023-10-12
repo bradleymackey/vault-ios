88163a2eb65c242b45057a1e292a579edb868871
Bradley Mackey <bradleymackey@me.com>
2023-10-13 00:06:07 +0100

    Adding specific item uses navigationlink flow

--- Vault/Vault/CodeAddView.swift
+++ Vault/Vault/CodeAddView.swift
@@ -24,25 +24,19 @@ struct CodeAddView: View {
 
     private var itemSelectionSection: some View {
         Section {
-            Button {
-                print("add 2FA code flow")
-            } label: {
+            NavigationLink(destination: Text("Coming Soon")) {
                 FormRow(image: Image(systemName: "qrcode"), color: .blue) {
                     Text("2FA Code")
                 }
             }
 
-            Button {
-                print("add note flow")
-            } label: {
+            NavigationLink(destination: Text("Coming Soon")) {
                 FormRow(image: Image(systemName: "text.alignleft"), color: .blue) {
                     Text("Private Note")
                 }
             }
 
-            Button {
-                print("add crypto seed word flow")
-            } label: {
+            NavigationLink(destination: Text("Coming Soon")) {
                 FormRow(image: Image(systemName: "bitcoinsign"), color: .blue) {
                     Text("Cryptocurrency Seed Phrase")
                 }
