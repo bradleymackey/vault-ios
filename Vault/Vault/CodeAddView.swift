b17a0b04686393c68fd7fe54657df4221c0e13e8
Bradley Mackey <bradleymackey@me.com>
2023-10-12 23:55:00 +0100

    Add possible storage formats to the code add screen

--- Vault/Vault/CodeAddView.swift
+++ Vault/Vault/CodeAddView.swift
@@ -1,7 +1,42 @@
 import SwiftUI
+import VaultUI
 
 struct CodeAddView: View {
     var body: some View {
-        Text("Add Code")
+        Form {
+            itemSelectionSection
+        }
+        .foregroundStyle(.primary)
+    }
+
+    private var itemSelectionSection: some View {
+        Section {
+            Button {
+                print("add 2FA code flow")
+            } label: {
+                FormRow(image: Image(systemName: "qrcode"), color: .blue) {
+                    Text("2FA Code")
+                }
+            }
+
+            Button {
+                print("add note flow")
+            } label: {
+                FormRow(image: Image(systemName: "text.alignleft"), color: .blue) {
+                    Text("Private Note")
+                }
+            }
+
+            Button {
+                print("add crypto seed word flow")
+            } label: {
+                FormRow(image: Image(systemName: "bitcoinsign"), color: .blue) {
+                    Text("Cryptocurrency Seed Phrase")
+                }
+            }
+        } footer: {
+            Text("Store a new item securely on your device.")
+                .foregroundStyle(.secondary)
+        }
     }
 }
