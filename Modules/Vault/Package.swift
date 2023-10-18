4f083eb6811823120adf7c16921da0173aec510f
Bradley Mackey <bradleymackey@me.com>
2023-10-18 09:06:34 +0100

    Enable upcoming swift settings in new vault package

--- Modules/Vault/Package.swift
+++ Modules/Vault/Package.swift
@@ -1,23 +1,32 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+let swiftSettings: [SwiftSetting] = [
+    .enableUpcomingFeature("ForwardTrailingClosures"),
+    .enableUpcomingFeature("BareSlashRegexLiterals"),
+    .enableUpcomingFeature("ConciseMagicFile"),
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("StrictConcurrency"),
+]
 
 let package = Package(
     name: "Vault",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Vault",
-            targets: ["Vault"]),
+            targets: ["Vault"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Vault"),
+            name: "Vault",
+            swiftSettings: swiftSettings
+        ),
         .testTarget(
             name: "VaultTests",
-            dependencies: ["Vault"]),
+            dependencies: ["Vault"],
+            swiftSettings: swiftSettings
+        ),
     ]
 )
