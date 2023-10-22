40192b0506c73cd8fa62bf241edd37df7ec100fa
Bradley Mackey <bradleymackey@me.com>
2023-10-22 16:53:24 +0100

    Make more properties private in SecureNoteDetailViewModel

--- Vault/Sources/VaultFeed/Presentation/SecureNote/SecureNoteDetailViewModel.swift
+++ Vault/Sources/VaultFeed/Presentation/SecureNote/SecureNoteDetailViewModel.swift
@@ -4,9 +4,8 @@ import VaultCore
 
 @MainActor
 public final class SecureNoteDetailViewModel {
-    public let storedNote: SecureNote
-    public let storedMetadata: StoredVaultItem.Metadata
-
+    private let storedNote: SecureNote
+    private let storedMetadata: StoredVaultItem.Metadata
     private let editor: any SecureNoteDetailEditor
 
     public init(storedNote: SecureNote, storedMetadata: StoredVaultItem.Metadata, editor: any SecureNoteDetailEditor) {
