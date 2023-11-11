6cb8cd8e02d6e40646c61286fa508d6429500b82
Bradley Mackey <bradleymackey@me.com>
2023-11-11 12:06:27 +0000

    VaultFeedDetailEditorAdapter holds impls for notes and secure notes

--- Vault/Tests/VaultFeedTests/Presentation/VaultFeedDetailEditorAdapterTests.swift
+++ Vault/Tests/VaultFeedTests/Presentation/VaultFeedDetailEditorAdapterTests.swift
@@ -11,7 +11,7 @@ final class VaultFeedDetailEditorAdapterTests: XCTestCase {
         XCTAssertTrue(feed.calls.isEmpty)
     }
 
-    func test_update_translatesCodeDataForCall() async throws {
+    func test_updateCode_translatesCodeDataForCall() async throws {
         let feed = MockVaultFeed()
         let sut = makeSUT(feed: feed)
 
@@ -45,14 +45,14 @@ final class VaultFeedDetailEditorAdapterTests: XCTestCase {
         await fulfillment(of: [exp])
     }
 
-    func test_update_propagatesFailureOnError() async {
+    func test_updateCode_propagatesFailureOnError() async {
         let feed = FailingVaultFeed()
         let sut = makeSUT(feed: feed)
 
         await XCTAssertThrowsError(try await sut.update(id: UUID(), item: uniqueCode(), edits: .init()))
     }
 
-    func test_delete_deletesFromFeed() async throws {
+    func test_deleteCode_deletesFromFeed() async throws {
         let feed = MockVaultFeed()
         let sut = makeSUT(feed: feed)
 
@@ -69,11 +69,72 @@ final class VaultFeedDetailEditorAdapterTests: XCTestCase {
         await fulfillment(of: [exp])
     }
 
-    func test_delete_propagatesFailureOnError() async {
+    func test_deleteCode_propagatesFailureOnError() async {
         let feed = FailingVaultFeed()
         let sut = makeSUT(feed: feed)
 
         await XCTAssertThrowsError(try await sut.deleteCode(id: UUID()))
+    }
+
+    func test_updateNote_translatesCodeDataForCall() async throws {
+        let feed = MockVaultFeed()
+        let sut = makeSUT(feed: feed)
+
+        var note = anyStoredNote()
+        note.title = "old title"
+        note.contents = "old contents"
+        var item = uniqueVaultItem(item: .secureNote(note))
+        item.metadata.userDescription = "old description"
+
+        let edits = SecureNoteDetailEdits(description: "new description", title: "new title", contents: "new contents")
+
+        let exp = expectation(description: "Wait for update")
+        feed.updateCalled = { _, data in
+            XCTAssertEqual(data.userDescription, "new description")
+            switch data.item {
+            case let .secureNote(note):
+                XCTAssertEqual(note.title, "new title")
+                XCTAssertEqual(note.contents, "new contents")
+            default:
+                XCTFail("invalid kind")
+            }
+            exp.fulfill()
+        }
+
+        try await sut.update(id: item.metadata.id, item: note, edits: edits)
+
+        await fulfillment(of: [exp])
+    }
+
+    func test_updateNote_propagatesFailureOnError() async {
+        let feed = FailingVaultFeed()
+        let sut = makeSUT(feed: feed)
+
+        await XCTAssertThrowsError(try await sut.update(id: UUID(), item: anyStoredNote(), edits: .init()))
+    }
+
+    func test_deleteNote_deletesFromFeed() async throws {
+        let feed = MockVaultFeed()
+        let sut = makeSUT(feed: feed)
+
+        let id = UUID()
+
+        let exp = expectation(description: "Wait for delete")
+        feed.deleteCalled = { actualID in
+            XCTAssertEqual(id, actualID)
+            exp.fulfill()
+        }
+
+        try await sut.deleteNote(id: id)
+
+        await fulfillment(of: [exp])
+    }
+
+    func test_deleteNote_propagatesFailureOnError() async {
+        let feed = FailingVaultFeed()
+        let sut = makeSUT(feed: feed)
+
+        await XCTAssertThrowsError(try await sut.deleteNote(id: UUID()))
     }
 }
 
