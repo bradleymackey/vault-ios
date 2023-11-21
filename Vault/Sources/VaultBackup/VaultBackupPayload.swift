0d81623c98c3de75d38bec44b0a1ccb34ba1cde4
Bradley Mackey <bradleymackey@me.com>
2023-11-21 10:46:02 +0000

    Add VaultBackupPayload

---
+++ Vault/Sources/VaultBackup/VaultBackupPayload.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+/// A full backup of the user's vault.
+///
+/// This is currently a "version 1" vault backup. It must be maintained for future compatibility in the event that
+/// this structure is changed in the future.
+public struct VaultBackupPayload: Codable {
+    /// Version of this backup, determined when the backup was initially created.
+    ///
+    /// Determines the decoding structure. This allows for future breaking changes.
+    public var version: VaultBackupVersion
+    /// The date that the backup was created.
+    public var created: Date
+    /// Custom user provided desciption attached to the backup.
+    public var userDescription: String
+    /// The individual items from the vault.
+    public var items: [VaultBackupItem]
+}
+
+// MARK: - Item
+
+public struct VaultBackupItem: Codable {
+    /// The underlying ID of the vault item in the application.
+    public var id: UUID
+    public var createdDate: Date
+    public var updatedDate: Date
+    public var userDescription: String?
+    /// The item's data that is used to reconstruct the item.
+    public var item: Item
+}
+
+extension VaultBackupItem {
+    /// A individual vault item with enough information to reconstruct it.
+    public enum Item: Codable {
+        case otp(OTP)
+        case note(Note)
+    }
+
+    /// A backed up OTP code.
+    public struct OTP: Codable {
+        var secretFormat: String
+        var secretData: Data
+        var authType: String
+        var period: Int?
+        var counter: Int?
+        var algorithm: String
+        var digits: Int
+        var accountName: String
+        var issuer: String?
+    }
+
+    /// A backed up secure note.
+    public struct Note: Codable {
+        var title: String
+        var rawContents: String?
+    }
+}
