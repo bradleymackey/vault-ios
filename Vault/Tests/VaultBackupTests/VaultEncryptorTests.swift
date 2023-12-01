f2877ca75db35f941965827c1b48a566e7e4dc3e
Bradley Mackey <bradleymackey@me.com>
2023-12-01 11:07:24 +0000

    Track VaultEncryptor for memory leaks

--- Vault/Tests/VaultBackupTests/VaultEncryptorTests.swift
+++ Vault/Tests/VaultBackupTests/VaultEncryptorTests.swift
@@ -4,6 +4,7 @@ import XCTest
 
 final class VaultEncryptor {
     init() {}
+
     func encrypt(data: Data) throws -> Data {
         data
     }
@@ -11,10 +12,20 @@ final class VaultEncryptor {
 
 final class VaultEncryptorTests: XCTestCase {
     func test_encrypt_emptyDataStaysEmpty() throws {
-        let sut = VaultEncryptor()
+        let sut = makeSUT()
 
         let result = try sut.encrypt(data: Data())
 
         XCTAssertEqual(result, Data())
+    }
+}
+
+// MARK: - Helpers
+
+extension VaultEncryptorTests {
+    private func makeSUT() -> VaultEncryptor {
+        let sut = VaultEncryptor()
+        trackForMemoryLeaks(sut)
+        return sut
     }
 }
