43fafb473d9fb12f38f1ab4560a9714dcafa846d
Bradley Mackey <bradleymackey@me.com>
2024-01-20 14:35:13 +0000

    Create VaultExportPayload

---
+++ Vault/Sources/VaultBackup/Export/VaultExportPayload.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/// Describes the payload for export. For example to the `VaultDocumentExporter`.
+///
+/// - important: All sensitive data should already be included in the `EncryptedVault`.
+/// Any other fields here should not contain senstive data.
+///
+/// This payload is export-only, as only the `encryptedVault` is required to be
+/// decrypted, parsed and imported.
+/// The other fields don't need importing.
+struct VaultExportPayload {
+    var encryptedVault: EncryptedVault
+    var userDescription: String
+    var created: Date
+}
