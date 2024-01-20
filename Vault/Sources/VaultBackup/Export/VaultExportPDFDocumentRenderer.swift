614e579f365fa02a63776c36382547d5ec8bddb6
Bradley Mackey <bradleymackey@me.com>
2024-01-20 14:45:05 +0000

    Create VaultExportPDFDocumentRenderer

---
+++ Vault/Sources/VaultBackup/Export/VaultExportPDFDocumentRenderer.swift
@@ -0,0 +1,11 @@
+import CryptoDocumentExporter
+import Foundation
+import PDFKit
+
+public struct VaultExportPDFDocumentRenderer: PDFDocumentRenderer {
+    public typealias Document = VaultExportPayload
+
+    public func render(document _: VaultExportPayload) -> PDFDocument? {
+        PDFDocument()
+    }
+}
