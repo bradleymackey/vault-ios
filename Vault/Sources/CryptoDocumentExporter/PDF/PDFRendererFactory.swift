0dcf84c6d1c6503105bdb865e1043343f0cb7b9c
Bradley Mackey <bradleymackey@me.com>
2024-01-23 12:25:38 +0000

    Use margins from Document Size

--- Vault/Sources/CryptoDocumentExporter/PDF/PDFRendererFactory.swift
+++ Vault/Sources/CryptoDocumentExporter/PDF/PDFRendererFactory.swift
@@ -4,5 +4,13 @@ import UIKit
 /// Creates a rendering context where PDFs will be drawn onto.
 @Spyable
 public protocol PDFRendererFactory {
+    var size: any PDFDocumentSize { get }
     func makeRenderer() -> UIGraphicsPDFRenderer
+}
+
+extension PDFRendererFactory {
+    public func makeRenderer() -> UIGraphicsPDFRenderer {
+        let size = size.pointSize()
+        return UIGraphicsPDFRenderer(bounds: .init(origin: .zero, size: .init(width: size.width, height: size.height)))
+    }
 }
