5b903db81b3c3a10567e45801c038ff41fc723b0
Bradley Mackey <bradleymackey@me.com>
2024-04-14 12:56:20 +0100

    Remove Combine from PendingValue (#57)
    - Convert implementation of PendingValue to use an
    `AsyncThrowingStream`, removing the dependency on Combine.
    - Add unit tests for `isWaiting`
    - Check for memory leaks in the unit tests for PendingValue

--- Vault/Sources/FoundationExtensions/PendingValue.swift
+++ Vault/Sources/FoundationExtensions/PendingValue.swift
@@ -1,12 +1,10 @@
-import Combine
 import Foundation
 
 /// Asynchronously return a value when a signal is triggered.
 public actor PendingValue<Output> {
-    /// A one-shot publisher, on which we check it's completion.
-    private var valuePipeline: PassthroughSubject<Output, any Error>?
-    /// Internal pipeline listener.
-    private var pipelineListener: AnyCancellable?
+    /// The stream that outputs the value internally within `awaitValue`.
+    private var streamContinuation: AsyncThrowingStream<Output, any Error>.Continuation?
+
     /// Last remembered value, used if the value is fulfilled before
     /// we await the given value.
     private var lastValue: Result<Output, any Error>?
@@ -14,78 +12,64 @@ public actor PendingValue<Output> {
     public init() {}
 }
 
-// MARK: - Helpers
-
-extension PendingValue {
-    /// Returns `true` if waiting on `waitToProduce` at this moment.
-    public var isWaiting: Bool {
-        valuePipeline != nil
-    }
-
-    /// Cancel the `waitToProduce`, causing it to throw a `CancellationError`.
-    public func cancel() {
-        valuePipeline?.send(completion: .failure(CancellationError()))
-    }
-}
-
 // MARK: - API
 
 extension PendingValue {
+    /// Whether a value is currently being waited for via `awaitValue`.
+    public var isWaiting: Bool {
+        streamContinuation != nil
+    }
+
+    /// Cancel the `awaitValue`, causing it to throw a `CancellationError`.
+    public func cancel() {
+        streamContinuation?.finish()
+    }
+
     /// If pending, produces the value, causing `waitToProduce` to return it's value immediately.
     public func fulfill(_ value: Output) {
         lastValue = .success(value)
-        valuePipeline?.send(value)
-        valuePipeline?.send(completion: .finished)
+        streamContinuation?.yield(value)
+        streamContinuation?.finish()
     }
 
     /// Produces an error to cause `waitToForValue` to throw.
     public func reject(error: any Error) {
         lastValue = .failure(error)
-        valuePipeline?.send(completion: .failure(error))
+        streamContinuation?.finish(throwing: error)
     }
 
     public struct AlreadyWaitingError: Error {}
 
     /// Wait for the production of the target value, cancelling on a Task cancellation.
-    /// Yields the value when `produceNow` is called, but waits until that moment.
+    /// Yields the value when `fulfill` or `reject` is called, but waits until that moment.
     ///
-    /// - throws: `CancellationError` if cancelled, `ProductionError.alreadyWaiting` if already waiting.
+    /// - throws: `CancellationError` if cancelled, `AlreadyWaitingError` if already waiting.
     public func awaitValue() async throws -> Output {
         if isWaiting {
             throw AlreadyWaitingError()
         }
+
         // If there's a pending value, get it.
         if let existing = lastValue {
             defer { lastValue = nil }
             return try existing.get()
         }
+
         // Otherwise, asynchronously wait for the production of the value.
         defer {
-            pipelineListener?.cancel()
-            valuePipeline = nil
+            streamContinuation = nil
         }
-        valuePipeline = .init()
-        return try await withTaskCancellationHandler {
-            try await withCheckedThrowingContinuation { cont in
-                guard let valuePipeline else {
-                    return cont.resume(throwing: CancellationError())
-                }
-                self.pipelineListener = valuePipeline.sink { completed in
-                    switch completed {
-                    case let .failure(error):
-                        cont.resume(throwing: error)
-                    case .finished:
-                        // already resolved with value in `recieveValue`.
-                        return
-                    }
-                } receiveValue: { value in
-                    cont.resume(returning: value)
-                }
-            }
-        } onCancel: {
-            Task {
-                await valuePipeline?.send(completion: .failure(CancellationError()))
-            }
+
+        let stream = AsyncThrowingStream { continuation in
+            self.streamContinuation = continuation
+        }
+
+        if let first = try await stream.first {
+            return first
+        } else {
+            // The stream will only exit without a value in the case that it's been cancelled.
+            // Propagate the cancellation.
+            throw CancellationError()
         }
     }
 }
@@ -96,5 +80,16 @@ extension PendingValue where Output == Void {
     /// If pending, produces the value, causing `waitToProduce` to return it's value immediately.
     public func fulfill() {
         fulfill(())
+    }
+}
+
+extension AsyncThrowingStream {
+    fileprivate var first: Element? {
+        get async throws {
+            for try await value in self {
+                return value
+            }
+            return nil
+        }
     }
 }
--- Vault/Tests/FoundationExtensionsTests/PendingValueTests.swift
+++ Vault/Tests/FoundationExtensionsTests/PendingValueTests.swift
@@ -1,5 +1,6 @@
 import Foundation
 import FoundationExtensions
+import TestHelpers
 import XCTest
 
 final class PendingValueTests: XCTestCase {
@@ -8,29 +9,13 @@ final class PendingValueTests: XCTestCase {
         case testCase2
     }
 
-    func test_fulfill_unsuspendsAwait() async throws {
-        let producer = PendingValue<Int>()
-        let exp = expectation(description: "waiting for value")
-        Task {
-            let result = try await producer.awaitValue()
-            XCTAssertEqual(result, 42)
-            exp.fulfill()
-        }
-        Task {
-            try await Task.sleep(for: .milliseconds(100))
-            await producer.fulfill(42)
-        }
-
-        await fulfillment(of: [exp], timeout: 1.0)
-    }
-
     func test_reject_unsuspendsAwait() async throws {
-        let producer = PendingValue<Int>()
+        let sut = makeSUT()
         let exp = expectation(description: "waiting for value")
         Task {
             defer { exp.fulfill() }
             do {
-                _ = try await producer.awaitValue()
+                _ = try await sut.awaitValue()
                 XCTFail()
             } catch {
                 XCTAssert(error is TestError)
@@ -38,19 +23,57 @@ final class PendingValueTests: XCTestCase {
         }
         Task {
             try await Task.sleep(for: .milliseconds(100))
-            await producer.reject(error: TestError.testCase)
+            await sut.reject(error: TestError.testCase)
+        }
+
+        await fulfillment(of: [exp], timeout: 1.0)
+    }
+
+    func test_awaitValue_throwsCancellationErrorIfCancelled() async throws {
+        let sut = makeSUT()
+
+        let exp = expectation(description: "waiting for value")
+        let handle = Task {
+            do {
+                _ = try await sut.awaitValue()
+                XCTFail("Unexpected path")
+            } catch is CancellationError {
+                // good
+            } catch {
+                XCTFail("Unexpected path")
+            }
+            exp.fulfill()
+        }
+
+        handle.cancel()
+
+        await fulfillment(of: [exp], timeout: 1.0)
+    }
+
+    func test_fulfill_unsuspendsAwait() async throws {
+        let sut = makeSUT()
+
+        let exp = expectation(description: "waiting for value")
+        Task {
+            let result = try await sut.awaitValue()
+            XCTAssertEqual(result, 42)
+            exp.fulfill()
+        }
+        Task {
+            try await Task.sleep(for: .milliseconds(100))
+            await sut.fulfill(42)
         }
 
         await fulfillment(of: [exp], timeout: 1.0)
     }
 
     func test_fulfill_beforeAwaitingReturnsInitialValue() async throws {
-        let producer = PendingValue<Int>()
-        await producer.fulfill(42)
+        let sut = makeSUT()
+        await sut.fulfill(42)
         let exp = expectation(description: "Produces")
         Task {
             defer { exp.fulfill() }
-            let value = try await producer.awaitValue()
+            let value = try await sut.awaitValue()
             XCTAssertEqual(value, 42)
         }
 
@@ -58,15 +81,15 @@ final class PendingValueTests: XCTestCase {
     }
 
     func test_fulfill_remembersMostRecentValueOnly() async throws {
-        let producer = PendingValue<Int>()
-        await producer.fulfill(42)
-        await producer.fulfill(43)
-        await producer.fulfill(44)
+        let sut = makeSUT()
+        await sut.fulfill(42)
+        await sut.fulfill(43)
+        await sut.fulfill(44)
 
         let exp = expectation(description: "Produces")
         Task {
             defer { exp.fulfill() }
-            let value = try await producer.awaitValue()
+            let value = try await sut.awaitValue()
             // The most recent value.
             XCTAssertEqual(value, 44)
         }
@@ -75,14 +98,14 @@ final class PendingValueTests: XCTestCase {
     }
 
     func test_reject_beforeAwaitResolvesWithInitialError() async throws {
-        let producer = PendingValue<Int>()
-        await producer.reject(error: TestError.testCase)
+        let sut = makeSUT()
+        await sut.reject(error: TestError.testCase)
 
         let exp = expectation(description: "Produces")
         Task {
             defer { exp.fulfill() }
             do {
-                _ = try await producer.awaitValue()
+                _ = try await sut.awaitValue()
                 XCTFail("Error expected to be thrown.")
             } catch TestError.testCase {
                 XCTAssert(true)
@@ -95,15 +118,15 @@ final class PendingValueTests: XCTestCase {
     }
 
     func test_reject_resolvesWithMostRecentError() async throws {
-        let producer = PendingValue<Int>()
-        await producer.reject(error: TestError.testCase)
-        await producer.reject(error: TestError.testCase2)
+        let sut = makeSUT()
+        await sut.reject(error: TestError.testCase)
+        await sut.reject(error: TestError.testCase2)
 
         let exp = expectation(description: "Produces")
         Task {
             defer { exp.fulfill() }
             do {
-                _ = try await producer.awaitValue()
+                _ = try await sut.awaitValue()
                 XCTFail()
             } catch TestError.testCase2 {
                 XCTAssert(true)
@@ -113,5 +136,123 @@ final class PendingValueTests: XCTestCase {
         }
 
         await fulfillment(of: [exp], timeout: 1.0)
+    }
+
+    func test_isWaiting_initiallyFalse() async {
+        let sut = makeSUT()
+
+        let isWaiting = await sut.isWaiting
+        XCTAssertFalse(isWaiting)
+    }
+
+    func test_isWaiting_trueWhenWaiting() async {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for task")
+
+        Task {
+            _ = try await sut.awaitValue()
+        }
+
+        Task {
+            try await allowTasksToProgress()
+            exp.fulfill()
+        }
+
+        await fulfillment(of: [exp])
+
+        let isWaiting = await sut.isWaiting
+        XCTAssertTrue(isWaiting)
+
+        await sut.cancel()
+    }
+
+    func test_isWaiting_falseWhenFulfilled() async throws {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for tasks")
+        exp.expectedFulfillmentCount = 2
+
+        Task {
+            _ = try await sut.awaitValue()
+            exp.fulfill()
+        }
+
+        Task {
+            try await allowTasksToProgress()
+            await sut.fulfill(42)
+            try await allowTasksToProgress()
+            exp.fulfill()
+        }
+
+        await fulfillment(of: [exp])
+
+        let isWaiting = await sut.isWaiting
+        XCTAssertFalse(isWaiting)
+    }
+
+    func test_isWaiting_falseWhenRejected() async throws {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for tasks")
+        exp.expectedFulfillmentCount = 2
+
+        Task {
+            _ = try? await sut.awaitValue()
+            exp.fulfill()
+        }
+
+        Task {
+            try await allowTasksToProgress()
+            await sut.reject(error: anyError())
+            try await allowTasksToProgress()
+            exp.fulfill()
+        }
+
+        await fulfillment(of: [exp])
+
+        let isWaiting = await sut.isWaiting
+        XCTAssertFalse(isWaiting)
+    }
+
+    func test_isWaiting_falseWhenCancelled() async throws {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for tasks")
+        exp.expectedFulfillmentCount = 2
+
+        Task {
+            _ = try? await sut.awaitValue()
+            exp.fulfill()
+        }
+
+        Task {
+            try await allowTasksToProgress()
+            await sut.cancel()
+            try await allowTasksToProgress()
+            exp.fulfill()
+        }
+
+        await fulfillment(of: [exp])
+
+        let isWaiting = await sut.isWaiting
+        XCTAssertFalse(isWaiting)
+    }
+}
+
+// MARK: Helpers
+
+extension PendingValueTests {
+    private typealias SUT = PendingValue<Int>
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> SUT {
+        let sut = PendingValue<Int>()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func anyError() -> any Error {
+        struct SomeError: Error {}
+        return SomeError()
+    }
+
+    private func allowTasksToProgress() async throws {
+        try await Task.sleep(for: .milliseconds(200))
     }
 }
