174c09b126469598612c17378512a302ac0a8ac0
Bradley Mackey <bradleymackey@me.com>
2024-04-14 10:51:44 +0100

    Use AsyncThrowingStream.Continuation for pipeline of events in PendingValue

--- Vault/Sources/FoundationExtensions/PendingValue.swift
+++ Vault/Sources/FoundationExtensions/PendingValue.swift
@@ -3,10 +3,10 @@ import Foundation
 
 /// Asynchronously return a value when a signal is triggered.
 public actor PendingValue<Output> {
-    private var stream: AsyncThrowingStream<Output, any Error>?
-    private var valuePipeline: ValuePipeline = .init()
-    /// Internal pipeline listener.
-    private var pipelineListener: AnyCancellable?
+    public private(set) var isWaiting = false
+
+    private var continuation: AsyncThrowingStream<Output, any Error>.Continuation?
+
     /// Last remembered value, used if the value is fulfilled before
     /// we await the given value.
     private var lastValue: Result<Output, any Error>?
@@ -17,36 +17,30 @@ public actor PendingValue<Output> {
 // MARK: - API
 
 extension PendingValue {
-    /// Returns `true` if waiting on `waitToProduce` at this moment.
-    public var isWaiting: Bool {
-        stream != nil
-    }
-
-    /// Cancel the `waitToProduce`, causing it to throw a `CancellationError`.
+    /// Cancel the `awaitValue`, causing it to throw a `CancellationError`.
     public func cancel() {
-        valuePipeline.cancel()
+        continuation?.finish()
     }
 
     /// If pending, produces the value, causing `waitToProduce` to return it's value immediately.
     public func fulfill(_ value: Output) {
         lastValue = .success(value)
-        valuePipeline.finish(result: .success(value))
+        continuation?.yield(value)
+        continuation?.finish()
     }
 
     /// Produces an error to cause `waitToForValue` to throw.
     public func reject(error: any Error) {
         lastValue = .failure(error)
-        valuePipeline.finish(result: .failure(error))
+        continuation?.finish(throwing: error)
     }
 
     public struct AlreadyWaitingError: Error {}
 
-    public struct MissingValueError: Error {}
-
     /// Wait for the production of the target value, cancelling on a Task cancellation.
-    /// Yields the value when `produceNow` is called, but waits until that moment.
+    /// Yields the value when `fulfill` or `reject` is called, but waits until that moment.
     ///
-    /// - throws: `CancellationError` if cancelled, `ProductionError.alreadyWaiting` if already waiting.
+    /// - throws: `CancellationError` if cancelled, `AlreadyWaitingError` if already waiting.
     public func awaitValue() async throws -> Output {
         if isWaiting {
             throw AlreadyWaitingError()
@@ -59,60 +53,20 @@ extension PendingValue {
         }
 
         // Otherwise, asynchronously wait for the production of the value.
+        isWaiting = true
         defer {
-            pipelineListener?.cancel()
-            stream = nil
+            continuation = nil
+            isWaiting = false
         }
 
-        var listener: AnyCancellable?
-        defer { listener?.cancel() }
-        stream = AsyncThrowingStream { continuation in
-            listener = valuePipeline.publisher().sink { completion in
-                switch completion {
-                case let .failure(error):
-                    continuation.finish(throwing: error)
-                case .finished:
-                    continuation.finish()
-                }
-            } receiveValue: { value in
-                continuation.yield(value)
-            }
+        let stream = AsyncThrowingStream { continuation in
+            self.continuation = continuation
         }
 
-        for try await value in stream! {
+        for try await value in stream {
             return value
         }
-        throw MissingValueError()
-    }
-}
-
-// MARK: - Internal
-
-extension PendingValue {
-    private struct ValuePipeline {
-        private var subject: PassthroughSubject<Output, any Error>
-
-        init() {
-            subject = .init()
-        }
-
-        func cancel() {
-            subject.send(completion: .failure(CancellationError()))
-        }
-
-        func finish(result: Result<Output, any Error>) {
-            switch result {
-            case let .success(value):
-                subject.send(value)
-                subject.send(completion: .finished)
-            case let .failure(err):
-                subject.send(completion: .failure(err))
-            }
-        }
-
-        func publisher() -> AnyPublisher<Output, any Error> {
-            subject.eraseToAnyPublisher()
-        }
+        throw CancellationError()
     }
 }
 
