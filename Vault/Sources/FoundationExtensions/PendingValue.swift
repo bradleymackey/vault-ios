fe79fcf3d80927bc257e2b5fb307c575fc1317b6
Bradley Mackey <bradleymackey@me.com>
2024-04-14 12:45:38 +0100

    Further improve readability of PendingValue's impl

--- Vault/Sources/FoundationExtensions/PendingValue.swift
+++ Vault/Sources/FoundationExtensions/PendingValue.swift
@@ -2,9 +2,8 @@ import Foundation
 
 /// Asynchronously return a value when a signal is triggered.
 public actor PendingValue<Output> {
-    public private(set) var isWaiting = false
-
-    private var continuation: AsyncThrowingStream<Output, any Error>.Continuation?
+    /// The stream that outputs the value internally within `awaitValue`.
+    private var streamContinuation: AsyncThrowingStream<Output, any Error>.Continuation?
 
     /// Last remembered value, used if the value is fulfilled before
     /// we await the given value.
@@ -16,22 +15,27 @@ public actor PendingValue<Output> {
 // MARK: - API
 
 extension PendingValue {
+    /// Whether a value is currently being waited for via `awaitValue`.
+    public var isWaiting: Bool {
+        streamContinuation != nil
+    }
+
     /// Cancel the `awaitValue`, causing it to throw a `CancellationError`.
     public func cancel() {
-        continuation?.finish()
+        streamContinuation?.finish()
     }
 
     /// If pending, produces the value, causing `waitToProduce` to return it's value immediately.
     public func fulfill(_ value: Output) {
         lastValue = .success(value)
-        continuation?.yield(value)
-        continuation?.finish()
+        streamContinuation?.yield(value)
+        streamContinuation?.finish()
     }
 
     /// Produces an error to cause `waitToForValue` to throw.
     public func reject(error: any Error) {
         lastValue = .failure(error)
-        continuation?.finish(throwing: error)
+        streamContinuation?.finish(throwing: error)
     }
 
     public struct AlreadyWaitingError: Error {}
@@ -52,20 +56,21 @@ extension PendingValue {
         }
 
         // Otherwise, asynchronously wait for the production of the value.
-        isWaiting = true
         defer {
-            continuation = nil
-            isWaiting = false
+            streamContinuation = nil
         }
 
         let stream = AsyncThrowingStream { continuation in
-            self.continuation = continuation
+            self.streamContinuation = continuation
         }
 
-        for try await value in stream {
-            return value
+        if let first = try await stream.first {
+            return first
+        } else {
+            // The stream will only exit without a value in the case that it's been cancelled.
+            // Propagate the cancellation.
+            throw CancellationError()
         }
-        throw CancellationError()
     }
 }
 
@@ -75,5 +80,16 @@ extension PendingValue where Output == Void {
     /// If pending, produces the value, causing `waitToProduce` to return it's value immediately.
     public func fulfill() {
         fulfill(())
+    }
+}
+
+extension AsyncThrowingStream {
+    fileprivate var first: Element? {
+        get async throws {
+            for try await value in self {
+                return value
+            }
+            return nil
+        }
     }
 }
