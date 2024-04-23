72e028e494053b5f94dfbb46cd1f943b0e8fdf0c
Bradley Mackey <bradleymackey@me.com>
2024-04-23 22:03:39 +0100

    Add cancel button to vault search bar

--- Vault/Sources/VaultiOS/General/SearchTextField.swift
+++ Vault/Sources/VaultiOS/General/SearchTextField.swift
@@ -5,13 +5,34 @@ struct SearchTextField: View {
     var title: String
     @Binding var text: String
 
+    @FocusState private var isFocused: Bool
+    let radius = 8.0
+
     var body: some View {
-        TextField(title, text: $text)
-            .textFieldStyle(.plain)
-            .submitLabel(.done)
-            .padding(8)
-            .background(backgroundColor)
-            .clipShape(RoundedRectangle(cornerRadius: 8))
+        HStack(spacing: 8) {
+            TextField(title, text: $text)
+                .textFieldStyle(.plain)
+                .submitLabel(.done)
+                .focused($isFocused)
+                .padding(radius)
+                .background(backgroundColor)
+                .clipShape(RoundedRectangle(cornerRadius: radius))
+
+            if text != "" {
+                cancelButton
+            }
+        }
+        .animation(.snappy, value: text)
+    }
+
+    private var cancelButton: some View {
+        Button {
+            text = ""
+            isFocused = false
+        } label: {
+            Text("Cancel")
+                .foregroundStyle(.primary, .secondary)
+        }
     }
 
     private var backgroundColor: Color {
