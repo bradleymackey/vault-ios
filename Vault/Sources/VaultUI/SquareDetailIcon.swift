0fd31a4ca112f95d315d09e8074e3f076f25b5b5
Bradley Mackey <bradleymackey@me.com>
2024-05-04 18:48:20 +0100

    Create SquareDetailIcon

---
+++ Vault/Sources/VaultUI/SquareDetailIcon.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+/// An icon with a title and detail text below.
+public struct SquareDetailIcon: View {
+    public var image: Image
+    public var color: Color
+    public var title: String
+    public var subtitle: String
+    
+    public init(image: Image, color: Color = .blue, title: String, subtitle: String) {
+        self.image = image.renderingMode(.template)
+        self.color = color
+        self.title = title
+        self.subtitle = subtitle
+    }
+    
+    public var body: some View {
+        VStack(alignment: .center, spacing: 12) {
+            ZStack {
+                color
+                image
+                    .font(.title)
+                    .foregroundStyle(.white)
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .clipShape(RoundedRectangle(cornerRadius: 25.0))
+            .frame(maxWidth: 100)
+            
+            labels
+        }
+    }
+    
+    private var labels: some View {
+        VStack(alignment: .center) {
+            Text(title)
+                .font(.headline)
+                .foregroundStyle(.primary)
+            Text(subtitle)
+                .font(.footnote)
+                .fontDesign(.monospaced)
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    SquareDetailIcon(
+        image: Image(systemName: "person"),
+        title: "Title",
+        subtitle: "Subtitle"
+    )
+}
