bd11feb0861953be8ca7888c455d5921f2e206a0
Bradley Mackey <bradleymackey@me.com>
2024-05-09 23:25:10 +0100

    Use same design for no description and description

--- Vault/Sources/VaultiOS/Feed/Previews/SecureNotePreviewView.swift
+++ Vault/Sources/VaultiOS/Feed/Previews/SecureNotePreviewView.swift
@@ -7,44 +7,29 @@ public struct SecureNotePreviewView: View {
     var behaviour: VaultItemViewBehaviour
 
     public var body: some View {
-        Group {
+        VStack(alignment: .center, spacing: 8) {
+            VStack(alignment: .center, spacing: 4) {
+                Image(systemName: "doc.text.fill")
+                    .font(.title3)
+                Text(viewModel.visibleTitle)
+                    .font(.headline)
+            }
+            .foregroundStyle(.primary)
+            .tint(.primary)
+            .multilineTextAlignment(.center)
+            .layoutPriority(100)
+
             if let description = viewModel.description, description.isNotEmpty {
-                noteWithTitleAndDescriptionBody(description: description)
-            } else {
-                noteWithTitleOnlyBody()
+                descriptionLabel(text: description)
+                    .layoutPriority(99)
+                    .multilineTextAlignment(.leading)
             }
         }
+        .frame(maxHeight: .infinity)
+        .padding(2)
         .shimmering(active: isShimmering)
         .aspectRatio(1, contentMode: .fill)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
-
-    private func noteWithTitleOnlyBody() -> some View {
-        VStack(alignment: .center, spacing: 4) {
-            Image(systemName: "doc.text.fill")
-                .font(.title3)
-            Text(viewModel.visibleTitle)
-                .font(.headline)
-        }
-        .foregroundStyle(.primary)
-        .tint(.primary)
-        .frame(maxHeight: .infinity)
-        .multilineTextAlignment(.center)
-        .padding(2)
-    }
-
-    private func noteWithTitleAndDescriptionBody(description _: String) -> some View {
-        VStack(alignment: .leading, spacing: 4) {
-            titleLabel
-                .layoutPriority(100)
-            if let description = viewModel.description {
-                descriptionLabel(text: description)
-                    .layoutPriority(99)
-            }
-            Spacer()
-        }
-        .multilineTextAlignment(.leading)
-        .padding(2)
     }
 
     private var isShimmering: Bool {
@@ -52,19 +37,6 @@ public struct SecureNotePreviewView: View {
         case .normal: false
         case .editingState: true
         }
-    }
-
-    private var titleLabel: some View {
-        HStack(alignment: .top, spacing: 4) {
-            Image(systemName: "doc.text.fill")
-            Text(viewModel.visibleTitle)
-            Spacer()
-        }
-        .font(.callout.bold())
-        .minimumScaleFactor(0.8)
-        .padding(.vertical, 4)
-        .foregroundStyle(.primary)
-        .tint(.primary)
     }
 
     private func descriptionLabel(text: String) -> some View {
