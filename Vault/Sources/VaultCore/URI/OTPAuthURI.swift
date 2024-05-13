6457bf0e50d9b4832147f7749e38434532ae6f42
Bradley Mackey <bradleymackey@me.com>
2024-05-13 22:09:29 +0100

    Add initial experience for scanning camera, getting result to desintation context

--- Vault/Sources/VaultCore/URI/OTPAuthURI.swift
+++ Vault/Sources/VaultCore/URI/OTPAuthURI.swift
@@ -20,3 +20,9 @@ extension OTPAuthURI {
         case counter
     }
 }
+
+extension OTPAuthURI {
+    public static var exampleCodeString: String {
+        "otpauth://totp/Google:tummy%40funny.com?period=30&digits=6&algorithm=SHA512&secret=5372UEJC"
+    }
+}
