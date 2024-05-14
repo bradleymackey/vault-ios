25cc48ac946aadaff92787d08481fa63c7e38aa1
Bradley Mackey <bradleymackey@me.com>
2024-05-14 13:05:49 +0100

    Support Xcode 15.4, fix some test failures (#121)

--- Vault/Sources/FoundationExtensions/IdentifiableSelf.swift
+++ Vault/Sources/FoundationExtensions/IdentifiableSelf.swift
@@ -1,4 +1,6 @@
-public protocol IdentifiableSelf: Identifiable where Self: Hashable {}
+public protocol IdentifiableSelf: Identifiable where Self: Hashable, ID: Hashable {
+    var id: ID { get }
+}
 
 extension IdentifiableSelf {
     public var id: Self { self }
