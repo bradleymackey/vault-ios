68ee46e37c032454827f4c46827a22ad18cb3bf6
Bradley Mackey <bradleymackey@me.com>
2024-05-16 11:50:46 +0100

    Make placeholder color gray

--- Vault/Sources/VaultFeed/Presentation/VaultItemColor.swift
+++ Vault/Sources/VaultFeed/Presentation/VaultItemColor.swift
@@ -13,7 +13,16 @@ public struct VaultItemColor: Equatable, Hashable {
 }
 
 extension VaultItemColor {
+    /// A sensible placeholder color to use when there's
     public static var `default`: VaultItemColor {
+        .gray
+    }
+
+    public static var gray: VaultItemColor {
+        VaultItemColor(red: 0.5, green: 0.5, blue: 0.5)
+    }
+
+    public static var black: VaultItemColor {
         VaultItemColor(red: 0, green: 0, blue: 0)
     }
 }
