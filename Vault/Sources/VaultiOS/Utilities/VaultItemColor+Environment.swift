c4ec4b77acd21cce3afa9fe497dce5792bddf613
Bradley Mackey <bradleymackey@me.com>
2024-05-16 11:32:30 +0100

    Ensure colorspace is converted correctly

--- Vault/Sources/VaultiOS/Utilities/VaultItemColor+Environment.swift
+++ Vault/Sources/VaultiOS/Utilities/VaultItemColor+Environment.swift
@@ -4,7 +4,10 @@ import VaultFeed
 
 extension VaultItemColor {
     init(uiColor: UIColor) {
-        let ciColor = uiColor.ciColor
+        guard let ciColor = uiColor.ciColorInRGBColorSpace else {
+            self.init(red: 0, green: 0, blue: 0)
+            return
+        }
         self.init(red: ciColor.red, green: ciColor.green, blue: ciColor.blue)
     }
 
@@ -20,5 +23,17 @@ extension VaultItemColor {
 
     var color: Color {
         Color(uiColor: uiColor)
+    }
+}
+
+extension UIColor {
+    fileprivate var ciColorInRGBColorSpace: CIColor? {
+        // Ensure the UIColor is in the RGB color space
+        guard let converted = cgColor
+            .converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)
+        else {
+            return nil
+        }
+        return CIColor(cgColor: converted)
     }
 }
