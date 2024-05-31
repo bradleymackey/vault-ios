a65b1e22d19282a2e11e0c0320d51a019b594dfa
Bradley Mackey <bradleymackey@me.com>
2024-05-31 23:14:45 +0100

    Add create password method to BackupKeyChangeViewModel

--- Vault/Sources/VaultFeed/Presentation/BackupKeyChangeViewModel.swift
+++ Vault/Sources/VaultFeed/Presentation/BackupKeyChangeViewModel.swift
@@ -4,22 +4,31 @@ import VaultBackup
 
 @MainActor
 @Observable
-final class BackupKeyChangeViewModel {
-    enum ExistingPasswordState: Equatable, Hashable {
+public final class BackupKeyChangeViewModel {
+    public enum ExistingPasswordState: Equatable, Hashable {
         case loading
         case hasExistingPassword(BackupPassword)
         case noExistingPassword
         case errorFetching
     }
 
-    private(set) var existingPassword: ExistingPasswordState = .loading
+    public enum NewPasswordState: Equatable, Hashable {
+        case neutral
+        case creating
+        case error
+        case success
+    }
+
+    public var newlyEnteredPassword = ""
+    public private(set) var existingPassword: ExistingPasswordState = .loading
+    public private(set) var newPassword: NewPasswordState = .neutral
     private let store: any BackupPasswordStore
 
-    init(store: any BackupPasswordStore) {
+    public init(store: any BackupPasswordStore) {
         self.store = store
     }
 
-    func loadInitialData() {
+    public func loadInitialData() {
         do {
             if let password = try store.fetchPassword() {
                 existingPassword = .hasExistingPassword(password)
@@ -28,6 +37,21 @@ final class BackupKeyChangeViewModel {
             }
         } catch {
             existingPassword = .errorFetching
+        }
+    }
+
+    public func saveEnteredPassword() async {
+        do {
+            let enteredPassword = newlyEnteredPassword
+            newPassword = .creating
+            let keygenTask = Task.detached(priority: .background) {
+                try BackupPassword.createEncryptionKey(text: enteredPassword)
+            }
+            let createdBackupPassword = try await keygenTask.value
+            try store.set(password: createdBackupPassword)
+            newPassword = .success
+        } catch {
+            newPassword = .error
         }
     }
 }
--- Vault/Sources/VaultFeed/Storage/Backup/BackupPassword.swift
+++ Vault/Sources/VaultFeed/Storage/Backup/BackupPassword.swift
@@ -15,10 +15,7 @@ public struct BackupPassword: Equatable, Hashable {
 
 extension BackupPassword {
     /// Creates an encryption key for the v1 version of an encrypted vault.
-    public static func createV1EncryptionKey(
-        text: String,
-        salt: Data
-    ) async throws -> BackupPassword {
+    public static func createEncryptionKey(text: String) throws -> BackupPassword {
         // FIXME: make sure parameters are applicable to DEBUG and RELEASE builds as appropriate
         // Need some application-layer toggle to swap these parameters out. Debug can be up to 100x slower.
         //
@@ -27,14 +24,9 @@ extension BackupPassword {
         //  - Insecure (for DEBUG)
         //  - Secure (These parameters)
         //  - Highly Secure (Even stronger, like a minute to derive the key?)
+        let salt = Data.random(count: 48)
         let deriver = VaultAppKeyDerivers.V1.fast
-        let key = try await withCheckedThrowingContinuation { continuation in
-            DispatchQueue.global(qos: .userInitiated).async {
-                continuation.resume(with: Result {
-                    try deriver.key(password: Data(text.utf8), salt: salt)
-                })
-            }
-        }
+        let key = try deriver.key(password: Data(text.utf8), salt: salt)
         return BackupPassword(key: key, salt: salt)
     }
 }
