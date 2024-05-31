a219f2942493cf42a45ca6988e56071e0f8278cb
Bradley Mackey <bradleymackey@me.com>
2024-05-31 07:34:08 +0100

    Implement KeychainBackupPasswordStore

--- Vault/Sources/VaultFeed/Storage/Backup/KeychainBackupPasswordStore.swift
+++ Vault/Sources/VaultFeed/Storage/Backup/KeychainBackupPasswordStore.swift
@@ -8,11 +8,31 @@ final class KeychainBackupPasswordStore: BackupPasswordStore {
     }
 
     func fetchPassword() throws -> BackupPassword? {
-        // TODO: implement
-        nil
+        let key = try fetchFromKeychainIfPresent(key: KeychainKey.key)
+        let salt = try fetchFromKeychainIfPresent(key: KeychainKey.salt)
+        guard let key, let salt else { return nil }
+        return BackupPassword(key: key, salt: salt)
     }
 
-    func set(password _: BackupPassword) throws {
-        // TODO: implement
+    func set(password: BackupPassword) throws {
+        try keychain.set(password.key, forKey: KeychainKey.key)
+        try keychain.set(password.salt, forKey: KeychainKey.salt)
+    }
+
+    private func fetchFromKeychainIfPresent(key: String) throws -> Data? {
+        if try keychain.hasItem(forKey: key) {
+            return try keychain.data(forKey: key)
+        } else {
+            return nil
+        }
+    }
+}
+
+// MARK: - Keys
+
+extension KeychainBackupPasswordStore {
+    private enum KeychainKey {
+        static let key = "vault-backup-password-key-v1"
+        static let salt = "vault-backup-password-salt-v1"
     }
 }
