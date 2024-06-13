c439eafdf6d73a699ffc2ec981042e5cabdb0b16
Bradley Mackey <bradleymackey@me.com>
2024-06-13 11:47:48 +0100

    Remove explicit history arg

--- Vault/Sources/CryptoDocumentExporter/PDF/PDFDocumentRenderer.swift
+++ Vault/Sources/CryptoDocumentExporter/PDF/PDFDocumentRenderer.swift
@@ -1,7 +1,7 @@
 import Foundation
 import PDFKit
 
-/// @mockable(typealias: Document = DataBlockDocument; history: render = true)
+/// @mockable(typealias: Document = DataBlockDocument)
 public protocol PDFDocumentRenderer<Document> {
     associatedtype Document
     func render(document: Document) throws -> PDFDocument
--- Vault/Sources/VaultFeed/Storage/Backup/BackupPasswordStore.swift
+++ Vault/Sources/VaultFeed/Storage/Backup/BackupPasswordStore.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Storage for the password used to encrypt backups.
 ///
-/// @mockable(history: fetchPassword = true; history: set = true)
+/// @mockable
 public protocol BackupPasswordStore: Observable {
     func fetchPassword() throws -> BackupPassword?
     func set(password: BackupPassword) throws
