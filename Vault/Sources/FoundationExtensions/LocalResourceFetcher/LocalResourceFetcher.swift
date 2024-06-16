e5a42f9bfbaf9e274a28d7108c2bc5c36eab3775
Bradley Mackey <bradleymackey@me.com>
2024-06-16 17:14:50 +0100

    LocalResourceFetcher must be Sendable

--- Vault/Sources/FoundationExtensions/LocalResourceFetcher/LocalResourceFetcher.swift
+++ Vault/Sources/FoundationExtensions/LocalResourceFetcher/LocalResourceFetcher.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol LocalResourceFetcher {
+public protocol LocalResourceFetcher: Sendable {
     /// Fetches a local resource, returning the data.
     func fetchLocalResource(at url: URL) async throws -> Data
     /// Fetches a local resource in the context of the given `Bundle`.
