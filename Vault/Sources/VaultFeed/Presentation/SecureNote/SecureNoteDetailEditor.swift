5b64d81c2f8d9c84c6be44448780f0ceefd52a0e
Bradley Mackey <bradleymackey@me.com>
2024-06-16 16:45:54 +0100

    SecureNoteDetailEditor must be isolated to the MainActor

--- Vault/Sources/VaultFeed/Presentation/SecureNote/SecureNoteDetailEditor.swift
+++ Vault/Sources/VaultFeed/Presentation/SecureNote/SecureNoteDetailEditor.swift
@@ -2,6 +2,7 @@ import Foundation
 import VaultCore
 
 /// @mockable
+@MainActor
 public protocol SecureNoteDetailEditor {
     func createNote(initialEdits: SecureNoteDetailEdits) async throws
     func updateNote(id: UUID, item: SecureNote, edits: SecureNoteDetailEdits) async throws
