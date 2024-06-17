9e109c05f4256b20cf6b4d8fe3c9cf0b161a8919
Bradley Mackey <bradleymackey@me.com>
2024-06-17 21:17:14 +0100

    PersistedVaultItemDecoder decodes metadata correctly

--- Vault/Tests/VaultFeedTests/Storage/PersistedVaultItemDecoderTests.swift
+++ Vault/Tests/VaultFeedTests/Storage/PersistedVaultItemDecoderTests.swift
@@ -1,9 +1,31 @@
 import Foundation
+import SwiftData
 import TestHelpers
 import XCTest
 @testable import VaultFeed
 
 final class PersistedVaultItemDecoderTests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    private var context: ModelContext!
+
+    override func setUp() async throws {
+        try await super.setUp()
+
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: PersistedVaultItem.self, configurations: config)
+        context = ModelContext(container)
+    }
+
+    override func tearDown() async throws {
+        try await super.tearDown()
+
+        context = nil
+    }
+}
+
+// MARK: - Generic
+
+extension PersistedVaultItemDecoderTests {
     func test_decodeItem_missingItemDetail() throws {
         let sut = makeSUT()
 
@@ -13,6 +35,90 @@ final class PersistedVaultItemDecoderTests: XCTestCase {
         )
 
         XCTAssertThrowsError(try sut.decode(item: persistedItem))
+    }
+}
+
+// MARK: - Metadata
+
+extension PersistedVaultItemDecoderTests {
+    func test_decodeMetadata_id() throws {
+        let id = UUID()
+        let item = makePersistedItem(id: id)
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertEqual(decoded.id, id)
+    }
+
+    func test_decodeMetadata_createdDate() throws {
+        let date = Date()
+        let item = makePersistedItem(createdDate: date)
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertEqual(decoded.metadata.created, date)
+    }
+
+    func test_decodeMetadata_updatedDate() throws {
+        let date = Date()
+        let item = makePersistedItem(updatedDate: date)
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertEqual(decoded.metadata.updated, date)
+    }
+
+    func test_decodeMetadata_userDescription() throws {
+        let description = "my description \(UUID().uuidString)"
+        let item = makePersistedItem(userDescription: description)
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertEqual(decoded.metadata.userDescription, description)
+    }
+
+    func test_decodeMetadata_colorIsNilIfAllNil() throws {
+        let item = makePersistedItem(
+            colorBlue: nil,
+            colorGreen: nil,
+            colorRed: nil
+        )
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertNil(decoded.metadata.color)
+    }
+
+    func test_decodeMetadata_colorIsNilIfAnyMissing() throws {
+        let item = makePersistedItem(
+            colorBlue: 0.5,
+            colorGreen: nil,
+            colorRed: nil
+        )
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        XCTAssertNil(decoded.metadata.color)
+    }
+
+    func test_decodeMetadata_decodesColorValues() throws {
+        let item = makePersistedItem(
+            colorBlue: 0.5,
+            colorGreen: 0.6,
+            colorRed: 0.7
+        )
+        let sut = makeSUT()
+
+        let decoded = try sut.decode(item: item)
+
+        let expectedColor = VaultItemColor(red: 0.7, green: 0.6, blue: 0.5)
+        XCTAssertEqual(decoded.metadata.color, expectedColor)
     }
 }
 
@@ -32,9 +138,14 @@ extension PersistedVaultItemDecoderTests {
         colorGreen: Double? = nil,
         colorRed: Double? = nil,
         noteDetails: PersistedNoteDetails? = nil,
-        otpDetails: PersistedOTPDetails? = nil
+        otpDetails: PersistedOTPDetails? = .init(
+            algorithm: "SHA1",
+            authType: "totp",
+            secretData: Data(),
+            secretFormat: "BASE_32"
+        )
     ) -> PersistedVaultItem {
-        .init(
+        let item = PersistedVaultItem(
             id: id,
             createdDate: createdDate,
             updatedDate: updatedDate,
@@ -45,5 +156,7 @@ extension PersistedVaultItemDecoderTests {
             noteDetails: noteDetails,
             otpDetails: otpDetails
         )
+        context.insert(item)
+        return item
     }
 }
