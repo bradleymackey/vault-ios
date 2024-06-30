9e131b44ed22bc39912b7d0ee6f66413178b7bb7
Bradley Mackey <bradleymackey@me.com>
2024-06-30 08:20:06 -0400

    Rename internal subject of the IntervalTimerMock

--- Vault/Sources/VaultCore/Clock/IntervalTimerMock.swift
+++ Vault/Sources/VaultCore/Clock/IntervalTimerMock.swift
@@ -2,18 +2,18 @@ import Combine
 import Foundation
 
 public final class IntervalTimerMock: IntervalTimer {
-    private let timerPublisher = PassthroughSubject<Void, Never>()
+    private let waitSubject = PassthroughSubject<Void, Never>()
     /// Mock: the intervals that were waited for.
     public var waitArgValues = [Double]()
 
     public init() {}
 
     public func finishTimer() {
-        timerPublisher.send()
+        waitSubject.send()
     }
 
     public func wait(for time: Double) -> AnyPublisher<Void, Never> {
         waitArgValues.append(time)
-        return timerPublisher.first().eraseToAnyPublisher()
+        return waitSubject.first().eraseToAnyPublisher()
     }
 }
