dc6f8270c039ae97d78779cc900a79f309652bea
Bradley Mackey <bradleymackey@me.com>
2024-07-11 10:29:54 +0100

    Update requirement names of VaultTagStore protocols

--- Vault/Sources/VaultFeed/Storage/VaultTagStore.swift
+++ Vault/Sources/VaultFeed/Storage/VaultTagStore.swift
@@ -6,7 +6,7 @@ import VaultCore
 /// @mockable
 public protocol VaultTagStoreReader: Sendable {
     /// Fetches all tags from storage.
-    func retrieve() async throws -> [VaultItemTag]
+    func retrieveTags() async throws -> [VaultItemTag]
 }
 
 /// Can write all stored vault tags.
@@ -15,12 +15,12 @@ public protocol VaultTagStoreReader: Sendable {
 public protocol VaultTagStoreWriter: Sendable {
     /// Creates a new tag with the given user visible data.
     @discardableResult
-    func insert(item: VaultItemTag.Write) async throws -> VaultItemTag.Identifier
+    func insertTag(item: VaultItemTag.Write) async throws -> VaultItemTag.Identifier
 
     /// Updates user-visible data for a given tag.
-    func update(id: VaultItemTag.Identifier, item: VaultItemTag.Write) async throws
+    func updateTag(id: VaultItemTag.Identifier, item: VaultItemTag.Write) async throws
 
     /// - Throws: if tag is in use, or there was an error in the backing layer.
     /// Has no effect if the tag does not exist.
-    func delete(id: VaultItemTag.Identifier) async throws
+    func deleteTag(id: VaultItemTag.Identifier) async throws
 }
