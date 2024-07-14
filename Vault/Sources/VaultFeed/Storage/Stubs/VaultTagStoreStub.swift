5d88dc5dec215a020381cb015e88dbd2ef6f8f4e
Bradley Mackey <bradleymackey@me.com>
2024-07-14 18:08:03 +0100

    Add Call count tracking to VaultTagStoreStub

--- Vault/Sources/VaultFeed/Storage/Stubs/VaultTagStoreStub.swift
+++ Vault/Sources/VaultFeed/Storage/Stubs/VaultTagStoreStub.swift
@@ -5,27 +5,35 @@ import VaultCore
 public final class VaultTagStoreStub: VaultTagStore {
     public init() {}
 
+    public var retrieveTagsCallCount = 0
     public var retrieveTagsCalled: () -> Void = {}
     public var retrieveTagsResult = [VaultItemTag]()
     public func retrieveTags() async throws -> [VaultItemTag] {
+        retrieveTagsCallCount += 1
         retrieveTagsCalled()
         return retrieveTagsResult
     }
 
+    public var insertTagCallCount = 0
     public var insertTagResult = VaultItemTag.Identifier(id: UUID())
     public var insertTagCalled: (VaultItemTag.Write) -> Void = { _ in }
     public func insertTag(item: VaultItemTag.Write) async throws -> VaultItemTag.Identifier {
+        insertTagCallCount += 1
         insertTagCalled(item)
         return insertTagResult
     }
 
+    public var updateTagCallCount = 0
     public var updateTagCalled: (VaultItemTag.Identifier, VaultItemTag.Write) -> Void = { _, _ in }
     public func updateTag(id: VaultItemTag.Identifier, item: VaultItemTag.Write) async throws {
+        updateTagCallCount += 1
         updateTagCalled(id, item)
     }
 
+    public var deleteTagCallCount = 0
     public var deleteTagCalled: (VaultItemTag.Identifier) -> Void = { _ in }
     public func deleteTag(id: VaultItemTag.Identifier) async throws {
+        deleteTagCallCount += 1
         deleteTagCalled(id)
     }
 }
