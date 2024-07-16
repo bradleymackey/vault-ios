e25f4a5afb75a73263aca99c925a5e2d6bc220ce
Bradley Mackey <bradleymackey@me.com>
2024-07-16 11:06:21 +0100

    Simplify logic and make make more erganonmic for brightness calculation

--- Vault/Sources/VaultiOS/General/Color+Constrast.swift
+++ Vault/Sources/VaultiOS/General/Color+Constrast.swift
@@ -2,25 +2,22 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    // Calculate brightness of the color
-    func brightness() -> Double {
+    var percievedBrightness: Double {
         var red: CGFloat = 0
         var green: CGFloat = 0
         var blue: CGFloat = 0
         var alpha: CGFloat = 0
 
-        // Use UIColor to get RGB components
         if UIColor(self).getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
             // Calculate brightness using standard formula
             return 0.299 * Double(red) + 0.587 * Double(green) + 0.114 * Double(blue)
         }
 
-        // Default brightness value
         return 0.0
     }
 
     // Determine appropriate foreground color for contrast
     var contrastingForegroundColor: Color {
-        brightness() > 0.9 ? .black : .white
+        percievedBrightness > 0.9 ? .black : .white
     }
 }
