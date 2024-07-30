27357459f8aaf6cb4ce5621ea02a45fc01b03821
Bradley Mackey <bradleymackey@me.com>
2024-07-30 11:44:40 +0100

    Create VaultItemLockState model

---
+++ Vault/Sources/VaultFeed/Models/VaultItemLockState.swift
@@ -0,0 +1,34 @@
+import Foundation
+import FoundationExtensions
+
+public enum VaultItemLockState: Equatable, Hashable, CaseIterable, IdentifiableSelf, Sendable {
+    /// This item is not locked and can be viewed immediately
+    case notLocked
+    /// The item is locked and secured at the OS level.
+    ///
+    /// For example, on iOS this requires the device password or biometric authentication.
+    case lockedWithNativeSecurity
+}
+
+extension VaultItemLockState {
+    public var systemIconName: String {
+        switch self {
+        case .notLocked: "lock.open"
+        case .lockedWithNativeSecurity: "lock"
+        }
+    }
+
+    public var localizedTitle: String {
+        switch self {
+        case .notLocked: localized(key: "vaultItemLockState.notLocked.title")
+        case .lockedWithNativeSecurity: localized(key: "vaultItemLockState.lockedWithNativeSecurity.title")
+        }
+    }
+
+    public var localizedSubtitle: String {
+        switch self {
+        case .notLocked: localized(key: "vaultItemLockState.notLocked.subtitle")
+        case .lockedWithNativeSecurity: localized(key: "vaultItemLockState.lockedWithNativeSecurity.subtitle")
+        }
+    }
+}
