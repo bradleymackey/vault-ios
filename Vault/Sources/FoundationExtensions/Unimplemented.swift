9282764375f2f71eb2c9ad658f6844f15478f433
Bradley Mackey <bradleymackey@me.com>
2024-07-30 21:03:53 +0100

    Add unimplemented helper method that will crash at runtime

--- Vault/Sources/FoundationExtensions/Unimplemented.swift
+++ Vault/Sources/FoundationExtensions/Unimplemented.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+public func unimplemented<T>(_ message: String? = nil, file: StaticString = #file, line: UInt = #line) -> T {
+    let finalMessage = "UNIMPLEMENTED: \(message ?? "This has not been implemented")"
+    fatalError(finalMessage, file: file, line: line)
+}
+
 public struct UnimplementedError: Error, LocalizedError {
     private let file: StaticString
     private let line: UInt
