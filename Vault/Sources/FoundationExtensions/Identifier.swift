8312401f956968a884029c392b2bfa41b8133075
Bradley Mackey <bradleymackey@me.com>
2024-08-02 11:29:15 +0100

    Create Identifier type

---
+++ Vault/Sources/FoundationExtensions/Identifier.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public struct Identifier<T>: Identifiable, Equatable, Hashable, Sendable {
+    public var id: UUID
+
+    public init(id: UUID = UUID()) {
+        self.id = id
+    }
+}
+
+extension Identifier: RawRepresentable {
+    public typealias RawValue = UUID
+
+    public init?(rawValue: UUID) {
+        self.init(id: rawValue)
+    }
+
+    public var rawValue: UUID {
+        id
+    }
+}
