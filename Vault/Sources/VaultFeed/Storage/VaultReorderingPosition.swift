65457ffe195e95e6a1119298dd3613a2e717a21e
Bradley Mackey <bradleymackey@me.com>
2024-08-03 10:36:25 +0100

    VaultReorderingPosition uses strongly typed ids

--- Vault/Sources/VaultFeed/Storage/VaultReorderingPosition.swift
+++ Vault/Sources/VaultFeed/Storage/VaultReorderingPosition.swift
@@ -1,14 +1,15 @@
 import Foundation
+import FoundationExtensions
 
-public enum VaultReorderingPosition {
+public enum VaultReorderingPosition: Equatable, Hashable {
     /// Position the item relative to the given item ID, before it.
-    case before(UUID)
+    case before(Identifier<VaultItem>)
     /// Position the item relative to the given item ID, after it.
-    case after(UUID)
+    case after(Identifier<VaultItem>)
 
-    public var id: UUID {
+    public var id: Identifier<VaultItem> {
         switch self {
-        case let .before(uuid), let .after(uuid): uuid
+        case let .before(id), let .after(id): id
         }
     }
 }
