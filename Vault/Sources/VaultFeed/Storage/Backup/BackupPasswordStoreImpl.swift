67fea76810a90c3b96948255ff849201261ab333
Bradley Mackey <bradleymackey@me.com>
2024-08-05 12:03:33 +0100

    Backup password is stored as 1 entry in the keychain

--- Vault/Sources/VaultFeed/Storage/Backup/BackupPasswordStoreImpl.swift
+++ Vault/Sources/VaultFeed/Storage/Backup/BackupPasswordStoreImpl.swift
@@ -21,42 +21,56 @@ public final class BackupPasswordStoreImpl: BackupPasswordStore {
         }
 
         do {
-            let key = try fetchDataFromKeychainIfPresent(key: KeychainKey.key)
-            let salt = try fetchDataFromKeychainIfPresent(key: KeychainKey.salt)
-            let deriverData = try fetchDataFromKeychainIfPresent(key: KeychainKey.deriver)
-            let deriver = try signatureDecoder().decode(ApplicationKeyDeriver.Signature.self, from: deriverData)
-            return BackupPassword(key: key, salt: salt, keyDervier: deriver)
+            let encodedPassword = try fetchDataFromKeychainIfPresent(key: KeychainKey.backupPassword)
+            let decoded = try backupPasswordDecoder().decode(BackupPasswordContainer.self, from: encodedPassword)
+            return decoded.password
         } catch is NotFoundInKeychain {
             return nil
         }
     }
 
     public func set(password: BackupPassword) throws {
-        try secureStorage.store(data: password.key, forKey: KeychainKey.key)
-        try secureStorage.store(data: password.salt, forKey: KeychainKey.salt)
-        let encodedDeriver = try signatureEncoder().encode(password.keyDervier)
-        try secureStorage.store(data: encodedDeriver, forKey: KeychainKey.deriver)
-    }
-
-    private func signatureEncoder() -> JSONEncoder {
-        let encoder = JSONEncoder()
-        encoder.dataEncodingStrategy = .base64
-        return encoder
-    }
-
-    private func signatureDecoder() -> JSONDecoder {
-        let decoder = JSONDecoder()
-        decoder.dataDecodingStrategy = .base64
-        return decoder
+        let container = BackupPasswordContainer(password: password)
+        let encodedPassword = try backupPasswordEncoder().encode(container)
+        try secureStorage.store(data: encodedPassword, forKey: KeychainKey.backupPassword)
     }
 }
 
-// MARK: - Keys
+// MARK: - Encoding
 
 extension BackupPasswordStoreImpl {
+    /// Codable container that is stored in the keychain.
+    private struct BackupPasswordContainer: Codable {
+        var key: Data
+        var salt: Data
+        var keyDervier: ApplicationKeyDeriver.Signature
+
+        init(password: BackupPassword) {
+            key = password.key
+            salt = password.salt
+            keyDervier = password.keyDervier
+        }
+
+        var password: BackupPassword {
+            BackupPassword(key: key, salt: salt, keyDervier: keyDervier)
+        }
+    }
+
+    private func backupPasswordEncoder() -> JSONEncoder {
+        let encoder = JSONEncoder()
+        encoder.dataEncodingStrategy = .base64
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }
+
+    private func backupPasswordDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dataDecodingStrategy = .base64
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+
     private enum KeychainKey {
-        static let key = "vault-backup-password-key-v1"
-        static let salt = "vault-backup-password-salt-v1"
-        static let deriver = "vault-backup-password-deriver-v1"
+        static let backupPassword = "vault-backup-password-v1"
     }
 }
