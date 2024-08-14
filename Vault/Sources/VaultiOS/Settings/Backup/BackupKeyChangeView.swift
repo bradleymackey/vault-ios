2d37ee3e3ec90ff8b92190a9e0eba53cf9230e2f
Bradley Mackey <bradleymackey@me.com>
2024-08-14 07:29:32 +0100

    Update encryption key generation copy

--- Vault/Sources/VaultiOS/Settings/Backup/BackupKeyChangeView.swift
+++ Vault/Sources/VaultiOS/Settings/Backup/BackupKeyChangeView.swift
@@ -108,10 +108,10 @@ struct BackupKeyChangeView: View {
 
                 switch viewModel.newPassword {
                 case .success:
-                    Label("Password and vault encryption key updated successfully", systemImage: "checkmark")
+                    Label("Vault encryption key updated successfully", systemImage: "checkmark.circle.fill")
                         .foregroundStyle(.green)
                 case .keygenError, .keygenCancelled:
-                    Label("Error generating encryption key", systemImage: "questionmark.key.filled")
+                    Label("Error generating encryption key", systemImage: "xmark.octagon.fill")
                         .foregroundStyle(.red)
                 case .creating:
                     HStack(alignment: .center, spacing: 4) {
