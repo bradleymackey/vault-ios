a63f1594895b54d6f95525f26de1b9b4c9ccdd5a
Bradley Mackey <bradleymackey@me.com>
2024-08-16 23:13:45 +0100

    Bump SwiftLint to 0.56.1

--- Vault/Package.swift
+++ Vault/Package.swift
@@ -258,8 +258,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "swiftlint",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
+            url: "https://github.com/realm/SwiftLint/releases/download/0.56.1/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "146ef723e83d301b9f1ef647dc924a55dae293887e633618e76f8cb526292f0c"
         ),
         .executableTarget(
             name: "KeygenSpeedtest",
