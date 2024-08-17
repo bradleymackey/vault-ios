6dcac7f93a9adbb32420f7a8209dab2750091573
Bradley Mackey <bradleymackey@me.com>
2024-08-17 19:48:54 +0100

    Improve design of secure note preview

--- Vault/Sources/VaultiOS/Views/Feed/Previews/SecureNotePreviewView.swift
+++ Vault/Sources/VaultiOS/Views/Feed/Previews/SecureNotePreviewView.swift
@@ -8,9 +8,9 @@ public struct SecureNotePreviewView: View {
 
     public var body: some View {
         VStack(alignment: .center, spacing: 8) {
-            VStack(alignment: .center, spacing: 4) {
+            HStack(alignment: .firstTextBaseline, spacing: 4) {
                 Image(systemName: "doc.text.fill")
-                    .font(.title3)
+                    .font(.headline)
                     .foregroundStyle(viewModel.color.color)
                 Text(viewModel.visibleTitle)
                     .font(.headline)
@@ -21,13 +21,20 @@ public struct SecureNotePreviewView: View {
             .layoutPriority(100)
 
             if let description = viewModel.description, description.isNotEmpty {
-                descriptionLabel(text: description)
+                Spacer()
+
+                Text(description)
+                    .font(.callout)
+                    .foregroundStyle(.secondary)
+                    .tint(.secondary)
                     .layoutPriority(99)
-                    .multilineTextAlignment(.leading)
+                    .multilineTextAlignment(.center)
+
+                Spacer()
             }
         }
         .frame(maxHeight: .infinity)
-        .padding(2)
+        .padding(8)
         .shimmering(active: isShimmering)
         .aspectRatio(1, contentMode: .fill)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -39,23 +46,24 @@ public struct SecureNotePreviewView: View {
         case .editingState: true
         }
     }
-
-    private func descriptionLabel(text: String) -> some View {
-        Text(text)
-            .font(.footnote)
-            .foregroundStyle(.secondary)
-            .tint(.secondary)
-    }
 }
 
-struct SecureNotePreviewView_Previews: PreviewProvider {
-    static var previews: some View {
-        SecureNotePreviewView(
-            viewModel: .init(title: "Test title", description: "desc", color: .init(red: 0, green: 0, blue: 0)),
-            behaviour: .normal
-        )
-        .frame(width: 200, height: 200)
-        .modifier(OTPCardViewModifier())
-        .padding()
-    }
+#Preview {
+    SecureNotePreviewView(
+        viewModel: .init(title: "Test title", description: "desc", color: .init(red: 0, green: 0, blue: 0)),
+        behaviour: .normal
+    )
+    .frame(width: 200, height: 200)
+    .modifier(OTPCardViewModifier())
+    .padding()
+}
+
+#Preview {
+    SecureNotePreviewView(
+        viewModel: .init(title: "Test title", description: "", color: .init(red: 0, green: 0, blue: 0)),
+        behaviour: .normal
+    )
+    .frame(width: 200, height: 200)
+    .modifier(OTPCardViewModifier())
+    .padding()
 }
