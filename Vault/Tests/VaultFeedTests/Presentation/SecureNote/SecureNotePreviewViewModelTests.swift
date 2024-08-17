e4e38cff60aeefab1cf396337175cb30cc918e2b
Bradley Mackey <bradleymackey@me.com>
2024-08-17 21:05:48 +0100

    Fix tests

--- Vault/Tests/VaultFeedTests/Presentation/SecureNote/SecureNotePreviewViewModelTests.swift
+++ Vault/Tests/VaultFeedTests/Presentation/SecureNote/SecureNotePreviewViewModelTests.swift
@@ -6,13 +6,18 @@ import XCTest
 
 final class SecureNotePreviewViewModelTests: XCTestCase {
     func test_visibleTitle_isPlaceholderEmptyTitleIfTitleEmpty() {
-        let sut = SecureNotePreviewViewModel(title: "", description: "description", color: .default)
+        let sut = SecureNotePreviewViewModel(title: "", description: "description", color: .default, isLocked: false)
 
         XCTAssertEqual(sut.visibleTitle, "Untitled Note")
     }
 
     func test_visibleTitle_isUserDefinedTitleIfTitleNotEmpty() {
-        let sut = SecureNotePreviewViewModel(title: "title", description: "description", color: .default)
+        let sut = SecureNotePreviewViewModel(
+            title: "title",
+            description: "description",
+            color: .default,
+            isLocked: false
+        )
 
         XCTAssertEqual(sut.visibleTitle, "title")
     }
