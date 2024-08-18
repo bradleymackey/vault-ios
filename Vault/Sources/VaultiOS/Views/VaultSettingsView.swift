6ff7eaa332e89ec274b86c0e9cb713b2b7f94097
Bradley Mackey <bradleymackey@me.com>
2024-08-18 08:01:24 +0100

    Make VaultSettingsView the actual settings view content

--- Vault/Sources/VaultiOS/Views/VaultSettingsView.swift
+++ Vault/Sources/VaultiOS/Views/VaultSettingsView.swift
@@ -1,11 +1,98 @@
 import SwiftUI
+import VaultFeed
 import VaultSettings
 
-struct VaultSettingsView: View {
-    var viewModel: SettingsViewModel
-    var localSettings: LocalSettings
+@MainActor
+public struct VaultSettingsView: View {
+    private var viewModel: SettingsViewModel
+    @Bindable private var localSettings: LocalSettings
 
-    var body: some View {
-        SettingsHomeView(viewModel: viewModel, localSettings: localSettings)
+    public init(viewModel: SettingsViewModel, localSettings: LocalSettings) {
+        self.viewModel = viewModel
+        _localSettings = Bindable(wrappedValue: localSettings)
+    }
+
+    public var body: some View {
+        Form {
+            viewOptionsSection
+            policySection
+        }
+        .navigationTitle(viewModel.title)
+    }
+
+    private var viewOptionsSection: some View {
+        Section {
+            Picker(selection: $localSettings.state.pasteTimeToLive) {
+                ForEach(PasteTTL.defaultOptions) { option in
+                    Text(option.localizedName)
+                        .tag(option)
+                }
+            } label: {
+                FormRow(image: Image(systemName: "clock.fill"), color: .red) {
+                    Text(viewModel.pasteTTLTitle)
+                }
+            }
+        }
+    }
+
+    private var policySection: some View {
+        Section {
+            NavigationLink {
+                AboutView(viewModel: viewModel)
+            } label: {
+                FormRow(
+                    image: Image(systemName: "key.fill"),
+                    color: .blue
+                ) {
+                    Text(viewModel.aboutTitle)
+                }
+            }
+
+            NavigationLink {
+                OpenSourceView()
+            } label: {
+                FormRow(
+                    image: Image(systemName: "figure.2.arms.open"),
+                    color: .purple
+                ) {
+                    Text(viewModel.openSourceTitle)
+                }
+            }
+
+            NavigationLink {
+                ThirdPartyView()
+            } label: {
+                FormRow(
+                    image: Image(systemName: "text.book.closed.fill"),
+                    color: .blue
+                ) {
+                    Text(viewModel.thirdPartyTitle)
+                }
+            }
+
+            NavigationLink {
+                SettingsDocumentView(title: viewModel.privacyPolicyTitle, viewModel: PrivacyPolicyViewModel())
+            } label: {
+                FormRow(
+                    image: Image(systemName: "lock.fill"),
+                    color: .red
+                ) {
+                    Text(viewModel.privacyPolicyTitle)
+                }
+            }
+
+            NavigationLink {
+                SettingsDocumentView(title: viewModel.termsOfUseTitle, viewModel: TermsOfServiceViewModel())
+            } label: {
+                FormRow(
+                    image: Image(systemName: "person.fill.checkmark"),
+                    color: .green
+                ) {
+                    Text(viewModel.termsOfUseTitle)
+                }
+            }
+        } header: {
+            Text(viewModel.policyAndLegacySectionTitle)
+        }
     }
 }
