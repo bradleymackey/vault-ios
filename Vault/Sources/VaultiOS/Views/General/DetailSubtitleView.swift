0c62d7321966c5b6f1f107455185078e9ec6f045
Bradley Mackey <bradleymackey@me.com>
2024-08-24 17:44:30 +0100

    DetailSubtitleView does not support images

--- Vault/Sources/VaultiOS/Views/General/DetailSubtitleView.swift
+++ Vault/Sources/VaultiOS/Views/General/DetailSubtitleView.swift
@@ -2,33 +2,26 @@ import Foundation
 import SwiftUI
 
 struct DetailSubtitleView: View {
-    var systemIcon: String?
     let title: String
     let subtitle: String?
 
     var body: some View {
-        HStack(alignment: .top, spacing: 12) {
-            if let systemIcon {
-                Image(systemName: systemIcon)
-                    .frame(minWidth: 24)
-                    .padding(.vertical, 2)
-            }
-
-            VStack(alignment: .leading) {
-                Text(title)
-                if let subtitle {
-                    Text(subtitle)
-                        .font(.footnote)
-                        .foregroundStyle(.secondary)
-                }
+        VStack(alignment: .leading) {
+            Text(title)
+            if let subtitle {
+                Text(subtitle)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+                    .transition(.move(edge: .top).combined(with: .opacity))
             }
         }
     }
 }
 
 #Preview {
-    Group {
+    List {
         DetailSubtitleView(title: "Test", subtitle: nil)
-        DetailSubtitleView(title: "Test", subtitle: "hello world")
+        DetailSubtitleView(title: "Test", subtitle: "Hello there")
+        DetailSubtitleView(title: "Test", subtitle: Array(repeating: "Hello", count: 20).joined(separator: " "))
     }
 }
