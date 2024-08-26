58b3bd97fbaff35c5265b10e7f88876f685eebcf
Bradley Mackey <bradleymackey@me.com>
2024-08-26 09:36:08 +0100

    Extract metadata from PDF document

--- Vault/Sources/VaultFeed/Presentation/BackupImportFlowViewModel.swift
+++ Vault/Sources/VaultFeed/Presentation/BackupImportFlowViewModel.swift
@@ -1,5 +1,7 @@
 import Foundation
 import PDFKit
+import VaultBackup
+import VaultCore
 
 @MainActor
 @Observable
@@ -17,7 +19,6 @@ public final class BackupImportFlowViewModel {
     }
 
     public private(set) var state: ImportState = .idle
-    public private(set) var pdf: PDFDocument?
     public let importContext: ImportContext
 
     public init(importContext: ImportContext) {
@@ -38,15 +39,63 @@ public final class BackupImportFlowViewModel {
     }
 
     private func importPDF(fromURL url: URL) {
-        if let pdf = PDFDocument(url: url) {
-            self.pdf = pdf
+        do {
+            let extracted = try extractEncryptedData(pdfURL: url)
             state = .success
-        } else {
-            state = .error(PresentationError(
-                userTitle: "PDF Error",
-                userDescription: "There was an error with the this PDF document. Please check the contents and try again.",
-                debugDescription: "PDF unable to be created from url \(url). Likely malformed."
+        } catch let error as ExtractionError {
+            state = .error(error.presentationError)
+        } catch {
+            state = .error(.init(
+                userTitle: "Import Error",
+                userDescription: "Unable to import document",
+                debugDescription: error.localizedDescription
             ))
         }
+    }
+}
+
+extension BackupImportFlowViewModel {
+    private enum ExtractionError: Error {
+        case unableToLoad(URL)
+        case missingData
+        case malformedBase64Data
+
+        var presentationError: PresentationError {
+            switch self {
+            case let .unableToLoad(url):
+                PresentationError(
+                    userTitle: "PDF Error",
+                    userDescription: "There was an error with the this PDF document. Please check the PDF, your internet, and try again.",
+                    debugDescription: "PDF unable to be created from url \(url). Likely malformed or no connection."
+                )
+            case .missingData:
+                PresentationError(
+                    userTitle: "Not a Vault Export",
+                    userDescription: "This is not a vault export or the data has been modified. Please scan the QR codes manually.",
+                    debugDescription: "Missing data in document attribute."
+                )
+            case .malformedBase64Data:
+                PresentationError(
+                    userTitle: "Can't Extract Data Automatically",
+                    userDescription: "We couldn't read this document automatically. Please scan the QR codes manually.",
+                    debugDescription: "Data is malformed for decoding."
+                )
+            }
+        }
+    }
+
+    /// Extracts the `EncryptedVault` from the PDF using the data stored in the `documentAttributes`
+    private func extractEncryptedData(pdfURL: URL) throws -> EncryptedVault {
+        guard let pdf = PDFDocument(url: pdfURL) else {
+            throw ExtractionError.unableToLoad(pdfURL)
+        }
+        let encryptedDataID = VaultIdentifiers.Backup.encryptedVaultData
+        guard let encryptedVault = pdf.documentAttributes?[encryptedDataID] as? String else {
+            throw ExtractionError.missingData
+        }
+        guard let data = Data(base64Encoded: encryptedVault) else {
+            throw ExtractionError.malformedBase64Data
+        }
+        return try EncryptedVaultCoder().decode(vaultData: data)
     }
 }
