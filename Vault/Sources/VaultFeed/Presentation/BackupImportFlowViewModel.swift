85f5e70ee24422905e6306b16127143ea4ec595b
Bradley Mackey <bradleymackey@me.com>
2024-08-27 07:29:37 +0100

    Use VaultBackupPDFDetatcher in the import flow view model

--- Vault/Sources/VaultFeed/Presentation/BackupImportFlowViewModel.swift
+++ Vault/Sources/VaultFeed/Presentation/BackupImportFlowViewModel.swift
@@ -10,6 +10,13 @@ public final class BackupImportFlowViewModel {
         case idle
         case error(PresentationError)
         case success
+
+        var isError: Bool {
+            switch self {
+            case .error: true
+            case .idle, .success: false
+            }
+        }
     }
 
     public enum ImportContext: Equatable {
@@ -23,10 +30,16 @@ public final class BackupImportFlowViewModel {
     /// The backup password the user already has on their device.
     /// If the imported backup was encrypted with this same password, we don't need to prompt the user.
     private let existingBackupPassword: BackupPassword?
+    private let backupPDFDetatcher: any VaultBackupPDFDetatcher
 
-    public init(importContext: ImportContext, existingBackupPassword: BackupPassword?) {
+    public init(
+        importContext: ImportContext,
+        existingBackupPassword: BackupPassword?,
+        backupPDFDetatcher: any VaultBackupPDFDetatcher = VaultBackupPDFDetatcherImpl()
+    ) {
         self.importContext = importContext
         self.existingBackupPassword = existingBackupPassword
+        self.backupPDFDetatcher = backupPDFDetatcher
     }
 
     public func handleImport(result: Result<URL, any Error>) {
@@ -42,77 +55,24 @@ public final class BackupImportFlowViewModel {
         }
     }
 
+    struct InvalidURLError: Error {}
+
     private func importPDF(fromURL url: URL) {
         do {
-            let extracted = try extractEncryptedData(pdfURL: url)
+            guard let pdf = PDFDocument(url: url) else {
+                throw InvalidURLError()
+            }
+            let encryptedVault = try backupPDFDetatcher.detachEncryptedVault(fromPDF: pdf)
             // TODO: actually import the data
             state = .success
-        } catch let error as ExtractionError {
-            state = .error(error.presentationError)
+        } catch let error as LocalizedError {
+            state = .error(.init(localizedError: error))
         } catch {
-            state = .error(.init(
-                userTitle: "Import Error",
-                userDescription: "Unable to import document",
-                debugDescription: error.localizedDescription
+            state = .error(PresentationError(
+                userTitle: "PDF Error",
+                userDescription: "There was an error with the this PDF document. Please check the PDF, your internet, and try again.",
+                debugDescription: "PDF unable to be created from url \(url). Likely malformed or no connection."
             ))
         }
-    }
-}
-
-extension BackupImportFlowViewModel {
-    private enum ExtractionError: Error {
-        case unableToLoad(URL)
-        case missingData
-        case noPages
-        case noAnnotations
-        case malformedBase64Data
-
-        var presentationError: PresentationError {
-            switch self {
-            case let .unableToLoad(url):
-                PresentationError(
-                    userTitle: "PDF Error",
-                    userDescription: "There was an error with the this PDF document. Please check the PDF, your internet, and try again.",
-                    debugDescription: "PDF unable to be created from url \(url). Likely malformed or no connection."
-                )
-            case .missingData, .noPages, .noAnnotations:
-                PresentationError(
-                    userTitle: "Not a Vault Export",
-                    userDescription: "This is not a vault export or the data has been modified. Please scan the QR codes manually.",
-                    debugDescription: "Missing data in document needed for extraction."
-                )
-            case .malformedBase64Data:
-                PresentationError(
-                    userTitle: "Can't Extract Data Automatically",
-                    userDescription: "We couldn't read this document automatically. Please scan the QR codes manually.",
-                    debugDescription: "Data is malformed for decoding."
-                )
-            }
-        }
-    }
-
-    /// Extracts the `EncryptedVault` from the PDF using the data stored in the `documentAttributes`
-    private func extractEncryptedData(pdfURL: URL) throws -> EncryptedVault {
-        guard let pdf = PDFDocument(url: pdfURL) else {
-            throw ExtractionError.unableToLoad(pdfURL)
-        }
-        guard let firstPage = pdf.page(at: 0) else {
-            throw ExtractionError.noPages
-        }
-        guard let annotation = firstPage.annotations
-            .first(where: { $0.contents?.starts(with: VaultIdentifiers.Backup.encryptedVaultData) == true })
-        else {
-            throw ExtractionError.noAnnotations
-        }
-        guard let contents = annotation.contents else {
-            throw ExtractionError.noAnnotations
-        }
-        guard let encodedString = contents.split(separator: ":")[safeIndex: 1] else {
-            throw ExtractionError.missingData
-        }
-        guard let data = Data(base64Encoded: String(encodedString)) else {
-            throw ExtractionError.malformedBase64Data
-        }
-        return try EncryptedVaultCoder().decode(vaultData: data)
     }
 }
