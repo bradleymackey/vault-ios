032fa961577fa42bc1a844bce399955c459a9ebb
Bradley Mackey <bradleymackey@me.com>
2024-08-30 10:02:39 +0100

    Use OSAllocatedUnfairLock within Atomic, as this is Safe in Swift

--- Vault/Sources/FoundationExtensions/Atomic/Atomic.swift
+++ Vault/Sources/FoundationExtensions/Atomic/Atomic.swift
@@ -9,42 +9,32 @@ import os
 /// - note: This is not safe to be used as a property wrapper until
 /// "modify accessors" are stabilised in Swift. This is because of implicit
 /// copy-on-write within getters/setters that can not work for collections.
-public final class Atomic<Value>: @unchecked Sendable {
+public final class Atomic<Value: Sendable>: Sendable {
     @usableFromInline
-    var _storage: Value
-
-    @usableFromInline
-    let lock = Lock()
-
-    /// Performs the operation in the `block` protected by a lock.
-    ///
-    /// - note: `block` is not, and cannot, be `async`. Swift's concurrency
-    /// model requires that we are able to make forward progress and if this block
-    /// were to suspend this could lead to a deadlock (due to the lock surrounding the block).
-    @usableFromInline
-    func perform<Result>(block: () throws -> Result) rethrows -> Result {
-        lock.lock()
-        defer { lock.unlock() }
-        return try block()
-    }
+    let lock: OSAllocatedUnfairLock<Value>
 
     @inlinable
     public init(initialValue value: Value) {
-        _storage = value
+        lock = .init(initialState: value)
     }
 
     @inlinable
-    public func get<T>(_ block: (Value) throws -> T) rethrows -> T {
-        try perform {
-            try block(_storage)
+    public var value: Value {
+        lock.withLock { $0 }
+    }
+
+    @inlinable
+    public func get<T: Sendable>(_ block: @Sendable (Value) throws -> T) rethrows -> T {
+        try lock.withLock { value in
+            try block(value)
         }
     }
 
     @discardableResult
     @inlinable
-    public func modify<T>(_ block: (inout Value) throws -> T) rethrows -> T {
-        try perform {
-            try block(&_storage)
+    public func modify<T: Sendable>(_ block: @Sendable (inout Value) throws -> T) rethrows -> T {
+        try lock.withLock { value in
+            try block(&value)
         }
     }
 }
