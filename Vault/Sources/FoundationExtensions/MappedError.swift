3b23ca8aefcea05bd9e94ff15b60f387965e3f70
Bradley Mackey <bradleymackey@me.com>
2024-09-06 20:04:08 +0100

    Add mapped error helper for catching specific types of errors

--- Vault/Sources/FoundationExtensions/MappedError.swift
+++ Vault/Sources/FoundationExtensions/MappedError.swift
@@ -21,6 +21,21 @@ public func withCatchingError(body: () throws -> some Any) -> (any Error)? {
 }
 
 /// Catches an error and returns the error type.
+///
+/// Throws if there was another error that we can't catch.
+@inlinable
+public func withCatchingSomeError<T, E>(body: () throws -> T) throws -> (E)? {
+    do {
+        _ = try body()
+        return nil
+    } catch let error as E {
+        return error
+    } catch {
+        throw error
+    }
+}
+
+/// Catches an error and returns the error type.
 @inlinable
 public func withCatchingAsyncError(body: () async throws -> some Any) async -> (any Error)? {
     do {
