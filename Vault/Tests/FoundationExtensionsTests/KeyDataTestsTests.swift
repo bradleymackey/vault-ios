1fc7ed83783f4c68595794a52a50c8d616130049
Bradley Mackey <bradleymackey@me.com>
2024-09-28 00:24:46 +0100

    Migrate KeyDataTests

--- Vault/Tests/FoundationExtensionsTests/KeyDataTestsTests.swift
+++ Vault/Tests/FoundationExtensionsTests/KeyDataTestsTests.swift
@@ -1,40 +1,50 @@
 import Foundation
-import XCTest
+import Testing
 @testable import FoundationExtensions
 
-final class KeyDataTests: XCTestCase {
-    func test_init_invalidKeyLengthThrows() {
-        XCTAssertThrowsError(
-            try KeyData<Bits256>(data: Data(repeating: 0x31, count: 21))
-        )
+struct KeyDataTests {
+    @Test(arguments: [0, 1, 21, 31, 33, 100])
+    func init_invalidKeyLengthThrows(keyLength: Int) {
+        #expect(throws: KeyData<Bits256>.LengthError.self) {
+            try KeyData<Bits256>(data: Data(repeating: 0x31, count: keyLength))
+        }
     }
 
-    func test_init_validKeyLengthCreatesKey() throws {
+    @Test
+    func init_validKeyLengthCreatesKey() throws {
         let sut = try KeyData<Bits256>(data: Data(repeating: 0x31, count: 32))
 
-        XCTAssertEqual(sut.data.count, 32)
+        #expect(sut.data.count == 32)
     }
 
-    func test_equatable_sameKeysEqual() throws {
-        let sut1 = try KeyData<Bits256>(data: Data(repeating: 0x31, count: 32))
-        let sut2 = try KeyData<Bits256>(data: Data(repeating: 0x31, count: 32))
+    @Test(arguments: [
+        Data(repeating: 0x31, count: 32),
+        Data.random(count: 32),
+        Data.random(count: 32),
+        Data.random(count: 32),
+    ])
+    func equatable_sameKeysEqual(data: Data) throws {
+        let sut1 = try KeyData<Bits256>(data: data)
+        let sut2 = try KeyData<Bits256>(data: data)
 
-        XCTAssertEqual(sut1, sut2)
+        #expect(sut1 == sut2)
     }
 
-    func test_equatable_differentKeysDifferent() throws {
+    @Test
+    func equatable_differentKeysDifferent() throws {
         let sut1 = try KeyData<Bits256>(data: Data(repeating: 0x31, count: 32))
         let sut2 = try KeyData<Bits256>(data: Data(repeating: 0x32, count: 32))
 
-        XCTAssertNotEqual(sut1, sut2)
+        #expect(sut1 != sut2)
     }
 
-    func test_random_createsRandomKey() throws {
+    @Test
+    func random_createsRandomKey() throws {
         var seen = Set<KeyData<Bits256>>()
         for _ in 1 ... 100 {
             let key = KeyData<Bits256>.random()
             defer { seen.insert(key) }
-            XCTAssertFalse(seen.contains(key))
+            #expect(seen.contains(key) == false)
         }
     }
 }
