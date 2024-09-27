bee547e7380fc807dd74d40128b6ea5e19c93c51
Bradley Mackey <bradleymackey@me.com>
2024-09-27 20:11:31 +0100

    Clear Defaults between test runs

--- Vault/Tests/FoundationExtensionsTests/DefaultsStoredTests.swift
+++ Vault/Tests/FoundationExtensionsTests/DefaultsStoredTests.swift
@@ -2,11 +2,21 @@ import Foundation
 import FoundationExtensions
 import Testing
 
-struct DefaultsStoredTests {
+final class DefaultsStoredTests {
+    let defaults: Defaults
+
+    init() throws {
+        let userDefaults = try #require(UserDefaults(suiteName: #file + #function))
+        userDefaults.removePersistentDomain(forName: #file + #function)
+        defaults = Defaults(userDefaults: userDefaults)
+    }
+
+    deinit {
+        defaults.removeAll()
+    }
+
     @Test(arguments: [0, 1234, 456])
     func init_fallsBackToDefaultIfNoStoredValue(defaultValue: Int) throws {
-        let defaults = try makeDefaults()
-
         @DefaultsStored(defaults: defaults, defaultsKey: .init("test1"), defaultValue: defaultValue)
         var coolNumber: Int
 
@@ -15,7 +25,6 @@ struct DefaultsStoredTests {
 
     @Test(arguments: [4567, 0, 99999])
     func init_fetchesInitialValueStoredInDefaults(initialValue: Int) throws {
-        let defaults = try makeDefaults()
         try defaults.set(initialValue, for: .init("test2"))
 
         @DefaultsStored(defaults: defaults, defaultsKey: .init("test2"), defaultValue: 1234)
@@ -28,7 +37,6 @@ struct DefaultsStoredTests {
     func test_wrappedValue_setsValueInDefaults() throws {
         let key: Key<Int> = .init("test3")
         let coolNumberInitial = 4567
-        let defaults = try makeDefaults()
         try defaults.set(coolNumberInitial, for: key)
 
         @DefaultsStored(defaults: defaults, defaultsKey: key, defaultValue: 1234)
@@ -40,15 +48,5 @@ struct DefaultsStoredTests {
 
         let defaultsValue = try #require(defaults.get(for: key))
         #expect(defaultsValue == 9876)
-    }
-}
-
-// MARK: - Helpers
-
-extension DefaultsStoredTests {
-    private func makeDefaults() throws -> Defaults {
-        let userDefaults = try #require(UserDefaults(suiteName: #file))
-        userDefaults.removePersistentDomain(forName: #file)
-        return Defaults(userDefaults: userDefaults)
     }
 }
