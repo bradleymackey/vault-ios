75ec1c7e10813b228ad4d4c81867ae80a9f6539a
Bradley Mackey <bradleymackey@me.com>
2024-09-28 17:04:11 +0100

    Migrate `ImageToolsTests` to Swift Testing (#318)
    - Simple inline replacements and snapshot test renames, quite simple!

--- Vault/Tests/ImageToolsTests/QRCodeImageRendererSnapshotTests.swift
+++ Vault/Tests/ImageToolsTests/QRCodeImageRendererSnapshotTests.swift
@@ -1,32 +1,24 @@
 import Foundation
 import ImageTools
 import SnapshotTesting
+import Testing
 import UIKit
-import XCTest
 
-final class QRCodeImageRendererSnapshotTests: XCTestCase {
-    func test_makeImage_generatesWithEmptyData() throws {
-        let sut = makeSUT()
+struct QRCodeImageRendererSnapshotTests {
+    let sut = QRCodeImageRenderer()
 
-        let image = try XCTUnwrap(sut.makeImage(fromData: Data()))
+    @Test
+    func makeImage_generatesWithEmptyData() throws {
+        let image = try #require(sut.makeImage(fromData: Data()))
 
         assertSnapshot(of: image, as: .image)
     }
 
-    func test_makeImage_generatesWithSomeData() throws {
-        let sut = makeSUT()
-
+    @Test
+    func makeImage_generatesWithSomeData() throws {
         let data = Data(repeating: 0xFF, count: 200)
-        let image = try XCTUnwrap(sut.makeImage(fromData: data))
+        let image = try #require(sut.makeImage(fromData: data))
 
         assertSnapshot(of: image, as: .image)
-    }
-}
-
-// MARK: - Helpers
-
-extension QRCodeImageRendererSnapshotTests {
-    private func makeSUT() -> QRCodeImageRenderer {
-        QRCodeImageRenderer()
     }
 }
