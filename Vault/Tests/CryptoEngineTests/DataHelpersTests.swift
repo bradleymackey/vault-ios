9009a36ac80d60fecb691c0af3449e21da932c84
Bradley Mackey <bradleymackey@me.com>
2024-09-28 17:09:38 +0100

    Migrate DataHelpersTests

--- Vault/Tests/CryptoEngineTests/DataHelpersTests.swift
+++ Vault/Tests/CryptoEngineTests/DataHelpersTests.swift
@@ -1,22 +1,25 @@
 import Foundation
-import XCTest
+import Testing
 @testable import CryptoEngine
 
-final class DataHelpersTests: XCTestCase {
-    func test_dataAsType_interpretsAsLittleEndian() {
+struct DataHelpersTests {
+    @Test
+    func dataAsType_interpretsAsLittleEndian() {
         let data32 = Data(hex: "ffffffee")
-        XCTAssertEqual(data32.asType(UInt32.self), 0xEEFF_FFFF)
+        #expect(data32.asType(UInt32.self) == 0xEEFF_FFFF)
         let data64 = Data(hex: "ffffffffffffffee")
-        XCTAssertEqual(data64.asType(UInt64.self), 0xEEFF_FFFF_FFFF_FFFF)
+        #expect(data64.asType(UInt64.self) == 0xEEFF_FFFF_FFFF_FFFF)
     }
 
-    func test_int64ToData_interpretsAsLittleEndian() {
+    @Test
+    func int64ToData_interpretsAsLittleEndian() {
         let number: UInt64 = 1
-        XCTAssertEqual(number.data.bytes, [1, 0, 0, 0, 0, 0, 0, 0])
+        #expect(number.data.bytes == [1, 0, 0, 0, 0, 0, 0, 0])
     }
 
-    func test_byteString_interpretsUTF8StringValues() {
+    @Test
+    func byteString_interpretsUTF8StringValues() {
         let value = Data(byteString: "1234")
-        XCTAssertEqual(value.bytes, [49, 50, 51, 52])
+        #expect(value.bytes == [49, 50, 51, 52])
     }
 }
