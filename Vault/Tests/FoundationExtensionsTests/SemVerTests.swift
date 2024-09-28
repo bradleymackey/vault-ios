fe994a8d964652548ddcd5cbd20a51502e2d7d85
Bradley Mackey <bradleymackey@me.com>
2024-09-28 15:52:57 +0100

    Improved cases for SemVer tests

--- Vault/Tests/FoundationExtensionsTests/SemVerTests.swift
+++ Vault/Tests/FoundationExtensionsTests/SemVerTests.swift
@@ -4,13 +4,18 @@ import TestHelpers
 import Testing
 
 struct SemVerTests {
-    @Test
-    func init_validString() throws {
-        let semVer = try SemVer(string: "1.0.2")
+    @Test(arguments: [
+        ("1.0.2", 1, 0, 2),
+        ("1.0.0", 1, 0, 0),
+        ("2.0.0", 2, 0, 0),
+        ("12.13.14", 12, 13, 14),
+    ])
+    func init_validString(string: String, major: Int, minor: Int, patch: Int) throws {
+        let semVer = try SemVer(string: string)
 
-        #expect(semVer.major == 1)
-        #expect(semVer.minor == 0)
-        #expect(semVer.patch == 2)
+        #expect(semVer.major == major)
+        #expect(semVer.minor == minor)
+        #expect(semVer.patch == patch)
     }
 
     @Test(arguments: ["1", "1.0", "2.1", ""])
@@ -20,15 +25,19 @@ struct SemVerTests {
         }
     }
 
-    @Test
-    func init_encodesToString() throws {
-        let sut = try SemVer(string: "3.4.1")
+    @Test(arguments: [
+        ("0.0.1", "\"0.0.1\""),
+        ("1.0.0", "\"1.0.0\""),
+        ("3.4.1", "\"3.4.1\""),
+    ])
+    func init_encodesToString(string: String, expected: String) throws {
+        let sut = try SemVer(string: string)
 
         let encoder = JSONEncoder()
 
         let encoded = try encoder.encode(sut)
         let string = String(data: encoded, encoding: .utf8)
-        #expect(string == "\"3.4.1\"")
+        #expect(string == expected)
     }
 
     @Test
