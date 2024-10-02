fae4a1f9e211aca007276a78f156c39038daf88d
Bradley Mackey <bradleymackey@me.com>
2024-10-02 11:15:20 +0100

    Migrate PDFContentDrawererTests

--- Vault/Tests/VaultExportTests/PDFContentDrawererTests.swift
+++ Vault/Tests/VaultExportTests/PDFContentDrawererTests.swift
@@ -1,107 +1,111 @@
 import Foundation
 import TestHelpers
-import XCTest
+import Testing
 @testable import VaultExport
 
-final class PDFContentDrawererTests: XCTestCase {
-    func test_drawContent_drawsOnceIfSuccess() throws {
-        var executions = 0
-        let sut = PDFContentDrawerer {
-            executions += 1
-            return .success(.didDrawToDocument)
-        } makeNewPage: {
-            // noop
-        }
-
-        try sut.drawContent()
-
-        XCTAssertEqual(executions, 1)
-    }
-
-    func test_drawContent_drawsTwiceIfInsufficientSpaceOnFirstCall() throws {
-        var executions = 0
-        let sut = PDFContentDrawerer {
-            defer { executions += 1 }
-            if executions == 0 {
-                return .failure(.insufficientSpace)
-            } else {
+struct PDFContentDrawererTests {
+    @Test
+    func drawContent_drawsOnceIfSuccess() async throws {
+        try await confirmation { confirmation in
+            let sut = PDFContentDrawerer {
+                defer { confirmation.confirm() }
                 return .success(.didDrawToDocument)
+            } makeNewPage: {
+                // noop
             }
-        } makeNewPage: {
-            // noop
+
+            try sut.drawContent()
         }
-
-        try sut.drawContent()
-
-        XCTAssertEqual(executions, 2)
     }
 
-    func test_drawContent_throwsErrorIfInsufficientSpaceMoreThanOnce() throws {
+    @Test
+    func drawContent_drawsTwiceIfInsufficientSpaceOnFirstCall() async throws {
+        try await confirmation(expectedCount: 2) { confirmation in
+            var hasCalled = false
+            let sut = PDFContentDrawerer {
+                defer { hasCalled = true }
+                defer { confirmation.confirm() }
+                if !hasCalled {
+                    return .failure(.insufficientSpace)
+                } else {
+                    return .success(.didDrawToDocument)
+                }
+            } makeNewPage: {
+                // noop
+            }
+
+            try sut.drawContent()
+        }
+    }
+
+    @Test
+    func drawContent_throwsErrorIfInsufficientSpaceMoreThanOnce() throws {
         let sut = PDFContentDrawerer {
             .failure(.insufficientSpace)
         } makeNewPage: {
             // noop
         }
 
-        XCTAssertThrowsError(try sut.drawContent())
-    }
-
-    func test_drawContent_doesNotDrawTwiceIfContentMissing() throws {
-        var executions = 0
-        let sut = PDFContentDrawerer {
-            executions += 1
-            return .failure(.contentMissing)
-        } makeNewPage: {
-            // noop
+        #expect(throws: PDFContentDrawerer.DrawError.insufficientSpace) {
+            try sut.drawContent()
         }
-
-        try sut.drawContent()
-
-        XCTAssertEqual(executions, 1)
     }
 
-    func test_drawContent_doesNotMakeNewPageOnSuccess() throws {
-        var executions = 0
-        let sut = PDFContentDrawerer {
-            .success(.didDrawToDocument)
-        } makeNewPage: {
-            executions += 1
-        }
-
-        try sut.drawContent()
-
-        XCTAssertEqual(executions, 0)
-    }
-
-    func test_drawContent_makesNewPageOnInsufficientSpace() throws {
-        var executions = 0
-        var drawExecutions = 0
-        let sut = PDFContentDrawerer {
-            defer { drawExecutions += 1 }
-            if drawExecutions == 0 {
-                return .failure(.insufficientSpace)
-            } else {
-                return .success(.didDrawToDocument)
+    @Test
+    func drawContent_doesNotDrawTwiceIfContentMissing() async throws {
+        try await confirmation(expectedCount: 1) { confirmation in
+            let sut = PDFContentDrawerer {
+                defer { confirmation.confirm() }
+                return .failure(.contentMissing)
+            } makeNewPage: {
+                // noop
             }
-        } makeNewPage: {
-            executions += 1
+
+            try sut.drawContent()
         }
-
-        try sut.drawContent()
-
-        XCTAssertEqual(executions, 1)
     }
 
-    func test_drawContent_doesNotMakeNewPageOnContentMissing() throws {
-        var executions = 0
-        let sut = PDFContentDrawerer {
-            .failure(.contentMissing)
-        } makeNewPage: {
-            executions += 1
+    @Test
+    func drawContent_doesNotMakeNewPageOnSuccess() async throws {
+        try await confirmation(expectedCount: 0) { confirmation in
+            let sut = PDFContentDrawerer {
+                .success(.didDrawToDocument)
+            } makeNewPage: {
+                confirmation.confirm()
+            }
+            try sut.drawContent()
         }
+    }
 
-        try sut.drawContent()
+    @Test
+    func drawContent_makesNewPageOnInsufficientSpace() async throws {
+        try await confirmation(expectedCount: 1) { confirmation in
+            var hasCalled = false
+            let sut = PDFContentDrawerer {
+                defer { hasCalled = true }
+                if !hasCalled {
+                    return .failure(.insufficientSpace)
+                } else {
+                    return .success(.didDrawToDocument)
+                }
+            } makeNewPage: {
+                confirmation.confirm()
+            }
 
-        XCTAssertEqual(executions, 0)
+            try sut.drawContent()
+        }
+    }
+
+    @Test
+    func drawContent_doesNotMakeNewPageOnContentMissing() async throws {
+        try await confirmation(expectedCount: 0) { confirmation in
+            let sut = PDFContentDrawerer {
+                .failure(.contentMissing)
+            } makeNewPage: {
+                confirmation.confirm()
+            }
+
+            try sut.drawContent()
+        }
     }
 }
