83848123cfdb1259b7fc8664e38e944dc20582d1
Bradley Mackey <bradleymackey@me.com>
2024-10-02 10:29:54 +0100

    Add ID to TimeoutError so we can check where a timeout originated from

--- Vault/Sources/FoundationExtensions/Timeout/TimeoutError.swift
+++ Vault/Sources/FoundationExtensions/Timeout/TimeoutError.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 public struct TimeoutError: Error, LocalizedError {
+    /// Identifies this timeout so we can distinguish where a timeout originated from.
+    public let id: UUID
     /// When the timeout occurred.
     public let occurred: Date = .init()
 
-    public init() {}
+    public init(id: UUID = UUID()) {
+        self.id = id
+    }
 
     public var errorDescription: String? {
         "The operation timed out."
