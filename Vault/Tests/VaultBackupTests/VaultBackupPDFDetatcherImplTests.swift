c7746fe44d323cca93abe313e2c5d72b34fe72af
Bradley Mackey <bradleymackey@me.com>
2024-10-03 08:15:40 +0100

    Migrate VaultBackupPDFDetatcherImplTests

--- Vault/Tests/VaultBackupTests/VaultBackupPDFDetatcherImplTests.swift
+++ Vault/Tests/VaultBackupTests/VaultBackupPDFDetatcherImplTests.swift
@@ -1,12 +1,13 @@
 import Foundation
 import PDFKit
 import TestHelpers
+import Testing
 import VaultCore
-import XCTest
 @testable import VaultBackup
 
-final class VaultBackupPDFDetatcherImplTests: XCTestCase {
-    func test_detachEncryptedVault_decodesIfPresent() throws {
+struct VaultBackupPDFDetatcherImplTests {
+    @Test
+    func detachEncryptedVault_decodesIfPresent() throws {
         let sut = makeSUT()
         let pdf = PDFDocument.onePage
         let annotation = try makeVaultAnnotation(vault: anyEncryptedVault())
@@ -14,37 +15,49 @@ final class VaultBackupPDFDetatcherImplTests: XCTestCase {
 
         let result = try sut.detachEncryptedVault(fromPDF: pdf)
 
-        XCTAssertEqual(result, anyEncryptedVault())
+        #expect(result == anyEncryptedVault())
     }
 
-    func test_detachEncryptedVault_noPagesThrowsError() throws {
+    @Test
+    func detachEncryptedVault_noPagesThrowsError() throws {
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.detachEncryptedVault(fromPDF: .noPages))
+        #expect(throws: (any Error).self) {
+            try sut.detachEncryptedVault(fromPDF: .noPages)
+        }
     }
 
-    func test_detachEncryptedVault_noAnnotationsThrowsError() throws {
+    @Test
+    func detachEncryptedVault_noAnnotationsThrowsError() throws {
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.detachEncryptedVault(fromPDF: .onePage))
+        #expect(throws: (any Error).self) {
+            try sut.detachEncryptedVault(fromPDF: .onePage)
+        }
     }
 
-    func test_detachEncryptedVault_missingPayloadThrowsError() throws {
+    @Test
+    func detachEncryptedVault_missingPayloadThrowsError() throws {
         let sut = makeSUT()
         let pdf = PDFDocument.onePage
         let annotation = makeEmptyAnnotation()
         pdf.page(at: 0)?.addAnnotation(annotation)
 
-        XCTAssertThrowsError(try sut.detachEncryptedVault(fromPDF: pdf))
+        #expect(throws: (any Error).self) {
+            try sut.detachEncryptedVault(fromPDF: pdf)
+        }
     }
 
-    func test_detachEncryptedVault_invalidDataThrowsError() throws {
+    @Test
+    func detachEncryptedVault_invalidDataThrowsError() throws {
         let sut = makeSUT()
         let pdf = PDFDocument.onePage
         let annotation = makeInvalidDataAnnotation()
         pdf.page(at: 0)?.addAnnotation(annotation)
 
-        XCTAssertThrowsError(try sut.detachEncryptedVault(fromPDF: pdf))
+        #expect(throws: (any Error).self) {
+            try sut.detachEncryptedVault(fromPDF: pdf)
+        }
     }
 }
 
