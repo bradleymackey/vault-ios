1bd392cae1e730ade8b1f750ce1cebec248ab896
Bradley Mackey <bradleymackey@me.com>
2024-10-03 08:23:45 +0100

    Migrate VaultExportDataBlockHeaderGeneratorTests

--- Vault/Tests/VaultBackupTests/VaultExportDataBlockHeaderGeneratorTests.swift
+++ Vault/Tests/VaultBackupTests/VaultExportDataBlockHeaderGeneratorTests.swift
@@ -1,23 +1,25 @@
 import Foundation
 import TestHelpers
-import XCTest
+import Testing
 @testable import VaultBackup
 
-final class VaultExportDataBlockHeaderGeneratorTests: XCTestCase {
-    func test_makeHeader_leftIsDate() {
+struct VaultExportDataBlockHeaderGeneratorTests {
+    @Test
+    func makeHeader_leftIsDate() {
         let date = Date(timeIntervalSince1970: 3_000_000)
         let sut = VaultExportDataBlockHeaderGenerator(dateCreated: date, totalNumberOfPages: 100)
 
         let header = sut.makeHeader(pageNumber: 1)
 
-        XCTAssertEqual(header?.left, "Created 2/4/70, 5:20 PM")
+        #expect(header?.left == "Created 2/4/70, 5:20 PM")
     }
 
-    func test_makeHeader_rightIsPageNumber() {
+    @Test
+    func makeHeader_rightIsPageNumber() {
         let sut = VaultExportDataBlockHeaderGenerator(dateCreated: Date(), totalNumberOfPages: 500)
 
         let header = sut.makeHeader(pageNumber: 442)
 
-        XCTAssertEqual(header?.right, "Page 442 of 500")
+        #expect(header?.right == "Page 442 of 500")
     }
 }
