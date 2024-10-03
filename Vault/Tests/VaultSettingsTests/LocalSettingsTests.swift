78aab2937180078ef3ebb531da80627981c2aa85
Bradley Mackey <bradleymackey@me.com>
2024-10-03 09:23:32 +0100

    Migrate LocalSettingsTests

--- Vault/Tests/VaultSettingsTests/LocalSettingsTests.swift
+++ Vault/Tests/VaultSettingsTests/LocalSettingsTests.swift
@@ -1,55 +1,39 @@
 import Foundation
 import FoundationExtensions
 import TestHelpers
+import Testing
 import VaultSettings
-import XCTest
 
-final class LocalSettingsTests: XCTestCase {
-    @MainActor
-    func test_pasteTimeToLive_sendsObservableChangeOnValueChange() async throws {
+struct LocalSettingsTests {
+    @Test
+    func pasteTimeToLive_defaultsToDefaultValue() throws {
         let defaults = try makeDefaults()
         let sut = try makeSUT(defaults: defaults)
 
-        await expectSingleMutation(observable: sut, keyPath: \.state.pasteTimeToLive) {
-            sut.state.pasteTimeToLive = .init(duration: 100)
-        }
-
-        await expectSingleMutation(observable: sut, keyPath: \.state.pasteTimeToLive) {
-            sut.state.pasteTimeToLive = .init(duration: 200)
-        }
+        #expect(sut.state.pasteTimeToLive == .default)
     }
 
-    @MainActor
-    func test_pasteTimeToLive_defaultsToDefaultValue() throws {
-        let defaults = try makeDefaults()
-        let sut = try makeSUT(defaults: defaults)
-
-        XCTAssertEqual(sut.state.pasteTimeToLive, .default)
-    }
-
-    @MainActor
-    func test_pasteTimeToLive_savesStateAfterStateChanged() throws {
+    @Test
+    func pasteTimeToLive_savesStateAfterStateChanged() throws {
         let defaults = try makeDefaults()
         let sutSave = try makeSUT(defaults: defaults)
         sutSave.state.pasteTimeToLive = .init(duration: 1234)
 
         let sutRetrieve = try makeSUT(defaults: defaults)
-        XCTAssertEqual(sutRetrieve.state.pasteTimeToLive, .init(duration: 1234))
+        #expect(sutRetrieve.state.pasteTimeToLive == .init(duration: 1234))
     }
 }
 
 // MARK: - Helpers
 
 extension LocalSettingsTests {
-    @MainActor
     private func makeSUT(defaults: Defaults) throws -> LocalSettings {
         let settings = LocalSettings(defaults: defaults)
-        trackForMemoryLeaks(settings)
         return settings
     }
 
     private func makeDefaults() throws -> Defaults {
-        let userDefaults = try XCTUnwrap(UserDefaults(suiteName: #file))
+        let userDefaults = try #require(UserDefaults(suiteName: #file))
         userDefaults.removePersistentDomain(forName: #file)
         return Defaults(userDefaults: userDefaults)
     }
