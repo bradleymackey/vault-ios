cf9af262e5cc2ba0639741c92a9f8941272d840b
Bradley Mackey <bradleymackey@me.com>
2024-10-03 08:26:42 +0100

    More cases in VaultExportDataBlockHeaderGeneratorTests

--- Vault/Tests/VaultBackupTests/VaultExportDataBlockHeaderGeneratorTests.swift
+++ Vault/Tests/VaultBackupTests/VaultExportDataBlockHeaderGeneratorTests.swift
@@ -4,22 +4,29 @@ import Testing
 @testable import VaultBackup
 
 struct VaultExportDataBlockHeaderGeneratorTests {
-    @Test
-    func makeHeader_leftIsDate() {
-        let date = Date(timeIntervalSince1970: 3_000_000)
+    @Test(arguments: [
+        (Date(timeIntervalSince1970: 3_000_000), "Created 2/4/70, 5:20 PM"),
+        (Date(timeIntervalSince1970: 1_727_868_260), "Created 10/2/24, 11:24 AM"),
+    ])
+    func makeHeader_leftIsDate(date: Date, expected: String) {
         let sut = VaultExportDataBlockHeaderGenerator(dateCreated: date, totalNumberOfPages: 100)
 
         let header = sut.makeHeader(pageNumber: 1)
 
-        #expect(header?.left == "Created 2/4/70, 5:20 PM")
+        #expect(header?.left == expected)
     }
 
-    @Test
-    func makeHeader_rightIsPageNumber() {
-        let sut = VaultExportDataBlockHeaderGenerator(dateCreated: Date(), totalNumberOfPages: 500)
+    @Test(arguments: [
+        (1, 1, "Page 1 of 1"),
+        (1, 99, "Page 1 of 99"),
+        (442, 500, "Page 442 of 500"),
+        (999, 999, "Page 999 of 999"),
+    ])
+    func makeHeader_rightIsPageNumber(pageNumber: Int, totalPages: Int, expected: String) {
+        let sut = VaultExportDataBlockHeaderGenerator(dateCreated: Date(), totalNumberOfPages: totalPages)
 
-        let header = sut.makeHeader(pageNumber: 442)
+        let header = sut.makeHeader(pageNumber: pageNumber)
 
-        #expect(header?.right == "Page 442 of 500")
+        #expect(header?.right == expected)
     }
 }
