ed1e01b7c9743d819eae813d07316dda68676fce
Bradley Mackey <bradleymackey@me.com>
2024-10-03 11:07:11 +0100

    Add expect(firstValues:) on publisher

--- Vault/Sources/TestHelpers/SwiftTesting/Publisher+Testing.swift
+++ Vault/Sources/TestHelpers/SwiftTesting/Publisher+Testing.swift
@@ -1,17 +1,56 @@
 import Combine
 import Foundation
+import FoundationExtensions
 import Testing
 
-extension Publisher where Failure == Never {
-    /// Creates a testing `confirmation` that a
-    public func expect(eventCount: Int, actions: () async throws -> Void) async throws {
+extension Publisher {
+    /// Creates a testing `confirmation` that a publisher will produce the given number of elements.
+    public func expect(
+        valueCount: Int,
+        sourceLocation _: SourceLocation = .__here(),
+        when actions: () async throws -> Void
+    ) async throws {
         var cancellable: AnyCancellable?
-        try await confirmation(expectedCount: eventCount) { confirmation in
+        defer { cancellable?.cancel() }
+        try await confirmation(expectedCount: valueCount) { confirmation in
             cancellable = sink { _ in
+                // noop
+            } receiveValue: { _ in
                 confirmation.confirm()
             }
             try await actions()
         }
-        cancellable?.cancel()
+    }
+}
+
+extension Publisher where Output: Equatable, Output: Sendable {
+    public func expect(
+        firstValues: [Output],
+        timeout: Duration? = nil,
+        sourceLocation: SourceLocation = .__here(),
+        // swiftlint:disable all
+        when actions: sending @escaping () async throws -> Void
+        // swiftlint:enable all
+    ) async throws {
+        var cancellable: AnyCancellable?
+        defer { cancellable?.cancel() }
+
+        let pending = PendingValue<Void>()
+
+        let collectedValues = Atomic<[Output]>(initialValue: [])
+        cancellable = prefix(firstValues.count).sink { _ in
+            Task { await pending.fulfill() }
+        } receiveValue: { value in
+            collectedValues.modify {
+                $0.append(value)
+            }
+        }
+
+        Task {
+            try await actions()
+        }
+
+        try await pending.awaitValue(timeout: timeout)
+        #expect(collectedValues.value == firstValues, sourceLocation: sourceLocation)
     }
 }
