9638e04fedfb0867607c7569079da002c3b124b3
Bradley Mackey <bradleymackey@me.com>
2024-11-04 11:30:24 +0000

    Update OTPCodeTimerPeriodStateTests

--- Vault/Tests/VaultFeedTests/Presentation/OTPCode/OTPCodeTimerPeriodStateTests.swift
+++ Vault/Tests/VaultFeedTests/Presentation/OTPCode/OTPCodeTimerPeriodStateTests.swift
@@ -1,29 +1,30 @@
 import Combine
 import Foundation
 import TestHelpers
+import Testing
 import VaultCore
 import VaultFeed
-import XCTest
 
-final class OTPCodeTimerPeriodStateTests: XCTestCase {
-    @MainActor
-    func test_init_initialAnimationStateIsFrozen() {
+@MainActor
+struct OTPCodeTimerPeriodStateTests {
+    @Test
+    func init_initialAnimationStateIsFrozen() {
         let sut = makeSUT(pub: PassthroughSubject().eraseToAnyPublisher())
 
-        XCTAssertEqual(sut.animationState, .freeze(fraction: 0))
+        #expect(sut.animationState == .freeze(fraction: 0))
     }
 
-    @MainActor
-    func test_animationState_assignsValueToState() async throws {
+    @Test
+    func animationState_assignsValueToState() async throws {
         let valueSubject = PassthroughSubject<OTPCodeTimerState, Never>()
         let sut = makeSUT(pub: valueSubject.eraseToAnyPublisher())
 
         let initialState = OTPCodeTimerState(startTime: 69, endTime: 420)
-        await expectSingleMutation(observable: sut, keyPath: \.animationState) {
+        try await sut.waitForChange(to: \.animationState) {
             valueSubject.send(initialState)
         }
 
-        XCTAssertEqual(sut.animationState, .animate(initialState))
+        #expect(sut.animationState == .animate(initialState))
     }
 
     // MARK: - Helpers
