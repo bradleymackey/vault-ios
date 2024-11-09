e2882ab6b3077af0b743bfa09f586ed79e9937fc
Bradley Mackey <bradleymackey@me.com>
2024-11-09 20:42:16 +0000

    Reorganize demo section to developer section

---
+++ Vault/Sources/VaultiOS/Views/Developer Tools/DeveloperCreateItemsView.swift
@@ -0,0 +1,26 @@
+import Foundation
+import SwiftUI
+
+struct DeveloperCreateItemsView: View {
+    var body: some View {
+        Form {
+            Button {
+                print("TODO: create code in DB")
+            } label: {
+                Text("Create code")
+            }
+
+            Button {
+                print("TODO: create note in DB")
+            } label: {
+                Text("Create note")
+            }
+
+            Button {
+                print("TODO: create encrypted note in DB")
+            } label: {
+                Text("Create encrypted note")
+            }
+        }
+    }
+}
