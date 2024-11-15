7711bbf044a5acecb4e3f28eecc028d369dd20a8
Bradley Mackey <bradleymackey@me.com>
2024-11-15 22:54:31 +0000

    Migrate BackupPasswordExporterTests to swift testing

--- Vault/Tests/VaultFeedTests/Storage/BackupPasswordExporterTests.swift
+++ Vault/Tests/VaultFeedTests/Storage/BackupPasswordExporterTests.swift
@@ -1,21 +1,21 @@
 import Foundation
 import TestHelpers
+import Testing
 import VaultKeygen
-import XCTest
 @testable import VaultFeed
 
-final class BackupPasswordExporterTests: XCTestCase {
-    @MainActor
-    func test_makeExport_encodesFromStore() async throws {
+struct BackupPasswordExporterTests {
+    @Test
+    func makeExport_encodesFromStore() async throws {
         let saltData = Data(repeating: 0x69, count: 20)
         let examplePassword = DerivedEncryptionKey(key: .repeating(byte: 0x68), salt: saltData, keyDervier: .testing)
         let sut = makeSUT(backupPassword: examplePassword)
 
         let export = try await sut.makeExport()
 
-        let str = try XCTUnwrap(String(data: export, encoding: .utf8))
+        let str = try #require(String(data: export, encoding: .utf8))
 
-        XCTAssertEqual(str, """
+        #expect(str == """
         {
           "KEY" : "aGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGg=",
           "KEY_DERIVER" : "vault.keygen.testing",
@@ -29,7 +29,6 @@ final class BackupPasswordExporterTests: XCTestCase {
 // MARK: - Helpers
 
 extension BackupPasswordExporterTests {
-    @MainActor
     private func makeSUT(
         backupPassword: DerivedEncryptionKey
     ) -> BackupPasswordExporter {
