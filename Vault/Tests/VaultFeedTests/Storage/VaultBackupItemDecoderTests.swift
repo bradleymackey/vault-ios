80f15fae86aea0f964f172546b686b654ce393d7
Bradley Mackey <bradleymackey@me.com>
2024-11-15 23:23:35 +0000

    Migrate VaultBackupItemDecoderTests to swift testing

--- Vault/Tests/VaultFeedTests/Storage/VaultBackupItemDecoderTests.swift
+++ Vault/Tests/VaultFeedTests/Storage/VaultBackupItemDecoderTests.swift
@@ -1,14 +1,15 @@
 import Foundation
 import TestHelpers
+import Testing
 import VaultBackup
 import VaultCore
-import XCTest
 @testable import VaultFeed
 
-final class VaultBackupItemDecoderTests: XCTestCase {
+final class VaultBackupItemDecoderTests {
     // MARK: - Note
 
-    func test_decodeNote_decodesNote() throws {
+    @Test
+    func decodeNote_decodesNote() throws {
         let id = UUID()
         let createdDate = Date()
         let updateDate = Date()
@@ -33,38 +34,40 @@ final class VaultBackupItemDecoderTests: XCTestCase {
 
         let decodedItem = try sut.decode(backupItem: item)
 
-        XCTAssertEqual(decodedItem.id.rawValue, id)
-        XCTAssertEqual(decodedItem.metadata.created, createdDate)
-        XCTAssertEqual(decodedItem.metadata.updated, updateDate)
-        XCTAssertEqual(decodedItem.metadata.userDescription, description)
-        XCTAssertEqual(decodedItem.metadata.visibility, .always)
-        XCTAssertEqual(decodedItem.metadata.searchableLevel, .full)
-        XCTAssertEqual(decodedItem.metadata.searchPassphrase, "hello")
-        XCTAssertEqual(decodedItem.metadata.killphrase, "killme")
-        XCTAssertEqual(decodedItem.metadata.color, .init(red: 0.1, green: 0.2, blue: 0.3))
-        XCTAssertEqual(decodedItem.metadata.tags, [.init(id: tag)])
-        XCTAssertEqual(decodedItem.metadata.lockState, .notLocked)
-        XCTAssertEqual(decodedItem.metadata.relativeOrder, 77777)
-        XCTAssertEqual(decodedItem.item.secureNote?.title, "title")
-        XCTAssertEqual(decodedItem.item.secureNote?.contents, "contents")
-        XCTAssertEqual(decodedItem.item.secureNote?.format, .markdown)
+        #expect(decodedItem.id.rawValue == id)
+        #expect(decodedItem.metadata.created == createdDate)
+        #expect(decodedItem.metadata.updated == updateDate)
+        #expect(decodedItem.metadata.userDescription == description)
+        #expect(decodedItem.metadata.visibility == .always)
+        #expect(decodedItem.metadata.searchableLevel == .full)
+        #expect(decodedItem.metadata.searchPassphrase == "hello")
+        #expect(decodedItem.metadata.killphrase == "killme")
+        #expect(decodedItem.metadata.color == .init(red: 0.1, green: 0.2, blue: 0.3))
+        #expect(decodedItem.metadata.tags == [.init(id: tag)])
+        #expect(decodedItem.metadata.lockState == .notLocked)
+        #expect(decodedItem.metadata.relativeOrder == 77777)
+        #expect(decodedItem.item.secureNote?.title == "title")
+        #expect(decodedItem.item.secureNote?.contents == "contents")
+        #expect(decodedItem.item.secureNote?.format == .markdown)
 
-        XCTAssertNil(decodedItem.item.encryptedItem)
-        XCTAssertNil(decodedItem.item.otpCode)
+        #expect(decodedItem.item.encryptedItem == nil)
+        #expect(decodedItem.item.otpCode == nil)
     }
 
-    func test_decodeNote_decodeswithNilContentsIntoEmptyString() throws {
+    @Test
+    func decodeNote_decodeswithNilContentsIntoEmptyString() throws {
         let item = anyNoteItem(contents: nil)
         let sut = makeSUT()
 
         let decodedItem = try sut.decode(backupItem: item)
 
-        XCTAssertEqual(decodedItem.item.secureNote?.contents, "")
+        #expect(decodedItem.item.secureNote?.contents == "")
     }
 
     // MARK: - Encrypted Item
 
-    func test_decodeEncryptedItem_decodesEncryptedItem() throws {
+    @Test
+    func decodeEncryptedItem_decodesEncryptedItem() throws {
         let id = UUID()
         let createdDate = Date()
         let updateDate = Date()
@@ -103,33 +106,34 @@ final class VaultBackupItemDecoderTests: XCTestCase {
 
         let decodedItem = try sut.decode(backupItem: item)
 
-        XCTAssertEqual(decodedItem.id.rawValue, id)
-        XCTAssertEqual(decodedItem.metadata.created, createdDate)
-        XCTAssertEqual(decodedItem.metadata.updated, updateDate)
-        XCTAssertEqual(decodedItem.metadata.userDescription, description)
-        XCTAssertEqual(decodedItem.metadata.visibility, .always)
-        XCTAssertEqual(decodedItem.metadata.searchableLevel, .full)
-        XCTAssertEqual(decodedItem.metadata.searchPassphrase, "hello")
-        XCTAssertEqual(decodedItem.metadata.killphrase, "killmenow")
-        XCTAssertEqual(decodedItem.metadata.color, .init(red: 0.1, green: 0.2, blue: 0.3))
-        XCTAssertEqual(decodedItem.metadata.tags, [.init(id: tag)])
-        XCTAssertEqual(decodedItem.metadata.lockState, .notLocked)
-        XCTAssertEqual(decodedItem.metadata.relativeOrder, 77777)
-        XCTAssertEqual(decodedItem.item.encryptedItem?.version, "1.0.7")
-        XCTAssertEqual(decodedItem.item.encryptedItem?.title, "this is my title")
-        XCTAssertEqual(decodedItem.item.encryptedItem?.data, itemData)
-        XCTAssertEqual(decodedItem.item.encryptedItem?.authentication, itemAuthentication)
-        XCTAssertEqual(decodedItem.item.encryptedItem?.encryptionIV, itemEncryptionIV)
-        XCTAssertEqual(decodedItem.item.encryptedItem?.keygenSalt, itemKeygenSalt)
-        XCTAssertEqual(decodedItem.item.encryptedItem?.keygenSignature, itemSignature)
+        #expect(decodedItem.id.rawValue == id)
+        #expect(decodedItem.metadata.created == createdDate)
+        #expect(decodedItem.metadata.updated == updateDate)
+        #expect(decodedItem.metadata.userDescription == description)
+        #expect(decodedItem.metadata.visibility == .always)
+        #expect(decodedItem.metadata.searchableLevel == .full)
+        #expect(decodedItem.metadata.searchPassphrase == "hello")
+        #expect(decodedItem.metadata.killphrase == "killmenow")
+        #expect(decodedItem.metadata.color == .init(red: 0.1, green: 0.2, blue: 0.3))
+        #expect(decodedItem.metadata.tags == [.init(id: tag)])
+        #expect(decodedItem.metadata.lockState == .notLocked)
+        #expect(decodedItem.metadata.relativeOrder == 77777)
+        #expect(decodedItem.item.encryptedItem?.version == "1.0.7")
+        #expect(decodedItem.item.encryptedItem?.title == "this is my title")
+        #expect(decodedItem.item.encryptedItem?.data == itemData)
+        #expect(decodedItem.item.encryptedItem?.authentication == itemAuthentication)
+        #expect(decodedItem.item.encryptedItem?.encryptionIV == itemEncryptionIV)
+        #expect(decodedItem.item.encryptedItem?.keygenSalt == itemKeygenSalt)
+        #expect(decodedItem.item.encryptedItem?.keygenSignature == itemSignature)
 
-        XCTAssertNil(decodedItem.item.secureNote)
-        XCTAssertNil(decodedItem.item.otpCode)
+        #expect(decodedItem.item.secureNote == nil)
+        #expect(decodedItem.item.otpCode == nil)
     }
 
     // MARK: - OTP Code
 
-    func test_decodeOTP_decodesTOTPCode() throws {
+    @Test
+    func decodeOTP_decodesTOTPCode() throws {
         let id = UUID()
         let createdDate = Date()
         let updateDate = Date()
@@ -164,29 +168,30 @@ final class VaultBackupItemDecoderTests: XCTestCase {
         let decodedItem = try sut.decode(backupItem: item)
 
         let expectedSecret = OTPAuthSecret(data: Data(hex: "ababababa"), format: .base32)
-        XCTAssertEqual(decodedItem.id.rawValue, id)
-        XCTAssertEqual(decodedItem.metadata.created, createdDate)
-        XCTAssertEqual(decodedItem.metadata.updated, updateDate)
-        XCTAssertEqual(decodedItem.metadata.userDescription, description)
-        XCTAssertEqual(decodedItem.metadata.searchableLevel, .full)
-        XCTAssertEqual(decodedItem.metadata.searchPassphrase, "pass")
-        XCTAssertEqual(decodedItem.metadata.killphrase, "killme")
-        XCTAssertEqual(decodedItem.metadata.visibility, .always)
-        XCTAssertEqual(decodedItem.metadata.color, .init(red: 0.1, green: 0.2, blue: 0.3))
-        XCTAssertEqual(decodedItem.metadata.lockState, .lockedWithNativeSecurity)
-        XCTAssertEqual(decodedItem.metadata.relativeOrder, 1234)
-        XCTAssertEqual(decodedItem.item.otpCode?.type, .totp(period: 30))
-        XCTAssertEqual(decodedItem.item.otpCode?.data.secret, expectedSecret)
-        XCTAssertEqual(decodedItem.item.otpCode?.data.accountName, "my acc")
-        XCTAssertEqual(decodedItem.item.otpCode?.data.issuer, "my iss")
-        XCTAssertEqual(decodedItem.item.otpCode?.data.digits, OTPAuthDigits(value: 5))
-        XCTAssertEqual(decodedItem.item.otpCode?.data.algorithm, .sha1)
+        #expect(decodedItem.id.rawValue == id)
+        #expect(decodedItem.metadata.created == createdDate)
+        #expect(decodedItem.metadata.updated == updateDate)
+        #expect(decodedItem.metadata.userDescription == description)
+        #expect(decodedItem.metadata.searchableLevel == .full)
+        #expect(decodedItem.metadata.searchPassphrase == "pass")
+        #expect(decodedItem.metadata.killphrase == "killme")
+        #expect(decodedItem.metadata.visibility == .always)
+        #expect(decodedItem.metadata.color == .init(red: 0.1, green: 0.2, blue: 0.3))
+        #expect(decodedItem.metadata.lockState == .lockedWithNativeSecurity)
+        #expect(decodedItem.metadata.relativeOrder == 1234)
+        #expect(decodedItem.item.otpCode?.type == .totp(period: 30))
+        #expect(decodedItem.item.otpCode?.data.secret == expectedSecret)
+        #expect(decodedItem.item.otpCode?.data.accountName == "my acc")
+        #expect(decodedItem.item.otpCode?.data.issuer == "my iss")
+        #expect(decodedItem.item.otpCode?.data.digits == OTPAuthDigits(value: 5))
+        #expect(decodedItem.item.otpCode?.data.algorithm == .sha1)
 
-        XCTAssertNil(decodedItem.item.encryptedItem)
-        XCTAssertNil(decodedItem.item.secureNote)
+        #expect(decodedItem.item.encryptedItem == nil)
+        #expect(decodedItem.item.secureNote == nil)
     }
 
-    func test_decodeOTP_decodesHOTPCode() throws {
+    @Test
+    func decodeOTP_decodesHOTPCode() throws {
         let id = UUID()
         let createdDate = Date()
         let updateDate = Date()
@@ -221,83 +226,102 @@ final class VaultBackupItemDecoderTests: XCTestCase {
         let decodedItem = try sut.decode(backupItem: item)
 
         let expectedSecret = OTPAuthSecret(data: Data(hex: "ababababaff"), format: .base32)
-        XCTAssertEqual(decodedItem.id.rawValue, id)
-        XCTAssertEqual(decodedItem.metadata.created, createdDate)
-        XCTAssertEqual(decodedItem.metadata.updated, updateDate)
-        XCTAssertEqual(decodedItem.metadata.relativeOrder, 4321)
-        XCTAssertEqual(decodedItem.metadata.userDescription, description)
-        XCTAssertEqual(decodedItem.metadata.searchableLevel, .full)
-        XCTAssertEqual(decodedItem.metadata.visibility, .onlySearch)
-        XCTAssertEqual(decodedItem.metadata.searchPassphrase, "nice")
-        XCTAssertEqual(decodedItem.metadata.killphrase, "killmeNOW")
-        XCTAssertEqual(decodedItem.metadata.color, .init(red: 0.2, green: 0.2, blue: 0.3))
-        XCTAssertEqual(decodedItem.metadata.lockState, .notLocked)
-        XCTAssertEqual(decodedItem.item.otpCode?.type, .hotp(counter: 10))
-        XCTAssertEqual(decodedItem.item.otpCode?.data.secret, expectedSecret)
-        XCTAssertEqual(decodedItem.item.otpCode?.data.accountName, "my acc a")
-        XCTAssertEqual(decodedItem.item.otpCode?.data.issuer, "my iss a")
-        XCTAssertEqual(decodedItem.item.otpCode?.data.digits, OTPAuthDigits(value: 7))
-        XCTAssertEqual(decodedItem.item.otpCode?.data.algorithm, .sha1)
+        #expect(decodedItem.id.rawValue == id)
+        #expect(decodedItem.metadata.created == createdDate)
+        #expect(decodedItem.metadata.updated == updateDate)
+        #expect(decodedItem.metadata.relativeOrder == 4321)
+        #expect(decodedItem.metadata.userDescription == description)
+        #expect(decodedItem.metadata.searchableLevel == .full)
+        #expect(decodedItem.metadata.visibility == .onlySearch)
+        #expect(decodedItem.metadata.searchPassphrase == "nice")
+        #expect(decodedItem.metadata.killphrase == "killmeNOW")
+        #expect(decodedItem.metadata.color == .init(red: 0.2, green: 0.2, blue: 0.3))
+        #expect(decodedItem.metadata.lockState == .notLocked)
+        #expect(decodedItem.item.otpCode?.type == .hotp(counter: 10))
+        #expect(decodedItem.item.otpCode?.data.secret == expectedSecret)
+        #expect(decodedItem.item.otpCode?.data.accountName == "my acc a")
+        #expect(decodedItem.item.otpCode?.data.issuer == "my iss a")
+        #expect(decodedItem.item.otpCode?.data.digits == OTPAuthDigits(value: 7))
+        #expect(decodedItem.item.otpCode?.data.algorithm == .sha1)
 
-        XCTAssertNil(decodedItem.item.encryptedItem)
-        XCTAssertNil(decodedItem.item.secureNote)
+        #expect(decodedItem.item.encryptedItem == nil)
+        #expect(decodedItem.item.secureNote == nil)
     }
 
-    func test_decodeOTP_succeedsForAnyOTPItem() throws {
+    @Test
+    func decodeOTP_succeedsForAnyOTPItem() throws {
         // Checks that 'anyOTPItem' has a valid item, to ensure the throwing tests are correct.
         let otp = anyOTPItem()
         let sut = makeSUT()
 
-        XCTAssertNoThrow(try sut.decode(backupItem: otp))
+        #expect(throws: Never.self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 
-    func test_decodeOTP_failsToDecodesAlgorithms() throws {
+    @Test
+    func decodeOTP_failsToDecodesAlgorithms() throws {
         let otp_sha1 = anyOTPItem(algorithm: "SHA1")
         let item_sha1 = try makeSUT().decode(backupItem: otp_sha1)
-        XCTAssertEqual(item_sha1.item.otpCode?.data.algorithm, .sha1)
+        #expect(item_sha1.item.otpCode?.data.algorithm == .sha1)
 
         let otp_sha256 = anyOTPItem(algorithm: "SHA256")
         let item_sha256 = try makeSUT().decode(backupItem: otp_sha256)
-        XCTAssertEqual(item_sha256.item.otpCode?.data.algorithm, .sha256)
+        #expect(item_sha256.item.otpCode?.data.algorithm == .sha256)
 
         let otp_sha512 = anyOTPItem(algorithm: "SHA512")
         let item_sha512 = try makeSUT().decode(backupItem: otp_sha512)
-        XCTAssertEqual(item_sha512.item.otpCode?.data.algorithm, .sha512)
+        #expect(item_sha512.item.otpCode?.data.algorithm == .sha512)
     }
 
-    func test_decodeOTP_failsForInvalidType() throws {
+    @Test
+    func decodeOTP_failsForInvalidType() throws {
         let otp = anyOTPItem(type: "inv")
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.decode(backupItem: otp))
+        #expect(throws: (any Error).self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 
-    func test_decodeOTP_failsToDecodeTOTPIfNoPeriod() throws {
+    @Test
+    func decodeOTP_failsToDecodeTOTPIfNoPeriod() throws {
         let otp = anyOTPItem(type: "totp", period: nil)
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.decode(backupItem: otp))
+        #expect(throws: (any Error).self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 
-    func test_decodeOTP_failsToDecodeHOTPIfNoCounter() throws {
+    @Test
+    func decodeOTP_failsToDecodeHOTPIfNoCounter() throws {
         let otp = anyOTPItem(type: "hotp", counter: nil)
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.decode(backupItem: otp))
+        #expect(throws: (any Error).self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 
-    func test_decodeOTP_failsToDecodeInvalidSecretFormat() throws {
+    @Test
+    func decodeOTP_failsToDecodeInvalidSecretFormat() throws {
         let otp = anyOTPItem(secretFormat: "inv")
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.decode(backupItem: otp))
+        #expect(throws: (any Error).self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 
-    func test_decodeOTP_failsToDecodeInvalidAlgorithm() throws {
+    @Test
+    func decodeOTP_failsToDecodeInvalidAlgorithm() throws {
         let otp = anyOTPItem(algorithm: "inv")
         let sut = makeSUT()
 
-        XCTAssertThrowsError(try sut.decode(backupItem: otp))
+        #expect(throws: (any Error).self) {
+            try sut.decode(backupItem: otp)
+        }
     }
 }
 
