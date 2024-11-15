080bc27761180f9b255db4e007b4ca6d0ea20657
Bradley Mackey <bradleymackey@me.com>
2024-11-15 23:12:54 +0000

    Migrate PersistedVaultTagDecoderTests to swift testing

--- Vault/Tests/VaultFeedTests/Storage/PersistedVaultTagDecoderTests.swift
+++ Vault/Tests/VaultFeedTests/Storage/PersistedVaultTagDecoderTests.swift
@@ -1,80 +1,76 @@
 import Foundation
 import SwiftData
 import TestHelpers
-import XCTest
+import Testing
 @testable import VaultFeed
 
-final class PersistedVaultTagDecoderTests: XCTestCase {
-    // swiftlint:disable:next implicitly_unwrapped_optional
-    private var context: ModelContext!
+final class PersistedVaultTagDecoderTests {
+    private let context: ModelContext
 
-    override func setUp() async throws {
-        try await super.setUp()
-
+    init() throws {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: PersistedVaultItem.self, configurations: config)
         context = ModelContext(container)
-    }
-
-    override func tearDown() async throws {
-        try await super.tearDown()
-
-        context = nil
     }
 }
 
 // MARK: - Fields
 
 extension PersistedVaultTagDecoderTests {
-    func test_decode_id() throws {
+    @Test
+    func decode_id() throws {
         let id = UUID()
         let item = makePersistedTag(id: id)
         let sut = makeSUT()
 
         let decoded = try sut.decode(item: item)
 
-        XCTAssertEqual(decoded.id.id, id)
+        #expect(decoded.id.id == id)
     }
 
-    func test_decode_name() throws {
+    @Test
+    func decode_name() throws {
         let name = "my tag name"
         let item = makePersistedTag(title: name)
         let sut = makeSUT()
 
         let decoded = try sut.decode(item: item)
 
-        XCTAssertEqual(decoded.name, name)
+        #expect(decoded.name == name)
     }
 
-    func test_decode_colorNilIsTagDefault() throws {
+    @Test
+    func decode_colorNilIsTagDefault() throws {
         let item = makePersistedTag(color: nil)
         let sut = makeSUT()
 
         let decoded = try sut.decode(item: item)
 
-        XCTAssertEqual(decoded.color, .tagDefault)
+        #expect(decoded.color == .tagDefault)
     }
 
-    func test_decode_colorWithValues() throws {
+    @Test
+    func decode_colorWithValues() throws {
         let color = PersistedColor(red: 0.5, green: 0.6, blue: 0.7)
         let item = makePersistedTag(color: color)
         let sut = makeSUT()
 
         let decoded = try sut.decode(item: item)
 
-        XCTAssertEqual(decoded.color.red, 0.5)
-        XCTAssertEqual(decoded.color.green, 0.6)
-        XCTAssertEqual(decoded.color.blue, 0.7)
+        #expect(decoded.color.red == 0.5)
+        #expect(decoded.color.green == 0.6)
+        #expect(decoded.color.blue == 0.7)
     }
 
-    func test_decode_iconName() throws {
+    @Test
+    func decode_iconName() throws {
         let iconName = "my icon name"
         let item = makePersistedTag(iconName: iconName)
         let sut = makeSUT()
 
         let decoded = try sut.decode(item: item)
 
-        XCTAssertEqual(decoded.iconName, iconName)
+        #expect(decoded.iconName == iconName)
     }
 }
 
