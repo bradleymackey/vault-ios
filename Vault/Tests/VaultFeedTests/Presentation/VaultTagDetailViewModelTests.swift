4aaf4d6618873016790e227ef3b00ef045a963f1
Bradley Mackey <bradleymackey@me.com>
2025-11-29 22:04:16 +0000

    Migrate VaultTagDetailViewModelTests

--- Vault/Tests/VaultFeedTests/Presentation/VaultTagDetailViewModelTests.swift
+++ Vault/Tests/VaultFeedTests/Presentation/VaultTagDetailViewModelTests.swift
@@ -1,50 +1,52 @@
 import Foundation
 import TestHelpers
-import XCTest
+import Testing
 @testable import VaultFeed
 
-final class VaultTagDetailViewModelTests: XCTestCase {
-    @MainActor
-    func test_init_hasNotSideEffectsOnStore() async {
+@Suite
+@MainActor
+struct VaultTagDetailViewModelTests {
+    @Test
+    func init_hasNotSideEffectsOnStore() {
         let store = VaultStoreStub.empty
         let tagStore = VaultTagStoreStub.empty
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
         _ = makeSUT(dataModel: dataModel)
 
-        XCTAssertEqual(store.calledMethods, [])
-        XCTAssertEqual(tagStore.calledMethods, [])
+        #expect(store.calledMethods == [])
+        #expect(tagStore.calledMethods == [])
     }
 
-    @MainActor
-    func test_init_errorsAreNil() async {
+    @Test
+    func init_errorsAreNil() {
         let sut = makeSUT()
 
-        XCTAssertNil(sut.saveError)
-        XCTAssertNil(sut.deleteError)
+        #expect(sut.saveError == nil)
+        #expect(sut.deleteError == nil)
     }
 
-    @MainActor
-    func test_init_whenExistingTagIsNil_setsDefaultValues() async {
+    @Test
+    func init_whenExistingTagIsNil_setsDefaultValues() {
         let sut = makeSUT()
 
-        XCTAssertEqual(sut.currentTag.name, "")
-        XCTAssertEqual(sut.currentTag.color, .tagDefault)
-        XCTAssertEqual(sut.currentTag.iconName, "tag.fill")
+        #expect(sut.currentTag.name == "")
+        #expect(sut.currentTag.color == .tagDefault)
+        #expect(sut.currentTag.iconName == "tag.fill")
     }
 
-    @MainActor
-    func test_init_whenExistingTagIsNotNil_setsValueFromTag() async {
+    @Test
+    func init_whenExistingTagIsNotNil_setsValueFromTag() {
         let color = VaultItemColor.random()
         let tag = VaultItemTag(id: .init(), name: "tag", color: color, iconName: "figure.2.arms.open")
         let sut = makeSUT(existingTag: tag)
 
-        XCTAssertEqual(sut.currentTag.name, "tag")
-        XCTAssertEqual(sut.currentTag.color, color)
-        XCTAssertEqual(sut.currentTag.iconName, "figure.2.arms.open")
+        #expect(sut.currentTag.name == "tag")
+        #expect(sut.currentTag.color == color)
+        #expect(sut.currentTag.iconName == "figure.2.arms.open")
     }
 
-    @MainActor
-    func test_save_newTagInsertsIntoStore() async {
+    @Test
+    func save_newTagInsertsIntoStore() async {
         let store = VaultStoreStub.empty
         let tagStore = VaultTagStoreStub.empty
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -52,13 +54,13 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.save()
 
-        XCTAssertEqual(store.calledMethods, [.export])
-        XCTAssertEqual(tagStore.calledMethods, [.insertTag, .retrieveTags])
-        XCTAssertNil(sut.saveError)
+        #expect(store.calledMethods == [.export])
+        #expect(tagStore.calledMethods == [.insertTag, .retrieveTags])
+        #expect(sut.saveError == nil)
     }
 
-    @MainActor
-    func test_save_existingTagUpdatesIntoStore() async {
+    @Test
+    func save_existingTagUpdatesIntoStore() async {
         let store = VaultStoreStub.empty
         let tagStore = VaultTagStoreStub.empty
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -67,13 +69,13 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.save()
 
-        XCTAssertEqual(store.calledMethods, [.export])
-        XCTAssertEqual(tagStore.calledMethods, [.updateTag, .retrieveTags])
-        XCTAssertNil(sut.saveError)
+        #expect(store.calledMethods == [.export])
+        #expect(tagStore.calledMethods == [.updateTag, .retrieveTags])
+        #expect(sut.saveError == nil)
     }
 
-    @MainActor
-    func test_save_insertErrorSetsSaveError() async {
+    @Test
+    func save_insertErrorSetsSaveError() async {
         let store = VaultStoreErroring(error: TestError())
         let tagStore = VaultTagStoreErroring(error: TestError())
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -81,11 +83,11 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.save()
 
-        XCTAssertNotNil(sut.saveError)
+        #expect(sut.saveError != nil)
     }
 
-    @MainActor
-    func test_save_updateErrorSetsSaveError() async {
+    @Test
+    func save_updateErrorSetsSaveError() async {
         let store = VaultStoreErroring(error: TestError())
         let tagStore = VaultTagStoreErroring(error: TestError())
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -94,11 +96,11 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.save()
 
-        XCTAssertNotNil(sut.saveError)
+        #expect(sut.saveError != nil)
     }
 
-    @MainActor
-    func test_delete_noExistingTagDoesNotCallDelete() async {
+    @Test
+    func delete_noExistingTagDoesNotCallDelete() async {
         let store = VaultStoreStub.empty
         let tagStore = VaultTagStoreStub.empty
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -106,13 +108,13 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.delete()
 
-        XCTAssertEqual(store.calledMethods, [])
-        XCTAssertEqual(tagStore.calledMethods, [])
-        XCTAssertNil(sut.deleteError)
+        #expect(store.calledMethods == [])
+        #expect(tagStore.calledMethods == [])
+        #expect(sut.deleteError == nil)
     }
 
-    @MainActor
-    func test_delete_existingTagCallsDelete() async {
+    @Test
+    func delete_existingTagCallsDelete() async {
         let store = VaultStoreStub.empty
         let tagStore = VaultTagStoreStub.empty
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -121,13 +123,13 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.delete()
 
-        XCTAssertEqual(store.calledMethods, [.retrieve, .export])
-        XCTAssertEqual(tagStore.calledMethods, [.deleteTag, .retrieveTags])
-        XCTAssertNil(sut.deleteError)
+        #expect(store.calledMethods == [.retrieve, .export])
+        #expect(tagStore.calledMethods == [.deleteTag, .retrieveTags])
+        #expect(sut.deleteError == nil)
     }
 
-    @MainActor
-    func test_delete_deleteErrorSetsDeleteError() async {
+    @Test
+    func delete_deleteErrorSetsDeleteError() async {
         let store = VaultStoreErroring(error: TestError())
         let tagStore = VaultTagStoreErroring(error: TestError())
         let dataModel = anyVaultDataModel(vaultStore: store, vaultTagStore: tagStore)
@@ -136,19 +138,19 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
         await sut.delete()
 
-        XCTAssertNotNil(sut.deleteError)
+        #expect(sut.deleteError != nil)
     }
 
-    @MainActor
-    func test_clearErrors_setsErrorsToNil() async {
+    @Test
+    func clearErrors_setsErrorsToNil() {
         let sut = makeSUT()
         sut.saveError = .init(userTitle: "title", userDescription: "desc", debugDescription: "debug")
         sut.deleteError = .init(userTitle: "title", userDescription: "desc", debugDescription: "debug")
 
         sut.clearErrors()
 
-        XCTAssertNil(sut.saveError)
-        XCTAssertNil(sut.deleteError)
+        #expect(sut.saveError == nil)
+        #expect(sut.deleteError == nil)
     }
 }
 
@@ -156,7 +158,7 @@ final class VaultTagDetailViewModelTests: XCTestCase {
 
 extension VaultTagDetailViewModelTests {
     @MainActor
-    func makeSUT(
+    private func makeSUT(
         dataModel: VaultDataModel = VaultDataModel(
             vaultStore: VaultStoreStub(),
             vaultTagStore: VaultTagStoreStub(),
